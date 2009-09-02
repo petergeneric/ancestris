4bf2ee26a8e651fa1eba4179f103efea83605884
dandre <dandre@ancestris.org>
2009-09-02 21:46:56 +0000

    import rapport gedart

---
+++ dev/app/src/report/gedart/GedartTemplates.java
@@ -0,0 +1,47 @@
+package gedart;
+
+import genj.report.ReportLoader;
+import genj.util.EnvironmentChecker;
+
+import java.io.File;
+import java.util.TreeMap;
+
+class GedartTemplates extends TreeMap<String, String> {
+
+	public GedartTemplates() {
+		put("default", "Par d�faut");
+		File gedartDir = new File(ReportLoader.getReportDirectory(),
+				"gedart/templates");
+		putAll(new GedartTemplates(gedartDir));
+
+		gedartDir = new File(ReportLoader.getReportDirectory(),
+		"gedartcustom/templates");
+		putAll(new GedartTemplates(gedartDir));
+
+// exists?
+		File dir = new File(EnvironmentChecker.getProperty(this,
+				"user.home.genj/gedart/templates", "?",
+				"Looking for gedart/templates"));
+
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		putAll(new GedartTemplates(dir));
+	}
+
+	public GedartTemplates(File dir) {
+		if (dir.isDirectory()) {
+
+			// loop over Templatess
+			File[] files = dir.listFiles();
+			for (int b = 0; b < files.length; b++) {
+				File file = files[b];
+				if (!file.isDirectory())
+					continue;
+				if (!new File(file, "index.vm").exists())
+					continue;
+				put(file.getName(), file.getName());
+			}
+		}
+	}
+}
