cf43ec242c2c35b19906a92afcbca3d31b427ce6
dandre <dandre@ancestris.org>
2009-09-02 20:37:36 +0000

    correction

--- 20090901/sandbox/console/src/core/com/sadinoff/genj/console/LineSource.java
+++
@@ -1,9 +0,0 @@
-package com.sadinoff.genj.console;
-
-import java.io.IOException;
-
-public interface LineSource 
-{
-    String readLine() throws IOException;
-    void setPrompt(String prompt);
-}
