cff9d39debd43a22a741a249ee3a3b8a2782d589
dandre <dandre@ancestris.org>
2009-09-23 08:46:48 +0000

    Le template default était mal reconnu dans le menu contextuel

--- dev/app/src/report/gedart/GedartTemplates.java
+++ dev/app/src/report/gedart/GedartTemplates.java
@@ -9,15 +9,17 @@ import java.util.TreeMap;
 class GedartTemplates extends TreeMap<String, GedartTemplate> {
 
 	public GedartTemplates() {
+		// first templates in report/gedart/templates
 		File gedartDir = new File(ReportLoader.getReportDirectory(),
 				"gedart/templates");
 		putAll(new GedartTemplates(gedartDir));
 
+		// second templates in report/gedartcustom/templates
 		gedartDir = new File(ReportLoader.getReportDirectory(),
 		"gedartcustom/templates");
 		putAll(new GedartTemplates(gedartDir));
 
-// exists?
+		// then templates in {user.home.genj}/gedart/templates
 		File dir = new File(EnvironmentChecker.getProperty(this,
 				"user.home.genj/gedart/templates", "?",
 				"Looking for gedart/templates"));
@@ -30,12 +32,10 @@ class GedartTemplates extends TreeMap<String, GedartTemplate> {
 
 	public GedartTemplates(File dir) {
 		if (dir.isDirectory()) {
-
 			// loop over Templatess
 			File[] files = dir.listFiles();
 			for (int b = 0; b < files.length; b++) {
-				File file = files[b];
-				GedartTemplate t = GedartTemplate.create(file);
+				GedartTemplate t = GedartTemplate.create(files[b]);
 				if (t == null)
 					continue;
 				put(t.getName(),t);
