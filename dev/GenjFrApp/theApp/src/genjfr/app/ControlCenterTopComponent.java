1650c086996dbeb9e21ee8312ce689dfc7e30f53
dandre <dandre@ancestris.org>
2010-04-06 22:26:05 +0000

    correction de #108: maintenant on peut détacher la fenêtre du CC sans avoir la possibilité de le fermer

--- dev/GenjFrApp/theApp/src/genjfr/app/ControlCenterTopComponent.java
+++ dev/GenjFrApp/theApp/src/genjfr/app/ControlCenterTopComponent.java
@@ -37,10 +37,12 @@ public final class ControlCenterTopComponent extends GenjViewTopComponent {
 //	putClientProperty(TopComponent.PROP_DRAGGING_DISABLED, Boolean.TRUE);
 //	putClientProperty(TopComponent.PROP_MAXIMIZATION_DISABLED, Boolean.TRUE);
 //	putClientProperty(TopComponent.PROP_SLIDING_DISABLED, Boolean.TRUE);
-	putClientProperty(TopComponent.PROP_UNDOCKING_DISABLED, Boolean.TRUE);
+//	putClientProperty(TopComponent.PROP_UNDOCKING_DISABLED, Boolean.TRUE);
 
     }
 
+    @Override
+    public boolean canClose(){return  false;}
 
     @Override
     String getDefaultFactoryMode() {return "genjfr-explorer";}
