dc7596ac4e216d39eda35aafe86ae4c447567489
dandre <dandre@ancestris.org>
2010-04-11 23:21:50 +0000

    envoi du répertoire userdir par mail (une partie uniquement !)

--- dev/GenjFrApp/FeedBack/src/genjfr/app/sendfeedback/SendAction.java
+++ dev/GenjFrApp/FeedBack/src/genjfr/app/sendfeedback/SendAction.java
@@ -12,15 +12,14 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.prefs.Preferences;
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.MultiPartEmail;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
@@ -33,18 +32,18 @@ public final class SendAction implements ActionListener {
     private final static String TEXTSEPARATOR = "\n=======================================\n";
     private final static String SEND = new String(NbBundle.getMessage(SendAction.class, "SEND_BUTTON"));
     private Preferences feedbackPrefs = NbPreferences.forModule(FeedbackPanel.class);
+    private File userDir;
+    private FeedbackPanel fbPanel;
 
-    GenjLogFile logFile = new GenjLogFile();
-    FeedbackPanel fbPanel = new FeedbackPanel(logFile.getSize());
-
-    private void setDefaultValues(FeedbackPanel panel){
+    private void setDefaultValues(FeedbackPanel panel) {
         panel.jtaText.setText(getSystemInfo());
         panel.jtEmailTo.setText(RESOURCES.getString("fb.mailto.default"));
-        panel.jtMailHost.setText(feedbackPrefs.get("mail.host",System.getProperty("mail.host")));
-        panel.jtName.setText(feedbackPrefs.get("mail.name",""));
-        panel.jtEmail.setText(feedbackPrefs.get("mail.address",""));
+        panel.jtMailHost.setText(feedbackPrefs.get("mail.host", System.getProperty("mail.host")));
+        panel.jtName.setText(feedbackPrefs.get("mail.name", ""));
+        panel.jtEmail.setText(feedbackPrefs.get("mail.address", ""));
     }
-    private void saveDefaultValues(FeedbackPanel panel){
+
+    private void saveDefaultValues(FeedbackPanel panel) {
         String s = panel.jtMailHost.getText();
         if (!s.equalsIgnoreCase(System.getProperty("mail.host"))) {
             feedbackPrefs.put("mail.host", s);
@@ -54,14 +53,26 @@ public final class SendAction implements ActionListener {
         feedbackPrefs.put("mail.address", panel.jtEmail.getText().trim());
 
     }
+
     public void actionPerformed(ActionEvent e) {
+
+        try {
+            userDir = sendUserDir();
+        } catch (Exception ex) {
+            NotifyDescriptor nd = new NotifyDescriptor.Message(RESOURCES.getString("fb.msg.senderror")
+                    + "\n(" + ex.getMessage() + ").", NotifyDescriptor.ERROR_MESSAGE);
+            DialogDisplayer.getDefault().notify(nd);
+        }
+
+        fbPanel = new FeedbackPanel(userDir.length());
+
         setDefaultValues(fbPanel);
 
-        DialogDescriptor dd = new DialogDescriptor(fbPanel,NbBundle.getMessage(this.getClass(), "FeedbackPanel.title"));
+        DialogDescriptor dd = new DialogDescriptor(fbPanel, NbBundle.getMessage(this.getClass(), "FeedbackPanel.title"));
         dd.setOptions(new Object[]{new String(SEND), DialogDescriptor.CANCEL_OPTION});
         DialogDisplayer.getDefault().createDialog(dd);
         DialogDisplayer.getDefault().notify(dd);
-        if (dd.getValue().equals(SEND)){
+        if (dd.getValue().equals(SEND)) {
             // on sauvegarde qq valeurs par defaut
             saveDefaultValues(fbPanel);
             System.setProperty("mail.host", fbPanel.jtMailHost.getText());
@@ -70,96 +81,105 @@ public final class SendAction implements ActionListener {
         }
     }
 
-	public static String getSystemInfo() {
-		Properties p = System.getProperties();
-		StringBuffer sb = new StringBuffer();
-		sb.append("GenjFr ");
-		sb.append(Version.getInstance().getBuildString());
-		sb.append(" (");
-		sb.append(Locale.getDefault());
-		sb.append(")\nOS : ");
-		sb.append(p.get("os.name"));
-		sb.append(" ");
-		sb.append(p.get("os.version"));
-		sb.append(" (");
-		sb.append(p.get("os.arch"));
-		sb.append(")\nJRE: ");
-		sb.append(p.get("java.vendor"));
-		sb.append(" ");
-		sb.append(p.get("java.version"));
-		sb.append("\n");
-		sb.append(TEXTSEPARATOR + RESOURCES.getString("fb.text.comment"));
-		sb.append("\n");
-		return sb.toString();
-	}
+    public static String getSystemInfo() {
+        Properties p = System.getProperties();
+        StringBuffer sb = new StringBuffer();
+        sb.append("GenjFr ");
+        sb.append(Version.getInstance().getBuildString());
+        sb.append(" (");
+        sb.append(Locale.getDefault());
+        sb.append(")\nOS : ");
+        sb.append(p.get("os.name"));
+        sb.append(" ");
+        sb.append(p.get("os.version"));
+        sb.append(" (");
+        sb.append(p.get("os.arch"));
+        sb.append(")\nJRE: ");
+        sb.append(p.get("java.vendor"));
+        sb.append(" ");
+        sb.append(p.get("java.version"));
+        sb.append("\n");
+        sb.append(TEXTSEPARATOR + RESOURCES.getString("fb.text.comment"));
+        sb.append("\n");
+        return sb.toString();
+    }
 
-    	private class SendWorker implements Runnable {
+    private class SendWorker implements Runnable {
 
-		public void run() {
+        public void run() {
 //			acSend.setEnabled(false);
 //			acCancel.setEnabled(false);
-                    String TS = new SimpleDateFormat("yyMMdd-HHmm").format(new Date());
-			String subject = "["+RESOURCES.getString("fb.tag.subject")+" "+TS+"] ";
-                        subject += fbPanel.jtSubject.getText().trim();
-			String name = fbPanel.jtName.getText().trim();
-			String email = fbPanel.jtEmail.getText().trim();
-			String msg = fbPanel.jtaText.getText().trim();
 
-			if (fbPanel.jcbIncGenjLog.isSelected() && logFile.getSize() > 0) {
-				msg += "\n" + TEXTSEPARATOR + "genj.log:\n"
-						+ logFile.getContent();
-			}
+            String TS = new SimpleDateFormat("yyMMdd-HHmm").format(new Date());
+            String subject = "[" + RESOURCES.getString("fb.tag.subject") + " " + TS + "] ";
+            subject += fbPanel.jtSubject.getText().trim();
+            String name = fbPanel.jtName.getText().trim();
+            String from = fbPanel.jtEmail.getText().trim();
+            String msg = fbPanel.jtaText.getText().trim();
 
-			boolean failed = false;
-			String response = "";
 
-			String to = fbPanel.jtEmailTo.getText().trim();
-			// Establish a network connection for sending mail
-			try {
-				URL u = new URL("mailto:" + to); // Create a mailto: URL
-                                // TODO: utiliser MailToURLConnection
-				URLConnection c = u.openConnection(); // Create a
-                                					// URLConnection for it
-				c.setDoInput(false); // Specify no input from this URL
-				c.setDoOutput(true); // Specify we'll do output
-				c.connect(); // Connect to mail host
-				PrintWriter out = // Get output stream to mail host
-				new PrintWriter(new OutputStreamWriter(c.getOutputStream()));
+            boolean failed = false;
+            String response = "";
 
-				// Write out mail headers. Don't let users fake the From address
-				out.println("From: \"" + name + "\" <" + email + ">");
-				out.println("To: " + to);
-				out.println("Subject: " + subject);
-				out.println("Content-Type: text/plain; charset="+
-						System.getProperties().get("file.encoding")+
-						"; format=flowed");
-				out.println("Content-Transfer-Encoding: 8bit");
-				out.println(); // blank line to end the list of headers
+            String to = fbPanel.jtEmailTo.getText().trim();
 
-				out.println(msg);
-				out.close();
+            try {
 
-			} catch (Exception e) {
-				response = e.getMessage();
-				failed = true;
-			}
+                EmailAttachment attachment = null;
+                new EmailAttachment();
+                if (fbPanel.jcbIncGenjLog.isSelected()) {
+                    attachment = new EmailAttachment();
+                    attachment.setURL(sendUserDir().toURI().toURL());
+                    attachment.setDisposition(EmailAttachment.ATTACHMENT);
+                    attachment.setDescription("GenjFr User Dir");
+                    attachment.setName("userdir.zip");
+                }
 
-			if (failed) {
-                            NotifyDescriptor nd = new NotifyDescriptor.Message(RESOURCES
-						.getString("fb.msg.senderror")
-                                                + "\n(" + response + ")."
-                                                , NotifyDescriptor.ERROR_MESSAGE);
-                            DialogDisplayer.getDefault().notify(nd);
+                // Create the email message
+                MultiPartEmail email = new MultiPartEmail();
 
-			} else {
-                            NotifyDescriptor nd = new NotifyDescriptor.Message(RESOURCES
-						.getString("fb.msg.thankyou")
-                                                , NotifyDescriptor.INFORMATION_MESSAGE);
-                            DialogDisplayer.getDefault().notify(nd);
-			}
 
-		}
-	}
+                email.addTo(to);
+                email.setFrom(from, name);
+                email.setSubject(subject);
+                email.setMsg(msg);
+                email.setHostName(fbPanel.jtMailHost.getText());
+                if (attachment != null) {
+                    email.attach(attachment);
+                }
+                email.send();
+            } catch (Exception e) {
+                response = e.getMessage();
+                genj.app.App.LOG.info("" + e);
+                failed = true;
+            }
+
+            if (failed) {
+                NotifyDescriptor nd = new NotifyDescriptor.Message(RESOURCES.getString("fb.msg.senderror")
+                        + "\n(" + response + ").", NotifyDescriptor.ERROR_MESSAGE);
+                DialogDisplayer.getDefault().notify(nd);
+
+            } else {
+                NotifyDescriptor nd = new NotifyDescriptor.Message(RESOURCES.getString("fb.msg.thankyou"), NotifyDescriptor.INFORMATION_MESSAGE);
+                DialogDisplayer.getDefault().notify(nd);
+            }
+
+        }
+    }
+
+    private File sendUserDir() throws IOException {
+        String baseDir = System.getProperty("netbeans.user");
+        Zipper zipUD = new Zipper(new File(baseDir));
+        zipUD.addIncludePatterns("config/Preferences/genjfr/.*");
+        zipUD.addIncludePatterns("config/Toolbars/.*");
+        zipUD.addIncludePatterns("config/Windows2Local/.*");
+        zipUD.addIncludePatterns("genjfr/.*");
+        zipUD.addIncludePatterns("config/Preferences/org/.*");
+        zipUD.addExcludePatterns("genjfr/.*\\.lck");
+        File temp = File.createTempFile("genjfr", ".zip");
+        zipUD.doExport(temp);
+        return temp;
+    }
 
     private class GenjLogFile {
 
