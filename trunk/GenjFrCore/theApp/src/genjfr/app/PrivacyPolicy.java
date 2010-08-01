569c7303b0561acba0ce66f1b4dc4c1e8e6441fe
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-08-01 13:57:44 +0000

    Privacy - Refonte de la politique de privacy pour le nombre d'années dans le module genjfr.

--- trunk/GenjFrCore/theApp/src/genjfr/app/PrivacyPolicy.java
+++ trunk/GenjFrCore/theApp/src/genjfr/app/PrivacyPolicy.java
@@ -9,12 +9,19 @@ import genj.gedcom.Fam;
 import genj.gedcom.Indi;
 import genj.gedcom.Property;
 import genj.gedcom.PropertyDate;
+import genj.gedcom.PropertyXRef;
 import genj.gedcom.time.Delta;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
- * A filtering scheme applying privacy to Gedcom properties overiding genjfr one
+ * A filtering scheme applying privacy to Gedcom properties
+ *
  * @author frederic using Genj base code
  *
  * La loi du 6 janvier 1978 définit les données à caractère personnel comme
@@ -37,30 +44,62 @@ import java.util.TreeMap;
  * compris les décisions de grâce, pour les minutes et répertoires des
  * notaires ainsi que pour les registres de l'état civil et de l'enregistrement
  * (…) ».
+ *
+ *
+ * Loi n°2008-696 du 15 juillet 2008 relative aux archives : une adaptation
+ * aux exigences actuelles de transparence et de simplification du droit
+ * Cette loi adoptée après un périple législatif de près de deux ans, adapte
+ * une législation inchangée depuis une trentaine d’années (loi n°79-18 du 
+ * 3 janvier 1979) aux exigences actuelles de transparence de l'administration
+ * et de simplification du droit. Au travers des différentes mesures exposées,
+ * la loi aligne par touches successives le régime des archives publiques sur
+ * celui des documents administratifs (loi n° 78-753 du 17 juillet 1978).
+ *
+ * FL : d'après cet alignement, je pourrais simplifier la compréhension comme
+ * suit:
+ * Les vivants:
+ * - 125 ans pour les dossiers médicaux
+ * - 100 ans pour les informations civiles des mineurs
+ * - 75 ans pour les informations civiles des non mineurs
+ * - 50 ans pour les données privées
+ * Les décédés:
+ * - 25 ans après le décès
+ *
+ * En terme de programmation, ces règles sont plutôt ambigües à mettre en oeuvre 
+ * aussi je préfère reporter la responsabilité sur l'utilisateur en lui donnant
+ * les règles ci-dessous, règles qui me paraissent à la fois traiter les cas
+ * généraux et les exceptions plus ciblées.
  * 
+ *
  */
 public class PrivacyPolicy {
 
-    // GENERAL PRIVACY RULE 1 (GP1) : Any information (property) dated within the most recent n years is private.
-    // GP1-1 : All information, regardless of the date, rolling up to a private information, including linked SOUR, REPO, NOTE, MEDIA, is considered private
-    //      (e.g. if a SOUR entity or a MEDIA supports a private birth, it is also private, even if this SOUR or MEDIA has no date or is not marked private itself.
-    //      (this is to protect sources documents for instance where the date is not an info of SOUR but still is 
-    //      (somewhere in the document, the picture, etc.
-    // GP1-2 : All 'entities' a private info belongs to, regardless of their other dates, are also considered private (this re-inforces protection by default)
+    // GENERAL PRIVACY RULE 1 (GP1) : "privacy years property"
+    // GP1-1 : Any information (=property) dated within the most recent n years is private.
+    //      For this we need to define the privacy date of a property : it will be the most recent date of any "DATE" property underneath that property
+    // GP1-2 : Any 'entity' father of a private years property is also a private years property
     //      (for instance, a person whose death is private, is private, even if its birth says it is not.
-    //      (No info of a private entity can be displayed, including its SOUR, REPO, NOTE, MEDIA contained in it
+    //      (this re-inforces protection by default)
+    // GP1-3 : Any 'entity' SOUR, REPO, NOTE, MEDIA, regardless of the date, child of a private years property, is considered a private years property
+    //      (this does not apply to INDI nor FAM entities.
+    //      (e.g. if a SOUR entity or a MEDIA supports a private birth, it is also private, even if this SOUR or MEDIA has no date or is not marked private itself.
+    //      (this is to protect sources documents for instance where the date is not an info of SOUR but still is
+    //      (somewhere in the document, the picture, etc.
     private int yearsInfoIsPrivate;
     //
-    // GENERAL PRIVACY RULE 2 (GP2) : Alive persons are private
+    // GENERAL PRIVACY RULE 2 (GP2) : "alive persons"
+    // GP2 : Alive persons are private years entity
     // All alive person (or supposed alive : no death event and birth < 120 years) is private, even though the rule of "n" years does not capture it as private
     // All info of a private personne is considered private (applying GR1-1).
     private boolean infoOfAliveIsPrivate;
     //
-    // LOCAL PRIVACY RULE (LP1) : All individuals or information *marked* private is private along with information held "below" such mark
-    // As a result, information held "above" such mark is not private, therefore is public (GR1-2 not applied here)
+    // LOCAL PRIVACY RULE (LP1) : "privacy marked property"
+    // All individuals or information *marked* private is private along with information held "below" such mark
+    // As a result, information held "above" such mark is not private marked, therefore is public
     private String tagMarkingPrivate;
     //
-    //  LOCAL PRIVACY RULE (LP2) : certain events or tags are considered private ; this does not imply that corresponding individuals are private
+    //  LOCAL PRIVACY RULE (LP2) : "private events"
+    // Certain events or tags are considered private ; this does not imply that corresponding individuals are private
     // Events/tags to be selected by preferences (using a map between property tag and string value "contained"
     // If string is "*" then all such tags are considered private
     // For instance:
@@ -71,6 +110,10 @@ public class PrivacyPolicy {
     //
     // EXCEPTION RULE (ER1) : All information of deceased is public, even though any of the above rules says it should be private
     private boolean infoOfDeceasedIsPublic;
+    //
+    //
+    // For performance reasons, keep track of private years entities
+    private Set<Entity> privateYearsEntities = new HashSet<Entity>();
 
     /**
      * Constructor
@@ -83,9 +126,9 @@ public class PrivacyPolicy {
         this.infoOfDeceasedIsPublic = infoOfDeceasedIsPublic;
     }
 
-    /**
+    /***************************************************************************
      * General privacy checker
-     */
+     **************************************************************************/
     public boolean isPrivate(Property prop) {
 
         // treat exception first : not private if property belongs to deceased
@@ -108,20 +151,114 @@ public class PrivacyPolicy {
             return true;
         }
 
-        // is prop tagged?
+        // is property tagged as private?
         if (tagMarkingPrivate != null && hasTagMarkingPrivate(prop)) {
             return true;
         }
 
-// FL: supprimé d'ici car doit être placé au niveau de certains tests élémentaires
-// (en effet: si on test la naissance, la personne peut ne pas avoir l'air privée sur la règle des années
-// alors qu'elle le serait sur le décès). Il faut donc ne pas se contenter de remonter l'arbre des propriétés.
-//        // is parent property is private?
-//        prop = prop.getParent();
-//        if (prop != null) {
-//            return isPrivate(prop);
-//        }
+        // FL: code ci-dessous supprimé d'ici car doit plutôt être placé au niveau de certains tests élémentaires
+        // (en effet: si on test la naissance, la personne peut ne pas avoir l'air privée sur la règle des années
+        // alors qu'elle le serait sur le décès). Il faut donc ne pas se contenter de remonter l'arbre des propriétés.
+        //        // is parent property is private?
+        //        prop = prop.getParent();
+        //        if (prop != null) {
+        //            return isPrivate(prop);
+        //        }
         return false;
+    }
+
+    /***************************************************************************
+     * Elemental checkers
+     **************************************************************************/
+    //
+    //
+    //
+    /**
+     * Elemental checker :
+     * Check whether this property is a "private years property"
+     * Returns true (private years) if one of two cases:
+     * - prop belongs to an INDI or FAM which has a private years date
+     * - prop does not belong to INDI nor FAM, and it is related to an entity
+     *   INDI or FAM which has a private years date
+     * Store privateYears entity in set as we go to improve performance
+     */
+    @SuppressWarnings("unchecked")
+    private boolean isWithinPrivateYears(Property prop) {
+        Entity ent = prop.getEntity();
+
+        // check if entity has already been considered as private before
+        if (privateYearsEntities.contains(ent)) {
+            return true;
+        }
+
+        if ((ent instanceof Indi) || (ent instanceof Fam)) {
+            if (hasPrivateYearsDate(ent)) {
+                privateYearsEntities.add(ent);
+                return true;
+            }
+        } else {
+            List<Entity> list = new ArrayList<Entity>();
+            List<PropertyXRef> refProps = ent.getProperties(PropertyXRef.class);
+            for (Iterator<PropertyXRef> it = refProps.iterator(); it.hasNext();) {
+                PropertyXRef propertyXRef = it.next();
+                Entity target = propertyXRef.getTargetEntity();
+                if (list.contains(target)) {
+                    continue;
+                }
+                if ((target instanceof Indi) || (target instanceof Fam)) {
+                    list.add(target);
+                }
+            }
+            if (!list.isEmpty()) {
+                for (Iterator<Entity> it = list.iterator(); it.hasNext();) {
+                    Entity entity = it.next();
+                    if (privateYearsEntities.contains(entity)) {
+                        return true;
+                    }
+                    if (hasPrivateYearsDate(entity)) {
+                        privateYearsEntities.add(entity);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Elemental checker :
+     * Check whether a prop belongs to a person alive or supposed alive : no death event and birth < xxx years
+     */
+    private boolean isInfoOfAlive(Property prop) {
+        return false;
+    }
+
+    /**
+     * Elemental checker :
+     * Check for marked with tag
+     */
+    private boolean isPrivateEvent(Property prop) {
+        return false;
+        // TODO: à ajouter
+        //        // is parent property is private?
+        //        prop = prop.getParent();
+        //        if (prop != null) {
+        //            return isPrivate(prop);
+        //        }
+    }
+
+    /**
+     * Elemental checker :
+     * Check for marked with tag
+     */
+    private boolean hasTagMarkingPrivate(Property prop) {
+        return getPropertyFor(prop, tagMarkingPrivate, Property.class) != null;
+        // TODO: à ajouter
+        //        // is parent property is private?
+        //        prop = prop.getParent();
+        //        if (prop != null) {
+        //            return isPrivate(prop);
+        //        }
     }
 
     /**
@@ -149,13 +286,22 @@ public class PrivacyPolicy {
         return false;
     }
 
-    /**
-     * Elemental checker :
-     * Check whether a prop is still within the privat years' - only if it has a date sub-property
-     */
-    private boolean isWithinPrivateYears(Property prop) {
-        // check date
-        PropertyDate date = (PropertyDate) getPropertyFor(prop, "DATE", PropertyDate.class);
+    /***************************************************************************
+     * Supporting methods follow
+     **************************************************************************/
+    @SuppressWarnings("unchecked")
+    private boolean hasPrivateYearsDate(Property prop) {
+        List<PropertyDate> dateProps = prop.getProperties(PropertyDate.class);
+        for (Iterator<PropertyDate> it = dateProps.iterator(); it.hasNext();) {
+            PropertyDate propertyDate = it.next();
+            if (isDatePrivate(propertyDate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDatePrivate(PropertyDate date) {
         if (date == null) {
             return false;
         }
@@ -164,62 +310,20 @@ public class PrivacyPolicy {
         return anniversary != null && anniversary.getYears() < yearsInfoIsPrivate;
     }
 
-    /**
-     * Elemental checker :
-     * Check whether a prop belongs to a person alive or supposed alive : no death event and birth < 120 years
-     */
-    private boolean isInfoOfAlive(Property prop) {
-        return false;
-    }
-
-    /**
-     * Elemental checker :
-     * Check for marked with tag
-     */
-    private boolean isPrivateEvent(Property prop) {
-        return false;
-// TODO: à ajouter
-//        // is parent property is private?
-//        prop = prop.getParent();
-//        if (prop != null) {
-//            return isPrivate(prop);
-//        }
-    }
-
-    /**
-     * Elemental checker :
-     * Check for marked with tag
-     */
-    private boolean hasTagMarkingPrivate(Property prop) {
-        return getPropertyFor(prop, tagMarkingPrivate, Property.class) != null;
-// TODO: à ajouter
-//        // is parent property is private?
-//        prop = prop.getParent();
-//        if (prop != null) {
-//            return isPrivate(prop);
-//        }
-    }
-
-    /**
-     * Supportting methods folow
-     */
-    /**
-     * filter a value
-     */
     // Find a sub-property by tag and type
+    @SuppressWarnings("unchecked")
     private Property getPropertyFor(Property prop, String tag, Class type) {
         // check children
         for (int i = 0, j = prop.getNoOfProperties(); i < j; i++) {
             Property child = prop.getProperty(i);
-            if (is(child, tag, type)) {
+            if (child.getTag().equals(tag) && type.isAssignableFrom(child.getClass())) {
                 return child;
             }
         }
         return null;
     }
 
-    @SuppressWarnings("unchecked")
-    private boolean is(Property prop, String tag, Class type) {
-        return prop.getTag().equals(tag) && type.isAssignableFrom(prop.getClass());
+    public Set<Entity> getCurrentPrivateEntities() {
+        return privateYearsEntities;
     }
 }
