e04d524b654743382b04b2f5df6044569c56d2c3
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-09-20 20:57:42 +0000

    correction d'affichage des menu

--- branches/dan-dev-0.5/GenjFrCore/theApp/src/genjfr/util/MyActionPresenter.java
+++ branches/dan-dev-0.5/GenjFrCore/theApp/src/genjfr/util/MyActionPresenter.java
@@ -21,12 +21,14 @@ package genjfr.util;
 
 import genj.util.swing.MenuHelper;
 import java.awt.Component;
+import java.util.Collection;
 
 
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import org.openide.util.Lookup;
 import org.openide.util.actions.ActionPresenterProvider;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -35,12 +37,21 @@ import org.openide.util.lookup.ServiceProvider;
  */
 @ServiceProvider(service = ActionPresenterProvider.class, position = 1)
 public class MyActionPresenter extends ActionPresenterProvider {
-
+    
+    private ActionPresenterProvider getOther(){
+       Collection<? extends ActionPresenterProvider> a = Lookup.getDefault().lookup(new Lookup.Template(ActionPresenterProvider.class)).allInstances();
+       for (ActionPresenterProvider app: a) {
+           if (app != this) {
+               return app;
+           }
+       }
+       return new Default();
+    }
     /**
      * Creates a PopupMenu
      */
     public JPopupMenu createEmptyPopup() {
-        return new JPopupMenu();
+        return getOther().createEmptyPopup();
     }
 
     public final JMenuItem createPopupPresenter(Action action) {
@@ -51,15 +62,43 @@ public class MyActionPresenter extends ActionPresenterProvider {
     }
 
     public JMenuItem createMenuPresenter(Action action) {
-        return new JMenuItem(action);
+        return getOther().createMenuPresenter(action);
     }
 
     public Component createToolbarPresenter(Action action) {
-        return new JButton(action);
+        return getOther().createToolbarPresenter(action);
     }
 
     public Component[] convertComponents(Component comp) {
-        return new Component[]{comp};
+        return getOther().convertComponents(comp);
     }
-} //MenuHelper
+
+    //
+    // Default implementation of the the presenter
+    //
+
+    private static final class Default extends ActionPresenterProvider {
+
+        public JMenuItem createMenuPresenter(Action action) {
+            return new JMenuItem(action);
+        }
+
+        public JMenuItem createPopupPresenter(Action action) {
+            return new JMenuItem(action);
+        }
+
+        public Component createToolbarPresenter(Action action) {
+            return new JButton(action);
+        }
+
+        public JPopupMenu createEmptyPopup() {
+            return new JPopupMenu();
+        }
+
+        public Component[] convertComponents(Component comp) {
+            return new Component[] {comp};
+        }
+    }
+
+} 
 
