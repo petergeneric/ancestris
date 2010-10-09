04d4d24e61a80b960946025f75b827b593059d79
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-10-09 07:38:28 +0000

    Editeur standard : ajout du genjfrplugin pour que l'etideur apparaissent dans le A Propos d'Ancestris.

---
+++ branches/dan-dev-0.5/GenjFrCore/EditorStd/src/genjfr/app/editorstd/EditorStdPlugin.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package genjfr.app.editorstd;
+
+import genjfr.app.pluginservice.GenjFrPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author frederic
+ */
+@ServiceProvider(service=genjfr.app.pluginservice.PluginInterface.class)
+public class EditorStdPlugin extends GenjFrPlugin{
+
+}
