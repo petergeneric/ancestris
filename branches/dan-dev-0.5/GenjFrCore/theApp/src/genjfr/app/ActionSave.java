d55f4eec337232685350a7d87e97801460b51ea5
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-10-10 17:04:58 +0000

    Fix d'un bug à sauvegarde si aucun gedcom sélectionné.

--- branches/dan-dev-0.5/GenjFrCore/theApp/src/genjfr/app/ActionSave.java
+++ branches/dan-dev-0.5/GenjFrCore/theApp/src/genjfr/app/ActionSave.java
@@ -2,33 +2,37 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package genjfr.app;
 
 import genj.app.Images;
+import genj.gedcom.Context;
 import genj.gedcom.Gedcom;
 import genj.util.Resources;
 import genj.util.swing.Action2;
 import java.awt.event.ActionEvent;
 
-  /**
-   * Action - Save
-   */
-  public class ActionSave extends Action2 {
+/**
+ * Action - Save
+ */
+public class ActionSave extends Action2 {
+
     /** gedcom */
     protected Gedcom gedcomBeingSaved;
     private Resources RES = Resources.get("genj.app");
 
-    public ActionSave(){
+    public ActionSave() {
         setText(RES.getString("cc.menu.save"));
-      setTip(RES, "cc.tip.save_file");
-      // setup
-      setImage(Images.imgSave);
+        setTip(RES, "cc.tip.save_file");
+        // setup
+        setImage(Images.imgSave);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        App.workbenchHelper.saveGedcom(App.center.getSelectedContext(false));
+        Context context = App.center.getSelectedContext(false);
+        if (context != null)  {
+            App.workbenchHelper.saveGedcom(context);
+        }
     }
+} // ActionSave
 
-  } // ActionSave
