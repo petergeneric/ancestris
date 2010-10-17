fd54bb17db90aab957d09cc1fc327b6bfa3d12f4
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-10-17 17:46:53 +0000

    Changement du message EditeurStd à fermeture

--- branches/dan-dev-0.5/GenjFrCore/EditorStd/src/genjfr/app/editorstd/EditorStdTopComponent.java
+++ branches/dan-dev-0.5/GenjFrCore/EditorStd/src/genjfr/app/editorstd/EditorStdTopComponent.java
@@ -201,11 +201,12 @@ public final class EditorStdTopComponent extends AncestrisTopComponent implement
         if (!canClose) {
             NotifyDescriptor d = new NotifyDescriptor.Confirmation(NbBundle.getMessage(SubmitterPanel.class, "CTL_EditionUnsaved", getGedcom().getName()),
                     NbBundle.getMessage(SubmitterPanel.class, "CTL_AskConfirmation"),
-                    NotifyDescriptor.YES_NO_CANCEL_OPTION);
+                    NotifyDescriptor.YES_NO_OPTION);
             Object ret = DialogDisplayer.getDefault().notify(d);
-            if (ret.equals(NotifyDescriptor.CANCEL_OPTION)) {
-                return false;
-            }
+//            Would be used to use cancel as well but too complex to intercept at application closure
+//            if (ret.equals(NotifyDescriptor.CANCEL_OPTION)) {
+//                return false;
+//            }
             if (ret.equals(NotifyDescriptor.OK_OPTION)) {
                 saveEditor();
             }
