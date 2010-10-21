4d665ef49e1f001cf8b7469c6d1fdb77b2a9fe57
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-10-21 11:48:43 +0000

    genjcore modifé

--- branches/dan-dev-0.5/GenjFrCore/GenjCore/src/genjfr/genjcore/GenjCorePlugin.java
+++ branches/dan-dev-0.5/GenjFrCore/GenjCore/src/genjfr/genjcore/GenjCorePlugin.java
@@ -11,7 +11,7 @@ import org.openide.util.lookup.ServiceProvider;
 /**
  *
  * @author daniel
- * modification pour forcer la MAJ de genjcore 20101020
+ * modification pour forcer la MAJ de genjcore 20101021
  */
 @ServiceProvider(service=genjfr.app.pluginservice.PluginInterface.class)
 public class GenjCorePlugin extends GenjFrPlugin{
