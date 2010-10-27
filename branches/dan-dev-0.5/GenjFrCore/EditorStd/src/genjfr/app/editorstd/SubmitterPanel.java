b069adf46d730caec77c8402637518a180b800e0
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2010-10-27 22:40:12 +0000

    Editeur standard : force remontée de certains fichiers... Aucune modif à part ça.

--- branches/dan-dev-0.5/GenjFrCore/EditorStd/src/genjfr/app/editorstd/SubmitterPanel.java
+++ branches/dan-dev-0.5/GenjFrCore/EditorStd/src/genjfr/app/editorstd/SubmitterPanel.java
@@ -31,6 +31,7 @@ public class SubmitterPanel extends EntityPanel {
         initComponents();
     }
 
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
