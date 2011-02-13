862df8c80863630a6374bb4e63e978fa676aa853
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-02-13 22:01:29 +0000

    Add MLE Ancestris Bean

--- branches/dan-dev-0.5/GenjFrCore/ancestris-beans/src/ancestris/modules/beans/AMLEBean.java
+++ branches/dan-dev-0.5/GenjFrCore/ancestris-beans/src/ancestris/modules/beans/AMLEBean.java
@@ -12,16 +12,46 @@
 package ancestris.modules.beans;
 
 import genj.edit.beans.MLEBean;
-import java.io.Serializable;
+import genj.gedcom.Property;
+import genj.gedcom.PropertyMultilineValue;
 
 /**
  *
  * @author daniel
  */
-public class AMLEBean extends MLEBean implements Serializable {
+public class AMLEBean extends MLEBean implements ABean {
 
     public AMLEBean() {
         super();
     }
 
+    private String tag = "";
+
+    /**
+     * Get the value of tag
+     *
+     * @return the value of tag
+     */
+    public String getTag() {
+        return tag;
+    }
+
+    /**
+     * Set the value of tag
+     *
+     * @param tag new value of tag
+     */
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * set root gedcom property for this bean
+     * @param property
+     */
+    @Override
+    public AMLEBean setRoot(Property root) {
+        super.setContext(root, tag);
+        return this;
+    }
 }
