3e1bbf9408a7c22a5947a96b04bec89c8d55a0e3
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-04-20 17:52:33 +0000

    première version du plugin export

---
+++ branches/dan-dev-0.5/GenjFrExtensions/GeneanetExport/src/org/ancetris/geneanetexport/GeneanetExportAction.java
@@ -0,0 +1,10 @@
+package org.ancetris.geneanetexport;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public final class GeneanetExportAction implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+       // TODO implement action body
+    }
+}
