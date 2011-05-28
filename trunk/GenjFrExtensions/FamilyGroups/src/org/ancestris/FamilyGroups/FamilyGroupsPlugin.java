175fce208e8d3659a3b4381de056cea2ac4e5806
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-05-28 16:54:29 +0000

    Add Pluginservice Interface implementation

---
+++ trunk/GenjFrExtensions/FamilyGroups/src/org/ancestris/FamilyGroups/FamilyGroupsPlugin.java
@@ -0,0 +1,13 @@
+package org.ancestris.FamilyGroups;
+
+import genjfr.app.pluginservice.GenjFrPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author daniel
+ */
+@ServiceProvider(service=genjfr.app.pluginservice.PluginInterface.class)
+public class FamilyGroupsPlugin extends GenjFrPlugin{
+
+}
