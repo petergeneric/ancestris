de92157e6bc4ef09488f40d718ca3e4a8f24981d
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-05-28 17:22:44 +0000

    Import du plugin  Export Cousin GenWeb suite à la séparaton du plugin ancestris export en 2 plugins Export Geneanet et Export Cousin GenWeb

---
+++ trunk/GenjFrExtensions/ExportCousinsGenWeb/src/org/ancestris/ancestrisexports/cousinsgenweb/CousinsGenWebPlugin.java
@@ -0,0 +1,13 @@
+package org.ancestris.ancestrisexports.cousinsgenweb;
+
+import genjfr.app.pluginservice.GenjFrPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author daniel
+ */
+@ServiceProvider(service=genjfr.app.pluginservice.PluginInterface.class)
+public class CousinsGenWebPlugin extends GenjFrPlugin{
+
+}
