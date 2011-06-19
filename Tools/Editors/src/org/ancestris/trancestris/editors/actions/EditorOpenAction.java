4092cea3c569c196184523d81738455b8fb993c9
lemovice <lemovice@ancestris.org>
2011-06-19 11:46:33 +0000

    Modify Open File logic

--- Tools/Editors/src/org/ancestris/trancestris/editors/actions/EditorOpenAction.java
+++ Tools/Editors/src/org/ancestris/trancestris/editors/actions/EditorOpenAction.java
@@ -1,47 +1,62 @@
 package org.ancestris.trancestris.editors.actions;
 
+import java.awt.Dialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import org.ancestris.trancestris.editors.EditorTopComponent;
 import org.ancestris.trancestris.resources.ResourceFile;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
 public final class EditorOpenAction implements ActionListener {
 
+    File defaultBundleFile = null;
+    Locale selectedLocale = null;
+    EditorOpenActionPanel editorOpenActionPanel = new EditorOpenActionPanel();
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        final FileNameExtensionFilter filter = new FileNameExtensionFilter("Properties files", "properties");
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileFilter(filter);
-        fileChooser.setAcceptAllFileFilterUsed(false);
-        if (fileChooser.showOpenDialog(WindowManager.getDefault().getMainWindow()) == JFileChooser.APPROVE_OPTION) {
-            Locale defaultLocale = new java.util.Locale("es", "ES");
-            File defaultBundleFile = fileChooser.getSelectedFile();
+
+        class EditorOpenActionPanelActionListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                defaultBundleFile = editorOpenActionPanel.defaultBundleFile;
+                selectedLocale = editorOpenActionPanel.selectedLocale;
+            }
+        }
+        DialogDescriptor GeneanetExportActionDescriptor = new DialogDescriptor(
+                editorOpenActionPanel,
+                NbBundle.getMessage(EditorOpenActionPanel.class, "CTL_EditorOpenAction"),
+                true,
+                new EditorOpenActionPanelActionListener());
+
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(GeneanetExportActionDescriptor);
+        dialog.setVisible(true);
+        dialog.toFront();
+        if (GeneanetExportActionDescriptor.getValue() == DialogDescriptor.OK_OPTION) {
+
             String defaultBundleFileName = defaultBundleFile.getName();
             int extensionIndex = defaultBundleFileName.lastIndexOf(".");
-            String translatedBundleFileName = "";
             try {
                 ResourceFile resourceFile = new ResourceFile(defaultBundleFile);
-                /*
-                 * 
-                 */
-                translatedBundleFileName = fileChooser.getCurrentDirectory().getCanonicalPath() + System.getProperty("file.separator");
-                translatedBundleFileName += defaultBundleFileName.substring(0, extensionIndex);
-                translatedBundleFileName += "_" + defaultLocale.getLanguage();
+
+                String translatedBundleFileName = defaultBundleFileName.substring(0, extensionIndex);
+                translatedBundleFileName += "_" + selectedLocale.getLanguage();
                 translatedBundleFileName += defaultBundleFileName.substring(extensionIndex, defaultBundleFileName.length());
                 File translatedBundleFile = new File(translatedBundleFileName);
                 if (translatedBundleFile.exists() == true) {
                     resourceFile.setTranslation(translatedBundleFile);
                 }
                 TopComponent tc = WindowManager.getDefault().findTopComponent("EditorTopComponent");
-                ((EditorTopComponent) tc).setBundles(resourceFile);
+                ((EditorTopComponent) tc).setBundles(resourceFile, selectedLocale);
             } catch (IOException ex) {
                 Exceptions.printStackTrace(ex);
             }
