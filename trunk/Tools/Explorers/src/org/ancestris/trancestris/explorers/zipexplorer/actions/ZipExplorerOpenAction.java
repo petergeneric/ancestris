f1bbb4bee76664af9544e6f7768062aeff9ac88e
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-06-26 20:52:20 +0000

    replace single file management by zip file Management

---
+++ trunk/Tools/Explorers/src/org/ancestris/trancestris/explorers/zipexplorer/actions/ZipExplorerOpenAction.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ancestris.trancestris.explorers.zipexplorer.actions;
+
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import org.ancestris.trancestris.explorers.zipexplorer.ZipExplorerTopComponent;
+import org.ancestris.trancestris.resources.ZipArchive;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
+
+public final class ZipExplorerOpenAction implements ActionListener {
+
+    File zipFile = null;
+    Locale fromLocale = Locale.FRANCE;
+    Locale toLocale = null;
+    ZipExplorerOpenActionPanel zipExplorerOpenActionPanel = new ZipExplorerOpenActionPanel();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        class zipExplorerOpenActionPanelActionListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                zipFile = zipExplorerOpenActionPanel.zipFile;
+                toLocale = zipExplorerOpenActionPanel.fromLocale;
+                toLocale = zipExplorerOpenActionPanel.toLocale;
+            }
+        }
+        DialogDescriptor zipExplorerOpenActionDescriptor = new DialogDescriptor(
+                zipExplorerOpenActionPanel,
+                NbBundle.getMessage(ZipExplorerOpenActionPanel.class, "CTL_ZipExplorerOpenAction"),
+                true,
+                new zipExplorerOpenActionPanelActionListener());
+
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(zipExplorerOpenActionDescriptor);
+        dialog.setVisible(true);
+        dialog.toFront();
+        if (zipExplorerOpenActionDescriptor.getValue() == DialogDescriptor.OK_OPTION) {
+
+            try {
+                ZipArchive zipArchive = new ZipArchive(zipFile, fromLocale, toLocale);
+
+                TopComponent tc = WindowManager.getDefault().findTopComponent("ZipExplorerTopComponent");
+                ((ZipExplorerTopComponent) tc).setBundles(zipArchive);
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+    }
+}
