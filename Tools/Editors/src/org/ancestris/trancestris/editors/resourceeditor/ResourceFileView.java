96851b6c3d742af318a151da831cc5def28a525a
lemovice <lemovice@ancestris.org>
2011-06-26 20:52:47 +0000

    replace single file management by zip file Management

--- Tools/Editors/src/org/ancestris/trancestris/editors/resourceeditor/ResourceFileView.java
+++ Tools/Editors/src/org/ancestris/trancestris/editors/resourceeditor/ResourceFileView.java
@@ -1,4 +1,4 @@
-package org.ancestris.trancestris.editors;
+package org.ancestris.trancestris.editors.resourceeditor;
 
 // Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.geocities.com/kpdus/jad.html
