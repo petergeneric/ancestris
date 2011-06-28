d1e5086a9266a619f74f5ec31977164f44746845
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-06-28 19:36:40 +0000

    Correct langage selection problems

--- trunk/Tools/Resources/src/org/ancestris/trancestris/resources/ZipArchive.java
+++ trunk/Tools/Resources/src/org/ancestris/trancestris/resources/ZipArchive.java
@@ -2,62 +2,73 @@ package org.ancestris.trancestris.resources;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class ZipArchive {
 
-    private ZipDirectory root;
     private static final String PREFIX = "Bundle";
     private static final String SUFFIX = ".properties";
+    private static final Logger logger = Logger.getLogger(ZipArchive.class.getName());
+    private ZipDirectory root;
 
-    public ZipArchive(File inputFile, Locale fromLocale, Locale toLocale) throws IOException {
+    public ZipArchive(File inputFile, Locale fromLocale, Locale toLocale) {
         root = new ZipDirectory(inputFile.getName());
         String fromBundleName = "";
         String toBundleName = "";
-        if (fromLocale.equals(Locale.UK)) {
+
+        if (fromLocale.getCountry().equals("")) {
             fromBundleName = PREFIX + SUFFIX;
         } else {
             fromBundleName = PREFIX + "_" + fromLocale.getLanguage() + SUFFIX;
-
         }
-        if (toLocale.equals(Locale.UK)) {
+
+        if (toLocale.getCountry().equals("")) {
             toBundleName = PREFIX + SUFFIX;
         } else {
             toBundleName = PREFIX + "_" + toLocale.getLanguage() + SUFFIX;
         }
 
-        ZipFile zipInputFile = new ZipFile(inputFile);
-        // Get Reference Bundle
-        for (Enumeration<ZipEntry> e = (Enumeration<ZipEntry>) zipInputFile.entries(); e.hasMoreElements();) {
-            ZipEntry zipEntry = e.nextElement();
-            if (!zipEntry.isDirectory()) {
-                String bundleName = zipEntry.getName();
-                bundleName = bundleName.substring(bundleName.lastIndexOf('/') + 1);
-                if (bundleName.equals(fromBundleName)) {
-                    root.put(zipEntry.getName(), zipInputFile.getInputStream(zipEntry));
+        try {
+            ZipFile zipInputFile = new ZipFile(inputFile);
+            // Get Reference Bundle
+            for (Enumeration<ZipEntry> e = (Enumeration<ZipEntry>) zipInputFile.entries(); e.hasMoreElements();) {
+                ZipEntry zipEntry = e.nextElement();
+                if (!zipEntry.isDirectory()) {
+                    String bundleName = zipEntry.getName();
+                    bundleName = bundleName.substring(bundleName.lastIndexOf('/') + 1);
+                    if (bundleName.equals(fromBundleName)) {
+                        root.put(zipEntry.getName(), zipInputFile.getInputStream(zipEntry));
+                    }
                 }
             }
-        }
-        // Get Translated Bundle
-        for (Enumeration<ZipEntry> e = (Enumeration<ZipEntry>) zipInputFile.entries(); e.hasMoreElements();) {
-            ZipEntry zipEntry = e.nextElement();
-            if (!zipEntry.isDirectory()) {
-                String bundleName = zipEntry.getName();
-                bundleName = bundleName.substring(bundleName.lastIndexOf('/') + 1);
-                if (bundleName.equals(toBundleName)) {
-                    root.setTranslation(zipEntry.getName(), zipInputFile.getInputStream(zipEntry));
+            // Get Translated Bundle
+            for (Enumeration<ZipEntry> e = (Enumeration<ZipEntry>) zipInputFile.entries(); e.hasMoreElements();) {
+                ZipEntry zipEntry = e.nextElement();
+                if (!zipEntry.isDirectory()) {
+                    String bundleName = zipEntry.getName();
+                    bundleName = bundleName.substring(bundleName.lastIndexOf('/') + 1);
+                    if (bundleName.equals(toBundleName)) {
+                        root.setTranslation(zipEntry.getName(), zipInputFile.getInputStream(zipEntry));
+                    }
                 }
             }
+        } catch (IOException ioe) {
+            logger.log(Level.SEVERE, null, ioe);
         }
     }
 
-    public void writeTo(OutputStream outputstream, boolean flag, String s) throws IOException {
-        root.writeTo(outputstream);
+    public void write(File outputFile, String s) {
+
+        try {
+            root.writeTo(null);
+        } catch (IOException ioe) {
+            Logger.getLogger(ZipArchive.class.getName()).log(Level.SEVERE, null, ioe);
+        }
     }
 
     public ZipDirectory getRoot() {
@@ -66,8 +77,5 @@ public class ZipArchive {
 
     public String getName() {
         return root.getName();
-    }
-
-    public void setTranslation(InputStream inputstream) throws IOException {
     }
 }
