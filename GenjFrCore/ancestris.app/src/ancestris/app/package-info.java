78a21f7f3635a149785f784c2d187a2020113e4c
dandre <dandre@ancestris.org>
2011-08-04 14:21:43 +0000

    fix svn inconstitencies

---
+++ GenjFrCore/ancestris.app/src/ancestris/app/package-info.java
@@ -0,0 +1,2 @@
+@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "View", categoryName = "#OptionsCategory_Name_View", iconBase = "ancestris/app/OptionsViews.png", keywords = "#OptionsCategory_Keywords_View", keywordsCategory = "View")
+package ancestris.app;
---
+++ GenjFrCore/ancestris.app/src/ancestris/util/Utilities.java
@@ -0,0 +1,27 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2011 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package ancestris.util;
+
+/**
+ *
+ * @author daniel
+ */
+public class Utilities {
+    public static String getClassName(Object o){
+        return getClassName(o.getClass());
+    }
+
+    public static String getClassName(Class c){
+        return c.getName().replace('.', '/');
+    }
+}
