c8dad27dbc31b3fc32d160f10e98ffe0423e3861
dandre <dandre@ancestris.org>
2011-08-21 20:37:44 +0000

    recents genealogies panel had been removed, resurect it

--- AncestrisCore/welcome/src/ancestris/welcome/ui/MyAncestrisTab.java
+++ AncestrisCore/welcome/src/ancestris/welcome/ui/MyAncestrisTab.java
@@ -77,8 +77,8 @@ class MyAncestrisTab extends AbstractTab {
 //        leftPanel.add(new RecentFilesPanel() );
         leftPanel.add( new ContentSection( new NewGedcomPanel(), false, false, false ) );
         leftPanel.add( new ContentSection( new OpenGedcomPanel(), false, false, false ) );
-        leftPanel.add( new ContentSection( BundleSupport.getLabel( "SectionSample" ), //NOI18N
-                new SamplePanel(), false, false, false ) );
+        leftPanel.add( new ContentSection( BundleSupport.getLabel( "SectionRecentFiles" ), //NOI18N
+                new RecentFilesPanel(), false, false, false ) );
 //        leftPanel.add( new NewGedcomPanel() );
 
         JPanel rightPanel = new JPanel( new GridLayout(0,1) );
