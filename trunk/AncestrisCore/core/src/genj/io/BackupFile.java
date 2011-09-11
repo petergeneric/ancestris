14dca77245184c61b8ac77e18fc56e692d36aed3
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-09-11 19:57:52 +0000

    change backup filename format. Now: file_aaaammdd-hhmmss.ged

--- trunk/AncestrisCore/core/src/genj/io/BackupFile.java
+++ trunk/AncestrisCore/core/src/genj/io/BackupFile.java
@@ -6,7 +6,9 @@ package genj.io;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,10 +17,12 @@ import java.util.regex.Pattern;
  * @author daniel
  */
 public class BackupFile {
+    static private final SimpleDateFormat ISO_FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss");
+
 
     public static void createBackup(File file) throws GedcomIOException {
         if (file.exists()) {
-            final Pattern p = Pattern.compile(file.getName() + "_([0-9]{4})_");
+            final Pattern p = Pattern.compile(getBasename(file.getName()) + "_([0-9]{8}-[0-9]{6})" + getExtension(file.getName()));
             final File theFile = file;
             File bak;
 
@@ -45,13 +49,9 @@ public class BackupFile {
                 if (!m.matches()) {
                     throw new GedcomIOException("Couldn't create backup for file " + file.getName(), -1);
                 }
-                bak = new File(file.getAbsolutePath() + "_"
-                        + getNextSuffix(m.group(1))
-                        + "_");
+                bak = getBackupFile(file);
             } else {
-                bak = new File(file.getAbsolutePath() + "_"
-                        + getNextSuffix("0")
-                        + "_");
+                bak = getBackupFile(file);
             }
             if (bak.exists()) {
                 throw new GedcomIOException("Couldn't create backup file " + bak.getName(), -1);
@@ -62,7 +62,49 @@ public class BackupFile {
         }
     }
 
-    private static String getNextSuffix(String suffix) {
+    private static String getNextSuffix1(String suffix) {
         return String.format("%04d", Integer.parseInt(suffix) + 1);
     }
+    private static File getBackupFile(File file){
+        String fileName = file.getAbsolutePath();
+        return new File(getBasename(fileName)+"_"+
+                ISO_FORMAT.format(new Date())+
+                getExtension(fileName));
+    }
+
+    // From Util.getExtension
+    /** Gets the extension of a specified file name. The extension is
+    * everything after the last dot. The extension includes the dot char so that
+    * fileName = Basename+Extension
+    *
+    * @param fileName name of the file
+    * @return extension of the file (or <code>""</code> if it had none)
+    */
+    private static String getExtension(String fileName) {
+        int index = fileName.lastIndexOf("."); // NOI18N
+
+        if (index == -1) {
+            return ""; // NOI18N
+        } else {
+            return fileName.substring(index);
+        }
+    }
+
+    /** Gets the basename of a specified file name. The basename is
+    * everything before the last dot.
+    *
+    * @param fileName name of the file
+    * @return basename of the file (or <code>fileName</code> if it had none)
+    */
+    private static String getBasename(String fileName) {
+        int index = fileName.lastIndexOf("."); // NOI18N
+
+        if (index == -1) {
+            return fileName; // NOI18N
+        } else {
+            return fileName.substring(0,index);
+        }
+    }
+
+
 }
