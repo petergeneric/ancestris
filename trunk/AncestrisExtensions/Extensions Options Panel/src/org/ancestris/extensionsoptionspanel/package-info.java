21cf96028e121a29c02f0e8bc170db9fe1e92648
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-09-11 13:10:58 +0000

    Remove unused directorys

--- trunk/AncestrisExtensions/Extensions Options Panel/src/org/ancestris/extensionsoptionspanel/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "Extensions", categoryName = "#OptionsCategory_Name_Extensions", iconBase = "org/ancestris/extensionsoptionspanel/extensions.png", keywords = "#OptionsCategory_Keywords_Extensions", keywordsCategory = "Extensions", position=1000)
-package org.ancestris.extensionsoptionspanel;
