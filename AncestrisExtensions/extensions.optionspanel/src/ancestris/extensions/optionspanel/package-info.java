3034bc69df7a2258cf0c2343ff4cb860aaca619c
lemovice <lemovice@ancestris.org>
2011-10-01 17:51:29 +0000

    move package from org.ancestris to ancestris as other ancestris packages

---
+++ AncestrisExtensions/extensions.optionspanel/src/ancestris/extensions/optionspanel/package-info.java
@@ -0,0 +1,2 @@
+@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "Extensions", categoryName = "#OptionsCategory_Name_Extensions", iconBase = "org/ancestris/extensions/optionspanel/extensions.png", keywords = "#OptionsCategory_Keywords_Extensions", keywordsCategory = "Extensions", position=1000)
+package ancestris.extensions.optionspanel;
