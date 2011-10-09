591a0a0070c7e6d2e7f15fd718cfc4daf602e03a
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-10-09 15:15:05 +0000

    Remove specific save file capability and use generic save file capability

--- trunk/AncestrisExtensions/Report Flash List/src/ancestris/extensions/flashlist/FlashListAction.java
+++ trunk/AncestrisExtensions/Report Flash List/src/ancestris/extensions/flashlist/FlashListAction.java
@@ -2,10 +2,13 @@ package ancestris.extensions.flashlist;
 
 import ancestris.app.App;
 import ancestris.extensions.reportsview.ReportViewTopComponent;
+import ancestris.extensions.reportsview.SaveReport;
 import genj.fo.Document;
 import genj.gedcom.Context;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.prefs.Preferences;
+import org.openide.util.NbPreferences;
 
 public final class FlashListAction implements ActionListener {
 
@@ -15,14 +18,16 @@ public final class FlashListAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         window = ReportViewTopComponent.findInstance();
+        Preferences modulePreferences = NbPreferences.forModule(ReportFlashList.class);
 
         context = App.center.getSelectedContext(true);
         if (context != null) {
             Document doc = new ReportFlashList().start(context.getGedcom());
-            window.openAtTabPosition(0);
+            window.displayDocument(doc);
+            window.open();
             window.requestActive();
-            window.getReportViewScrollPane();
-            window.test (doc);
+            String fileName = new SaveReport(doc, modulePreferences.get("reportFilename", "")).saveFile();
+            modulePreferences.put("reportFilename", fileName);
         }
     }
 }
