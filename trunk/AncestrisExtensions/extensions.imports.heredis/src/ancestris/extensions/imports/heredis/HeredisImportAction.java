42f4d0c2e691478ffbb18c9544b609a9c83fd082
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-11-21 22:15:50 +0000

    Add Warning DialogMessage, add Open DialogTitle.

--- trunk/AncestrisExtensions/extensions.imports.heredis/src/ancestris/extensions/imports/heredis/HeredisImportAction.java
+++ trunk/AncestrisExtensions/extensions.imports.heredis/src/ancestris/extensions/imports/heredis/HeredisImportAction.java
@@ -5,15 +5,18 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
 
 public final class HeredisImportAction implements ActionListener {
 
-    JFileChooser fc = new JFileChooser() {
-    };
+    JFileChooser fc = new JFileChooser();
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, NbBundle.getMessage(HeredisImportAction.class, "warning"));
+        fc.setDialogTitle(NbBundle.getMessage(HeredisImportAction.class, "openDialog.Title"));
         if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             File importFile = fc.getSelectedFile();
             File outFile = null;
@@ -23,7 +26,7 @@ public final class HeredisImportAction implements ActionListener {
                 Exceptions.printStackTrace(ex);
             }
 
-            new HeredisImport (importFile, outFile).run();
+            new HeredisImport(importFile, outFile).run();
         }
     }
 }
