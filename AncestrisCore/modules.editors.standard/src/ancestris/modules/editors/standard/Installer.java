a8c278151f58fcf2067cef9e4a91d0ea5baf9e8e
dandre <dandre@ancestris.org>
2011-11-29 00:14:51 +0000

    open an editor window on right clic -> edit

--- AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/Installer.java
+++ AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/Installer.java
@@ -16,18 +16,17 @@ import genj.util.swing.Action2;
 import genj.util.swing.Action2.Group;
 import genj.view.ActionProvider;
 import ancestris.core.pluginservice.AncestrisPlugin;
+import genj.gedcom.Fam;
+import genj.gedcom.Indi;
+import genj.view.SelectionSink;
 import java.awt.event.ActionEvent;
-import javax.swing.JScrollPane;
-import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
 import org.openide.modules.ModuleInstall;
-import org.openide.util.NbBundle;
 
 /**
  * Manages a module's lifecycle. Remember that an installer is optional and
  * often not needed at all.
  */
-public class Installer extends ModuleInstall implements ActionProvider{
+public class Installer extends ModuleInstall implements ActionProvider {
 
     @Override
     public void restored() {
@@ -35,29 +34,27 @@ public class Installer extends ModuleInstall implements ActionProvider{
         // By default, do nothing.
         // Put your startup code here.
     }
-    
+
     public void createActions(Context context, Purpose purpose, Group into) {
-        if (purpose != Purpose.CONTEXT)
+        if (purpose != Purpose.CONTEXT) {
             return;
-        for (EditorTopComponent edit : AncestrisPlugin.lookupAll(EditorTopComponent.class) ) {
-            Context viewContext = edit.getContext();
-            if (viewContext == null)
-                continue;
-            if (viewContext.sameGedcom(context)){
-                into.add(new OpenInEditor(edit,context));
-                return;
-            }
         }
-        into.add(new OpenInEditor(null,context));
+        // XXX: must be rewritten
+        if (context == null) {
+            return;
+        }
+        if (!(context.getEntity() instanceof Indi)
+                && !(context.getEntity() instanceof Fam)) {
+            return;
+        }
+        into.add(new OpenEditor(context));
     }
 
-    private static class OpenInEditor extends Action2 {
+    private static class OpenEditor extends Action2 {
 
-        EditorTopComponent editor;
         Context context;
 
-        public OpenInEditor(EditorTopComponent editor, Context context) {
-            this.editor = editor;
+        public OpenEditor(Context context) {
             this.context = context;
             setText(ResourcesSupport.getTitle("OpenInEditor"));  // NOI18N
             setImage(ResourcesSupport.editorIcon);
@@ -65,19 +62,14 @@ public class Installer extends ModuleInstall implements ActionProvider{
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (editor != null)
-                editor.setContext(context,true);
-            else{
-                String title = NbBundle.getMessage(this.getClass(), "dialog.gedcom.edit.title", context.getGedcom());
-                final GedcomPanel panel = new GedcomPanel();
-                panel.setContext(context);
-                NotifyDescriptor nd = new NotifyDescriptor(new JScrollPane(panel), title, NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.PLAIN_MESSAGE, null, null);
-                DialogDisplayer.getDefault().notify(nd);
-                if (nd.getValue().equals(NotifyDescriptor.OK_OPTION)) {
-                    panel.commit();
-                }
+            SelectionSink.Dispatcher.muteSelection(true);
+            if (context.getEntity() instanceof Indi) {
+                EntityEditor.editEntity((Indi) (context.getEntity()), false);
             }
+            if (context.getEntity() instanceof Fam) {
+                EntityEditor.editEntity((Fam) (context.getEntity()), false);
+            }
+            SelectionSink.Dispatcher.muteSelection(false);
         }
     }
-
 }
