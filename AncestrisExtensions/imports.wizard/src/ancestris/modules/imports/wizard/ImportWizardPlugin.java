0b39a53b1afdbca9c995fade6cda8610f7dc9a66
lemovice <lemovice@ancestris.org>
2011-12-17 22:59:00 +0000

    Add AncestrisPlugin depencies modif build.xml for plugin version automatic increment according subversion revision number

---
+++ AncestrisExtensions/imports.wizard/src/ancestris/modules/imports/wizard/ImportWizardPlugin.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ancestris.modules.imports.wizard;
+
+import ancestris.core.pluginservice.AncestrisPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author daniel
+ */
+@ServiceProvider(service=ancestris.core.pluginservice.PluginInterface.class)
+public class ImportWizardPlugin extends AncestrisPlugin{
+
+}
