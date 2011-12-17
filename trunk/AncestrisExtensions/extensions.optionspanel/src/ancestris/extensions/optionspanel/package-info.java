12df45b1c2c0ed32b6a9d75b5122fbb3dfa22825
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-12-17 21:56:15 +0000

    Suppress commit

--- trunk/AncestrisExtensions/extensions.optionspanel/src/ancestris/extensions/optionspanel/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "Extensions", categoryName = "#OptionsCategory_Name_Extensions", iconBase = "ancestris/extensions/optionspanel/extensions.png", keywords = "#OptionsCategory_Keywords_Extensions", keywordsCategory = "Extensions", position=1000)
-package ancestris.extensions.optionspanel;
