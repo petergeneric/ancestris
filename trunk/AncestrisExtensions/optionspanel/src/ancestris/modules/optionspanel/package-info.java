4d18ba66eb2830f66bfbd40cab00f2652c434004
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2011-12-17 22:13:43 +0000

    Rename directory according ancestris naming rules

---
+++ trunk/AncestrisExtensions/optionspanel/src/ancestris/modules/optionspanel/package-info.java
@@ -0,0 +1,2 @@
+@org.netbeans.spi.options.OptionsPanelController.ContainerRegistration(id = "Extensions", categoryName = "#OptionsCategory_Name_Extensions", iconBase = "ancestris/modules/optionspanel/extensions.png", keywords = "#OptionsCategory_Keywords_Extensions", keywordsCategory = "Extensions", position=1000)
+package ancestris.modules.optionspanel;
