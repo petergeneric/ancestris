0d02928f4c59dd4a7e21f027aca27b04d060e78f
lemovice <lemovice@ancestris.org>
2012-01-23 22:37:53 +0000

    First plugin version

---
+++ AncestrisExtensions/reports.relatives/src/ancestris/modules/reports/relatives/ReportRelativesPlugin.java
@@ -0,0 +1,25 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2011 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package ancestris.modules.reports.relatives;
+
+import ancestris.core.pluginservice.AncestrisPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author daniel
+ */
+@ServiceProvider(service=ancestris.core.pluginservice.PluginInterface.class)
+public class ReportRelativesPlugin extends AncestrisPlugin{
+
+}
