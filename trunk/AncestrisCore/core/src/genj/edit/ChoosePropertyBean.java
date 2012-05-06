19bf7a33be484284cfbd26ffc2866863da7bea15
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-05-06 12:23:59 +0000

    Correct compilation error with OpenJdk 7

--- trunk/AncestrisCore/core/src/genj/edit/ChoosePropertyBean.java
+++ trunk/AncestrisCore/core/src/genj/edit/ChoosePropertyBean.java
@@ -187,11 +187,12 @@ public class ChoosePropertyBean extends JComponent {
   /**
    * Tag List Cell Renderer
    */
-  class MetaDefRenderer extends DefaultListCellRenderer implements ListCellRenderer {
+  class MetaDefRenderer extends DefaultListCellRenderer implements ListCellRenderer <Object> {
 
     /**
      * Return component for rendering list element
      */
+        @Override
     public Component getListCellRendererComponent(JList list,Object value,int index,boolean isSelected,boolean cellHasFocus) {
       super.getListCellRendererComponent(list,value,index,isSelected,cellHasFocus);
         MetaProperty def = (MetaProperty)value;
