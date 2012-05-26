052442660c3dd7a6ea481c6f10962e1b7c427541
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-05-26 22:18:07 +0000

    replace Resources by NbBundle equivalent

--- trunk/AncestrisCore/ancestris.app/src/ancestris/app/ActionClose.java
+++ trunk/AncestrisCore/ancestris.app/src/ancestris/app/ActionClose.java
@@ -6,13 +6,10 @@ package ancestris.app;
 
 import ancestris.view.Images;
 import genj.gedcom.Context;
-import genj.gedcom.Gedcom;
-import genj.util.Resources;
 import genj.util.swing.Action2;
-import ancestris.core.pluginservice.AncestrisPlugin;
-import ancestris.gedcom.GedcomDirectory;
 import genj.app.Workbench;
 import java.awt.event.ActionEvent;
+import org.openide.util.NbBundle;
 
 /**
  *
@@ -23,12 +20,11 @@ import java.awt.event.ActionEvent;
  */
 public class ActionClose extends Action2 {
 
-    private Resources RES = Resources.get(ActionClose.class);
     private Context contextBeingClosed = null;
 
     /** constructor */
     public ActionClose() {
-        setText(RES, "CTL_ActionClose");
+        setText(NbBundle.getMessage(ActionClose.class,"CTL_ActionClose"));
         setImage(Images.imgClose);
         setEnabled(enabled);
     }
