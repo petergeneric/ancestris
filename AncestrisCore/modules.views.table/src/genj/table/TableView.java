7ca9fa08ebc552ca826ccc064705a7bb0ddea8f1
dandre <dandre@ancestris.org>
2012-06-25 22:38:56 +0000

    move table view from core module to a separate module

--- AncestrisCore/modules.views.table/src/genj/table/TableView.java
+++ AncestrisCore/modules.views.table/src/genj/table/TableView.java
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+//XXX: try to not export this api
 package genj.table;
 
 import genj.common.AbstractPropertyTableModel;
