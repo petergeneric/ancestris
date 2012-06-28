096de82c470d22d360f0918fefad6b1abcbf1534
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-06-28 21:36:08 +0000

    move search block view from core and app modules to a separate module

---
+++ trunk/AncestrisCore/modules.utilities.search/src/ancestris/modules/utilities/search/SearchPlugin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ancestris.modules.utilities.search;
+
+import ancestris.core.pluginservice.AncestrisPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author daniel
+ */
+@ServiceProvider(service = ancestris.core.pluginservice.PluginInterface.class)
+public class SearchPlugin extends AncestrisPlugin {
+}
