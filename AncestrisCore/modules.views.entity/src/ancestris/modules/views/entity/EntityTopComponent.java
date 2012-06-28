c4282955fc3fad904f1dee1377b0e29d70d54a79
dandre <dandre@ancestris.org>
2012-06-28 20:35:52 +0000

    move entity view from core and app modules to a separate module

--- AncestrisCore/modules.views.entity/src/ancestris/modules/views/entity/EntityTopComponent.java
+++ AncestrisCore/modules.views.entity/src/ancestris/modules/views/entity/EntityTopComponent.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ancestris.app;
+package ancestris.modules.views.entity;
 
 import ancestris.view.GenjViewTopComponent;
 import ancestris.view.AncestrisDockModes;
