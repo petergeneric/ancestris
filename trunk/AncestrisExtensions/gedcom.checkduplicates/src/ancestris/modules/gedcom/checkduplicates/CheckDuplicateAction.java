50464909362a2637d2ea96aea9067970791059fc
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-06-29 19:17:59 +0000

    create worker thread, add progress handle display and remove layer.xml

--- trunk/AncestrisExtensions/gedcom.checkduplicates/src/ancestris/modules/gedcom/checkduplicates/CheckDuplicateAction.java
+++ trunk/AncestrisExtensions/gedcom.checkduplicates/src/ancestris/modules/gedcom/checkduplicates/CheckDuplicateAction.java
@@ -5,7 +5,13 @@ import genj.gedcom.Context;
 import genj.gedcom.Gedcom;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 
+@ActionID(id = "ancestris.modules.gedcom.checkduplicates.CheckDuplicateAction", category = "Tools")
+@ActionRegistration(iconInMenu = true, displayName = "#CTL_CheckDuplicateAction")
+@ActionReference(path = "Menu/Tools/Gedcom")
 public final class CheckDuplicateAction implements ActionListener {
 
     @Override
@@ -14,7 +20,8 @@ public final class CheckDuplicateAction implements ActionListener {
 
         if ((context = App.center.getSelectedContext(true)) != null) {
             Gedcom myGedcom = context.getGedcom();
-            new CheckDuplicates(myGedcom, myGedcom).run();
+            Thread t = new Thread(new CheckDuplicates(myGedcom, myGedcom));
+            t.start();
         }
     }
 }
