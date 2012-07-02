0b4b17d40a6a92838cda4272e18743974da25260
dandre <dandre@ancestris.org>
2012-07-02 21:45:28 +0000

    Move all reports to a separate modules Move All-in-One tree report to its own module Note: all reports are disabled ATM. Will be re-enabled later

---
+++ AncestrisCore/report.svgtree/src/ancestris/report/svgtree/graphics/GraphicsOutput.java
@@ -0,0 +1,32 @@
+/**
+ * Reports are Freeware Code Snippets
+ *
+ * This report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package ancestris.report.svgtree.graphics;
+
+import genj.report.Report;
+
+import java.io.IOException;
+
+
+/**
+ * Interface for classes writing report output.
+ *
+ * @author Przemek Wiech <pwiech@losthive.org>
+ */
+public interface GraphicsOutput {
+
+    /**
+     * Writes the family tree to the output.
+     */
+	public void output(GraphicsRenderer renderer) throws IOException;
+
+    /**
+     * Displays the generated content.
+     */
+    public Object result(Report report);
+}
---
+++ AncestrisCore/reports/src/ancestris/reports/phonetics/Phonetics.java
@@ -0,0 +1,14 @@
+package ancestris.reports.phonetics;
+
+
+/**
+ * Our phonetics interface
+ */
+public interface Phonetics {
+
+    /**
+     * encode implementation
+     */
+    public String encode(String name);
+
+}
