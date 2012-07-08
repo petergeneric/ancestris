a0c73b321d0dcb5cb24a78dc52eac4e021cb316a
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-07-08 21:37:39 +0000

    Modify listener architecture

--- trunk/AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/SosaNumbersPlugin.java
+++ trunk/AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/SosaNumbersPlugin.java
@@ -9,17 +9,60 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-
 package ancestris.modules.gedcom.sosanumbers;
 
 import ancestris.core.pluginservice.AncestrisPlugin;
+import ancestris.util.swing.SelectEntityDialog;
+import genj.app.GedcomFileListener;
+import genj.gedcom.Context;
+import genj.gedcom.Gedcom;
+import genj.gedcom.Indi;
+import java.util.prefs.Preferences;
+import org.openide.util.NbBundle;
+import org.openide.util.NbPreferences;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author daniel
  */
-@ServiceProvider(service=ancestris.core.pluginservice.PluginInterface.class)
-public class SosaNumbersPlugin extends AncestrisPlugin{
+@ServiceProvider(service = ancestris.core.pluginservice.PluginInterface.class)
+public class SosaNumbersPlugin extends AncestrisPlugin implements GedcomFileListener {
 
+    SosaNumbers sosaNumbers = null;
+
+    @Override
+    public void commitRequested(Context context) {
+    }
+
+    @Override
+    public void gedcomClosed(Gedcom gedcom) {
+        if (sosaNumbers != null) {
+//        gedcom.removeGedcomListener(sosaNumbers);
+        }
+    }
+
+    @Override
+    public void gedcomOpened(Gedcom gedcom) {
+        Preferences modulePreferences = NbPreferences.forModule(SosaNumbers.class);
+        String selectedEntityID = modulePreferences.get("SelectEntityDialog." + gedcom.getName(), "");
+        Indi indiDeCujus = null;
+        if (selectedEntityID.isEmpty()) {
+            if (gedcom.getIndis().isEmpty() == false) {
+                SelectEntityDialog selectEntityDialog = new SelectEntityDialog(NbBundle.getMessage(this.getClass(), "GenerateSosaAction.AskDeCujus"), gedcom, Gedcom.INDI);
+                if ((indiDeCujus = (Indi) selectEntityDialog.getEntity()) != null) {
+                    modulePreferences.put("SelectEntityDialog." + gedcom.getName(), indiDeCujus.getId());
+                } else {
+                    modulePreferences.put("SelectEntityDialog." + gedcom.getName(), "No SOSA");
+                }
+            }
+        } else if (!selectedEntityID.equals("No SOSA")) {
+            indiDeCujus = (Indi) gedcom.getEntity(Gedcom.INDI, selectedEntityID);
+        }
+        if (indiDeCujus != null) {
+            sosaNumbers = new SosaNumbers();
+            sosaNumbers.generateSosaNbs(gedcom, indiDeCujus);
+//          gedcom.addGedcomListener(sosaNumbers);
+        }
+    }
 }
