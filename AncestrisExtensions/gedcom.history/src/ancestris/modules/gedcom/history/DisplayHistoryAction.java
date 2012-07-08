cc29d5521712038a2b84aef78d08247b56daf1e7
lemovice <lemovice@ancestris.org>
2012-07-08 21:20:45 +0000

    First version of the plugin

---
+++ AncestrisExtensions/gedcom.history/src/ancestris/modules/gedcom/history/DisplayHistoryAction.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2012 lemovice
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package ancestris.modules.gedcom.history;
+
+import ancestris.app.App;
+import ancestris.core.pluginservice.PluginInterface;
+import genj.gedcom.Context;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.Lookup;
+
+@ActionID(category = "Tools",
+id = "ancestris.modules.gedcom.history.DisplayHistoryAction")
+@ActionRegistration(displayName = "#CTL_DisplayHistoryAction")
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools/Gedcom", position = 3333)
+})
+public final class DisplayHistoryAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (PluginInterface pluginInterface : Lookup.getDefault().lookupAll(PluginInterface.class)) {
+            if (pluginInterface instanceof GedcomHistoryPlugin) {
+                Context context = App.center.getSelectedContext(true);
+                if (context != null) {
+                    String gedcomName = context.getGedcom().getName().substring(0, context.getGedcom().getName().lastIndexOf(".") == -1 ? context.getGedcom().getName().length() : context.getGedcom().getName().lastIndexOf("."));
+
+                    GedcomHistoryTopComponent GedcomHistoryTopComponent = new GedcomHistoryTopComponent(((GedcomHistoryPlugin) pluginInterface).getGedcomHistory(gedcomName));
+                    GedcomHistoryTopComponent.open();
+                    GedcomHistoryTopComponent.requestActive();
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ AncestrisExtensions/gedcom.history/src/ancestris/modules/gedcom/history/GedcomHistoryPlugin.java
@@ -0,0 +1,101 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2011 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package ancestris.modules.gedcom.history;
+
+import ancestris.core.pluginservice.AncestrisPlugin;
+import genj.app.GedcomFileListener;
+import genj.gedcom.Context;
+import genj.gedcom.Gedcom;
+import java.io.*;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import org.openide.modules.Places;
+import org.openide.util.Exceptions;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author lemovice
+ */
+@ServiceProvider(service = ancestris.core.pluginservice.PluginInterface.class)
+public class GedcomHistoryPlugin extends AncestrisPlugin implements GedcomFileListener {
+
+    private final static Logger log = Logger.getLogger(GedcomHistoryPlugin.class.getName());
+    private final static HashMap<String, GedcomHistory> gedcomHistoryMap = new <String, GedcomHistory>HashMap();
+
+    @Override
+    public void commitRequested(Context context) {
+        log.log(Level.INFO, "Commit requested {0}", context.getGedcom().getName());
+    }
+
+    @Override
+    public void gedcomClosed(Gedcom gedcom) {
+        String gedcomName = gedcom.getName().substring(0, gedcom.getName().lastIndexOf(".") == -1 ? gedcom.getName().length() : gedcom.getName().lastIndexOf("."));
+        File cacheSubdirectory = Places.getCacheSubdirectory(GedcomHistoryPlugin.class.getCanonicalName());
+        File historyFile = new File(cacheSubdirectory.getAbsolutePath() + System.getProperty("file.separator") + gedcomName + ".hist");
+        log.log(Level.INFO, "saving history File {0}", historyFile.getAbsoluteFile());
+
+        gedcom.removeGedcomListener(gedcomHistoryMap.get(gedcomName));
+        try {
+            // create JAXB context and instantiate marshaller
+            JAXBContext context = JAXBContext.newInstance(GedcomHistory.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            m.marshal(gedcomHistoryMap.get(gedcomName), new FileWriter(historyFile));
+            gedcomHistoryMap.remove(gedcomName);
+        } catch (JAXBException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+    @Override
+    public void gedcomOpened(Gedcom gedcom) {
+        String gedcomName = gedcom.getName().substring(0, gedcom.getName().lastIndexOf(".") == -1 ? gedcom.getName().length() : gedcom.getName().lastIndexOf("."));
+        File cacheSubdirectory = Places.getCacheSubdirectory(GedcomHistoryPlugin.class.getCanonicalName());
+        File historyFile = new File(cacheSubdirectory.getAbsolutePath() + System.getProperty("file.separator") + gedcomName + ".hist");
+        log.log(Level.INFO, "opening history file {0}", historyFile.getAbsoluteFile());
+
+        if (historyFile.exists() == true) {
+            try {
+                JAXBContext context = JAXBContext.newInstance(GedcomHistory.class);
+
+                Unmarshaller um = context.createUnmarshaller();
+                GedcomHistory gedcomHistory;
+                try {
+                    gedcomHistory = (GedcomHistory) um.unmarshal(new FileReader(historyFile));
+                    gedcomHistoryMap.put(gedcomName, gedcomHistory);
+                    gedcom.addGedcomListener(gedcomHistoryMap.get(gedcomName));
+                } catch (FileNotFoundException ex) {
+                    gedcomHistoryMap.put(gedcomName, new GedcomHistory(gedcomName));
+                    gedcom.addGedcomListener(gedcomHistoryMap.get(gedcomName));
+                }
+            } catch (JAXBException ex) {
+                gedcomHistoryMap.put(gedcomName, new GedcomHistory(gedcomName));
+                gedcom.addGedcomListener(gedcomHistoryMap.get(gedcomName));
+            }
+        } else {
+            gedcomHistoryMap.put(gedcomName, new GedcomHistory(gedcomName));
+            gedcom.addGedcomListener(gedcomHistoryMap.get(gedcomName));
+        }
+    }
+
+    public GedcomHistory getGedcomHistory(String gedcomName) {
+        return gedcomHistoryMap.get(gedcomName);
+    }
+}
