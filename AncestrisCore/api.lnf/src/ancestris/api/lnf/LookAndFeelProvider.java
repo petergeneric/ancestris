503bd94dcac4762ba32e65e05f7dbefefc5c58d7
dandre <dandre@ancestris.org>
2012-08-13 21:40:05 +0000

    write javadoc

--- AncestrisCore/api.lnf/src/ancestris/api/lnf/LookAndFeelProvider.java
+++ AncestrisCore/api.lnf/src/ancestris/api/lnf/LookAndFeelProvider.java
@@ -15,11 +15,18 @@ import javax.swing.ImageIcon;
 import org.openide.util.Lookup;
 
 /**
- *XXX: to be documented
  * @author daniel
  */
 public abstract class LookAndFeelProvider {
 
+    /**
+     * Return the uniq name for this LnF provider. .
+     * It can be used to store setting. For instance this name is used in 
+     * {@link LookAndFeelProvider#getProviderFromName(java.lang.String) }.
+     * 
+     * @return The uniq name for this LnF provider. 
+     * 
+     */
     public abstract String getName();
 
     /**
@@ -30,8 +37,16 @@ public abstract class LookAndFeelProvider {
         return null;
     }
 
+    /**
+     * A localized display name for this provider.
+     * @return 
+     */
     public abstract String getDisplayName();
 
+    /**
+     * A localized sample image for this provider.
+     * @return 
+     */
     public abstract ImageIcon getSampleImage();
 
     @Override
@@ -39,10 +54,19 @@ public abstract class LookAndFeelProvider {
         return getDisplayName();
     }
 
+    /**
+     * Gets all LookAndFeel Providers installed in system.
+     * @return 
+     */
     public static LookAndFeelProvider[] getProviders() {
         return (Lookup.getDefault().lookupAll(LookAndFeelProvider.class)).toArray(new LookAndFeelProvider[]{});
     }
 
+    /**
+     * retreive LnF provider from its uniq name. Default to standard LnF
+     * @param name
+     * @return 
+     */
     public static LookAndFeelProvider getProviderFromName(String name) {
         if (name != null) {
             for (LookAndFeelProvider provider : Lookup.getDefault().lookupAll(LookAndFeelProvider.class)) {
