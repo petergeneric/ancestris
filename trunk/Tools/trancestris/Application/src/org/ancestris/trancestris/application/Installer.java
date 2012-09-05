750d1cd64b16c9cb5968c717f64fe231de878b37
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-09-05 21:25:38 +0000

    Ask user if he want to send the translated file before exiting

--- trunk/Tools/trancestris/Application/src/org/ancestris/trancestris/application/Installer.java
+++ trunk/Tools/trancestris/Application/src/org/ancestris/trancestris/application/Installer.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.logging.Level;
@@ -19,10 +20,14 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import javax.swing.JOptionPane;
 import org.ancestris.trancestris.application.actions.DownloadBundlePanel;
+import org.ancestris.trancestris.application.actions.SendTranslationAction;
+import org.ancestris.trancestris.application.actions.SendTranslationPanel;
 import org.ancestris.trancestris.application.utils.DownloadBundleWorker;
+import org.ancestris.trancestris.application.utils.SendMessageWorker;
 import org.ancestris.trancestris.application.utils.TipOfTheDay;
 import org.ancestris.trancestris.explorers.zipexplorer.ZipExplorerTopComponent;
 import org.ancestris.trancestris.resources.ZipArchive;
+import org.netbeans.api.options.OptionsDisplayer;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
@@ -41,11 +46,21 @@ public class Installer extends ModuleInstall {
 
     private Preferences modulePreferences = NbPreferences.forModule(Installer.class);
     private static final Logger logger = Logger.getLogger(Installer.class.getName());
+    private final static String SEND = NbBundle.getMessage(SendTranslationAction.class, "SendTranslationAction.button.send");
+    String toLocale = "";
+    String fromLocale = "";
 
     @Override
     public boolean closing() {
         TopComponent tc = WindowManager.getDefault().findTopComponent("ZipExplorerTopComponent");
         ZipArchive zipArchive = ((ZipExplorerTopComponent) tc).getBundles();
+        SendTranslationPanel sendTranslationPanel = new SendTranslationPanel();
+        String archiveName = "";
+        String filePath = "";
+        String prefix = "";
+        String suffix = "";
+        File zipOutputFile = null;
+
         if (zipArchive != null) {
             if (((ZipExplorerTopComponent) tc).getBundles().isChange()) {
                 NotifyDescriptor nd = new NotifyDescriptor.Confirmation(NbBundle.getMessage(Installer.class, "Exit.confirm"), NotifyDescriptor.YES_NO_OPTION);
@@ -53,10 +68,54 @@ public class Installer extends ModuleInstall {
                 if (nd.getValue() == DialogDescriptor.YES_OPTION) {
                     zipArchive.write();
                 }
-                return true;
-            } else {
-                return true;
             }
+
+            // Send Translation
+            NotifyDescriptor nd = new NotifyDescriptor.Confirmation(NbBundle.getMessage(Installer.class, "Send.confirm"), NotifyDescriptor.YES_NO_OPTION);
+            DialogDisplayer.getDefault().notify(nd);
+            if (nd.getValue() == DialogDescriptor.YES_OPTION) {
+                if (zipArchive.hasTranslation() == true) {
+                    archiveName = zipArchive.getName();
+                    filePath = zipArchive.getZipFile().getParent();
+                    prefix = archiveName.substring(0, archiveName.indexOf('.'));
+                    suffix = archiveName.substring(archiveName.indexOf('.') + 1);
+                    toLocale = zipArchive.getToLocale().getLanguage();
+                    fromLocale = zipArchive.getFromLocale().getLanguage();
+
+                    zipOutputFile = new File(filePath + File.separator + prefix + "_" + toLocale + "." + suffix);
+                    if (!zipOutputFile.exists()) {
+                        try {
+                            zipOutputFile.createNewFile();
+                        } catch (IOException ex) {
+                            Exceptions.printStackTrace(ex);
+                        }
+                    }
+                    zipArchive.saveTranslation(zipOutputFile);
+
+                    // saveTranslation modify directory structures
+                    zipArchive.write();
+
+                    setDefaultValues(sendTranslationPanel);
+                    DialogDescriptor dd = new DialogDescriptor(sendTranslationPanel, NbBundle.getMessage(SendTranslationAction.class, "SendTranslationPanel.title"));
+                    dd.setOptions(new Object[]{SEND, DialogDescriptor.CANCEL_OPTION});
+                    DialogDisplayer.getDefault().createDialog(dd);
+                    DialogDisplayer.getDefault().notify(dd);
+                    if (dd.getValue().equals(SEND)) {
+                        saveValues(sendTranslationPanel);
+                        String subject = sendTranslationPanel.getSubjectFormattedTextField();
+                        String name = sendTranslationPanel.getNameFormattedTextField();
+                        String from = sendTranslationPanel.getEmailFormattedTextField();
+                        String message = sendTranslationPanel.getMessageTextArea();
+                        String to = sendTranslationPanel.getMailToFormattedTextField();
+
+                        Thread t = new Thread(new SendMessageWorker(name, from, to, subject, message, zipOutputFile));
+                        t.start();
+                    }
+                }
+            }
+
+            return true;
+
         } else {
             return true;
         }
@@ -98,7 +157,7 @@ public class Installer extends ModuleInstall {
                 String fileName = "";
                 File bundleFile = null;
                 URL url;
-
+                
                 dirName = modulePreferences.get("Dossier", System.getProperty("user.dir"));
                 fileName = modulePreferences.get("Fichier", "Ancestris_Bundles.zip");
                 bundleFile = new File(dirName + System.getProperty("file.separator") + fileName);
@@ -203,6 +262,14 @@ public class Installer extends ModuleInstall {
                     }
                 }
 
+                // set mail properties
+                if (modulePreferences.get("mail.host", "").equals("")) {
+                    NotifyDescriptor nd1 = new NotifyDescriptor.Message(NbBundle.getMessage(SendTranslationPanel.class, "SendTranslationPanel.msg.setParameters"), NotifyDescriptor.INFORMATION_MESSAGE);
+                    DialogDisplayer.getDefault().notify(nd1);
+                    OptionsDisplayer.getDefault().open("SendTranslation");
+                }
+
+
                 // Tip of the day
                 TipOfTheDay tipOfTheDay = new TipOfTheDay();
 
@@ -217,5 +284,20 @@ public class Installer extends ModuleInstall {
         String locale[] = (str + "__").split("_", 3);
 
         return new Locale(locale[0], locale[1], locale[2]);
+    }
+
+    private void setDefaultValues(SendTranslationPanel sendTranslationPanel) {
+        sendTranslationPanel.setMailToFormattedTextField(modulePreferences.get("mailto.address", "francois@ancestris.org"));
+        sendTranslationPanel.setNameFormattedTextField(modulePreferences.get("mail.name", ""));
+        sendTranslationPanel.setEmailFormattedTextField(modulePreferences.get("mail.address", ""));
+        String TS = new SimpleDateFormat(NbBundle.getMessage(SendTranslationAction.class, "SendTranslationAction.msg.subject.date")).format(new Date());
+        String subject = "[" + NbBundle.getMessage(SendTranslationAction.class, "SendTranslationAction.msg.subject.tag", fromLocale, toLocale) + " " + TS + "] ";
+        sendTranslationPanel.setSubjectFormattedTextField(subject);
+    }
+
+    private void saveValues(SendTranslationPanel sendTranslationPanel) {
+        modulePreferences.put("mail.name", sendTranslationPanel.getNameFormattedTextField());
+        modulePreferences.put("mail.address", sendTranslationPanel.getEmailFormattedTextField());
+        modulePreferences.put("mailto.address", sendTranslationPanel.getMailToFormattedTextField());
     }
 }
