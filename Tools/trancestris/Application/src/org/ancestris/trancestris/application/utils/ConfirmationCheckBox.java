1e968d29b3c0c07619b13d0d1ed8d268186303c3
lemovice <lemovice@ancestris.org>
2012-09-09 09:37:18 +0000

    remove tip of the day Action should be provide by Tip of the day module if needed replace dependency on org.jdesktop.swingx library by dependency on Tip of the day Module move ConfimationCheckBox to utils

--- Tools/trancestris/Application/src/org/ancestris/trancestris/application/utils/ConfirmationCheckBox.java
+++ Tools/trancestris/Application/src/org/ancestris/trancestris/application/utils/ConfirmationCheckBox.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.ancestris.trancestris.application;
+package org.ancestris.trancestris.application.utils;
 
 /**
  *
