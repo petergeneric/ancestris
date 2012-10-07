d4c1f8061b3cac1f9880b388b8559df0494715cd
lemovice <lemovice@ancestris.org>
2012-10-07 13:47:06 +0000

    First version of trancestris help

---
+++ Tools/trancestris/Help/src/org/ancestris/trancestris/modules/help/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "help-hs.xml", position = 3333)
+package org.ancestris.trancestris.modules.help.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
