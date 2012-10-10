badb44b9d5863942e971cbdb92bd044deb3d7735
JoPol <JoPol@ancestris.org>
2012-10-10 15:06:05 +0000

    semi automated download for mashups

---
+++ AncestrisExtensions/report.rdf/src/genjreports/rdf/semweb/Predicate.java
@@ -0,0 +1,47 @@
+// @formatter:off
+/*
+ * Copyright 2012, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
+ */
+// @formatter:on
+package genjreports.rdf.semweb;
+
+import static com.hp.hpl.jena.rdf.model.ResourceFactory.createProperty;
+import static genjreports.rdf.semweb.Prefix.*;
+
+import com.hp.hpl.jena.rdf.model.Property;
+
+public enum Predicate
+{
+    seeAlso(rdfs), //
+    sameAs(rdfs), //
+    hasLabel(rdfs), //
+    isDefinedBy(rdfs), //
+    ;
+    private final Prefix prefix;
+    private final Property property;
+
+    Predicate(final Prefix prefix)
+    {
+        this.prefix = prefix;
+        this.property = createProperty(prefix.uri + name());
+    }
+
+    public String toString()
+    {
+        return prefix + ":" + name();
+    }
+
+    public Property property()
+    {
+        return property;
+    }
+}
---
+++ AncestrisExtensions/report.rdf/src/genjreports/rdf/semweb/Prefix.java
@@ -0,0 +1,37 @@
+// @formatter:off
+/*
+ * Copyright 2012, J. Pol
+ *
+ * This file is part of free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ *
+ * This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. A copy of the GNU General Public License is
+ * available at <http://www.gnu.org/licenses/>.
+ */
+// @formatter:on
+package genjreports.rdf.semweb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Prefix
+{
+    xsd("http://www.w3.org/2001/XMLSchema#"), //
+    rdfs("http://www.w3.org/2000/01/rdf-schema#"), //
+    ;
+    public final String uri;
+    public static final Map<String, String> NAME_ID_MAP = new HashMap<String, String>();
+    static
+    {
+        for (Prefix p : values())
+            NAME_ID_MAP.put(p.name(), p.uri);
+    }
+
+    private Prefix(final String uri)
+    {
+        this.uri = uri;
+    }
+}
