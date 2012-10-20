b5ef1308232e3cf68f2aec74aa1223c3f28d6c03
lemovice <lemovice@ancestris.org>
2012-10-20 14:20:57 +0000

    Add a wizard for file selection

---
+++ AncestrisExtensions/gedcom.mergefile/src/ancestris/modules/gedcom/mergefile/GedcomMergeWizardAction.java
@@ -0,0 +1,97 @@
+package ancestris.modules.gedcom.mergefile;
+
+import ancestris.gedcom.GedcomDirectory;
+import ancestris.gedcom.GedcomMgr;
+import genj.gedcom.Context;
+import genj.gedcom.Gedcom;
+import genj.gedcom.Indi;
+import genj.util.Origin;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.NbBundle;
+
+@ActionID(category = "Tools", id = "ancestris.modules.gedcom.merge.GedcomMergeAction")
+@ActionRegistration(iconBase = "ancestris/modules/gedcom/merge/arrow_merge_270_left.png",
+displayName = "#CTL_GedcomMergeAction")
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools/Gedcom", position = 3333)
+})
+@NbBundle.Messages("CTL_GedcomMergeAction=Merge 2 Gedcom")
+public final class GedcomMergeWizardAction implements ActionListener {
+
+    private final static Logger LOG = Logger.getLogger(GedcomMergeWizardAction.class.getName(), null);
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        List<WizardDescriptor.Panel<WizardDescriptor>> panels = new ArrayList<WizardDescriptor.Panel<WizardDescriptor>>();
+        panels.add(new GedcomMergeWizardPanel1());
+        panels.add(new GedcomMergeWizardPanel2());
+        panels.add(new GedcomMergeWizardPanel3());
+        panels.add(new GedcomMergeWizardPanel4());
+        String[] steps = new String[panels.size()];
+        for (int i = 0; i < panels.size(); i++) {
+            Component c = panels.get(i).getComponent();
+            // Default step name to component name of panel.
+            steps[i] = c.getName();
+            if (c instanceof JComponent) { // assume Swing components
+                JComponent jc = (JComponent) c;
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, i);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps);
+                jc.putClientProperty(WizardDescriptor.PROP_AUTO_WIZARD_STYLE, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DISPLAYED, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_NUMBERED, true);
+            }
+        }
+        WizardDescriptor wiz = new WizardDescriptor(new WizardDescriptor.ArrayIterator<WizardDescriptor>(panels));
+        // {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
+        wiz.setTitleFormat(new MessageFormat("{0}"));
+        wiz.setTitle("...dialog title...");
+        if (DialogDisplayer.getDefault().notify(wiz) == WizardDescriptor.FINISH_OPTION) {
+            Context mergedGedcomContext;
+
+            GedcomMergeWizardPanel2 gedcomMergeWizardPanel2 = (GedcomMergeWizardPanel2) panels.get(1);
+            Context gedcomAContext = gedcomMergeWizardPanel2.getComponent().getGedcomContext();
+
+            GedcomMergeWizardPanel3 gedcomMergeWizardPanel3 = (GedcomMergeWizardPanel3) panels.get(2);
+            Context gedcomBContext = gedcomMergeWizardPanel3.getComponent().getGedcomContext();
+
+            GedcomMergeWizardPanel4 gedcomMergeWizardPanel4 = (GedcomMergeWizardPanel4) panels.get(3);
+            File gedcomMergeFile = gedcomMergeWizardPanel4.getComponent().getGedcomMergeFile();
+
+
+            // form the origin
+            Gedcom mergedGedcom;
+            try {
+                mergedGedcom = new Gedcom(Origin.create(gedcomMergeFile.toURI().toURL()));
+            } catch (MalformedURLException ex) {
+                LOG.log(Level.WARNING, "unexpected exception creating new gedcom", ex);
+                return;
+            }
+            new GedcomMerge().merge(gedcomAContext.getGedcom(), gedcomBContext.getGedcom(), mergedGedcom);
+            mergedGedcomContext = GedcomMgr.getDefault().setGedcom(mergedGedcom);
+            Indi firstIndi = (Indi) mergedGedcomContext.getGedcom().getFirstEntity(Gedcom.INDI);
+
+            // save gedcom file
+            GedcomMgr.getDefault().saveGedcom(new Context(firstIndi), FileUtil.toFileObject(gedcomMergeFile));
+
+            // and reopens the file
+            GedcomDirectory.getDefault().openGedcom(FileUtil.toFileObject(gedcomMergeFile));
+        }
+    }
+}
---
+++ AncestrisExtensions/gedcom.mergefile/src/ancestris/modules/gedcom/mergefile/GedcomMergeWizardPanel2.java
@@ -0,0 +1,57 @@
+package ancestris.modules.gedcom.mergefile;
+
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+
+public class GedcomMergeWizardPanel2 implements WizardDescriptor.Panel<WizardDescriptor> {
+
+    /**
+     * The visual component that displays this panel. If you need to access the
+     * component from this class, just use getComponent().
+     */
+    private GedcomMergeVisualPanel2 component;
+
+    // Get the visual component for the panel. In this template, the component
+    // is kept separate. This can be more efficient: if the wizard is created
+    // but never displayed, or not all panels are displayed, it is better to
+    // create only those which really need to be visible.
+    @Override
+    public GedcomMergeVisualPanel2 getComponent() {
+        if (component == null) {
+            component = new GedcomMergeVisualPanel2();
+        }
+        return component;
+    }
+
+    @Override
+    public HelpCtx getHelp() {
+        // Show no Help button for this panel:
+        return HelpCtx.DEFAULT_HELP;
+        // If you have context help:
+        // return new HelpCtx("help.key.here");
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void removeChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void readSettings(WizardDescriptor wiz) {
+        // use wiz.getProperty to retrieve previous panel state
+    }
+
+    @Override
+    public void storeSettings(WizardDescriptor wiz) {
+        // use wiz.putProperty to remember current panel state
+    }
+}
