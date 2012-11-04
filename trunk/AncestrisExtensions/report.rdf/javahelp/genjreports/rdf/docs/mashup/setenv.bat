6d66a67bbea00fa004c5e8e791d336ff55144fab
JoPol <JoPol@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-11-04 13:50:28 +0000

    replaced having to install jena command line tools by having to download jar files (until delivered)

--- trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.bat
+++ trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.bat
@@ -1,47 +1,20 @@
-@set MODS=C:\Users\user\Appdata\Roaming\.ancestris\trunk\modules
-@set JENAROOT=D:\apache-jena-2.7.3\apache-jena-2.7.3
-
-@rem # you may also want to customise the java command at the end
-@rem ################### en customize
+set MODS=C:\Users\user\Appdata\Roaming\.ancestris\trunk
+set APP=C:\Program Files\ancestris
 
 @echo off
 
-echo MODS=%MODS%
-echo JENAROOT=%JENAROOT%
-echo input=%1
-echo output=%2
-
-if EXIST %JENAROOT% goto :okJena
-echo please fix: JENAROOT does not exist
-pause
-exit /B
-:okJena
-
 if EXIST %MODS% goto :okMods
-echo please fix: MODS does not exist
+echo please fix me: MODS does not exist
 pause
 exit /B
 :okMods
 
-set CLASSPATH=.;%MODS%\genjreports-rdf.jar
+if EXIST %APP% goto :okApp
+echo please fix me: MODS does not exist
+pause
+exit /B
+:okApp
 
-rem CLASSPATH=%CLASSPATH%;%MODS%\ext\arq-2.8.7.jar
-rem CLASSPATH=%CLASSPATH%;%MODS%\ext\iri-0.8.jar
-rem CLASSPATH=%CLASSPATH%;%MODS%\ext\jena-2.6.4.jar
-set CLASSPATH=%CLASSPATH%;%MODS%\ext\log4j-1.2.13.jar
-set CLASSPATH=%CLASSPATH%;%MODS%\ext\slf4j-api-1.5.8.jar
-set CLASSPATH=%CLASSPATH%;%MODS%\ext\slf4j-log4j12-1.5.8.jar
-rem CLASSPATH=%CLASSPATH%;%MODS%\ext\xercesImpl-2.7.1.jar
-
-set CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\jena-arq-2.9.3.jar
-set CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\jena-core-2.7.3.jar
-set CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\jena-iri-0.9.3.jar
-rem CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\log4j-1.2.13.jar
-rem CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\slf4j-api-1.5.8.jar
-rem CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\slf4j-log4j12-1.6.4.jar
-set CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\xercesImpl-2.10.0.jar
-set CLASSPATH=%CLASSPATH%;%JENAROOT%\lib\xml-apis-1.4.01.jar
-
-set JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:%JENAROOT%/jena-log4j.properties
-
+set CLASSPATH=%MODS%\modules\genjreports-rdf.jar;%APP%\ancestris\modules\*;%APP%\platform\*;%APP%\platform\lib\*
+set JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:log4j.properties
 echo on
