4e91142c1ba6e8722dbe2ff7a8f10260483190f7
JoPol <JoPol@ancestris.org>
2012-11-04 13:50:28 +0000

    replaced having to install jena command line tools by having to download jar files (until delivered)

--- AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.sh
+++ AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.sh
@@ -1,22 +1,5 @@
-set JENAROOT=/PATH_TO/apache-jena-2.7.3
-set MODS=/PATH_TO/.ancestris/trunk/modules
+set MODS=/PATH_TO/.ancestris/trunk
+set APP=/PATH_TO/ancestris
 
-set CLASSPATH=$CLASSPATH:$MODS/genjreports-rdf.jar
-
-#set CLASSPATH=$CLASSPATH$;$MODS$/ext/arq-2.8.7.jar
-#set CLASSPATH=$CLASSPATH$;$MODS$/ext/iri-0.8.jar
-#set CLASSPATH=$CLASSPATH$;$MODS$/ext/jena-2.6.4.jar
- set CLASSPATH=$CLASSPATH$;$MODS$/ext/log4j-1.2.13.jar
- set CLASSPATH=$CLASSPATH$;$MODS$/ext/slf4j-api-1.5.8.jar
- set CLASSPATH=$CLASSPATH$;$MODS$/ext/slf4j-log4j12-1.5.8.jar
-#set CLASSPATH=$CLASSPATH$;$MODS$/ext/xercesImpl-2.7.1.jar
-
- set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/jena-arq-2.9.3.jar
- set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/jena-core-2.7.3.jar
- set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/jena-iri-0.9.3.jar
-#set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/log4j-1.2.13.jar
-#set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/slf4j-api-1.5.8.jar
-#set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/slf4j-log4j12-1.6.4.jar
- set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/xercesImpl-2.10.0.jar
- set CLASSPATH=$CLASSPATH$;$JENAROOT$/lib/xml-apis-1.4.01.jar
-java $OPTS$ genjreports.rdf.semweb.Mashup kennedyMashupInput.txt http://my.domain.com/gedcom/places/ kennedyMashupOutput.n3 "de|fr"
+export CLASSPATH=$MODS$/modules/genjreports-rdf.jar;$APP$/ancestris/modules/*;$APP$/platform/*;$APP$/platform/lib/*
+export JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:log4j.properties
