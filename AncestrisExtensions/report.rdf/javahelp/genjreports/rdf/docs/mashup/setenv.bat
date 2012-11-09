55592686756effc7614df90359eb47ba27d49db5
JoPol <JoPol@ancestris.org>
2012-11-09 18:44:32 +0000

    fixed class not found, but gedsem.Convert still broke with NullPointerException

--- AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.bat
+++ AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.bat
@@ -1,20 +1,19 @@
-set MODS=C:\Users\user\Appdata\Roaming\.ancestris\trunk
-set APP=C:\Program Files\ancestris
+set APP=C:\Users\Falkink\Documents\ancestris-20121002\ancestris
+set MODS=C:\Users\Falkink\Appdata\Roaming\.ancestris\trunk
 
-@echo off
-
-if EXIST %MODS% goto :okMods
-echo please fix me: MODS does not exist
-pause
-exit /B
-:okMods
-
-if EXIST %APP% goto :okApp
-echo please fix me: MODS does not exist
-pause
-exit /B
-:okApp
-
-set CLASSPATH=%MODS%\modules\genjreports-rdf.jar;%APP%\ancestris\modules\*;%APP%\platform\*;%APP%\platform\lib\*
+rem ### required for semweb 
+set CLASSPATH=
+set CLASSPATH=%CLASSPATH%;%MODS%\modules\genjreports-rdf.jar
+set CLASSPATH=%CLASSPATH%;%MODS%\modules\docs\genjreports-rdf.jar
+set CLASSPATH=%CLASSPATH%;%MODS%\modules\ext\*
 set JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:log4j.properties
-echo on
+
+rem ### required for gedsem 
+set CLASSPATH=%CLASSPATH%;%APP%\ancestris\modules\ancestris-core.jar
+set CLASSPATH=%CLASSPATH%;%APP%\ancestris\modules\ancestris-libs-genj.jar
+set CLASSPATH=%CLASSPATH%;%APP%\platform\lib\*
+set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.user.dir=%APP%\bin
+set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.home=%APP%\platform
+set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.user=%MODS%
+set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.dirs=%APP%\ancestris
+
