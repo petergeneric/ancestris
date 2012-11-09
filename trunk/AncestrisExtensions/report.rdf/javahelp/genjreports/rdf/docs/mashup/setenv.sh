27eaddb2a169ad942f0cfdbc53122e3583ebfed3
JoPol <JoPol@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-11-09 18:44:32 +0000

    fixed class not found, but gedsem.Convert still broke with NullPointerException

--- trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.sh
+++ trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/setenv.sh
@@ -1,5 +1,18 @@
-set MODS=/PATH_TO/.ancestris/trunk
-set APP=/PATH_TO/ancestris
+export MODS=/PATH_TO/.ancestris/trunk
+export APP=/PATH_TO/ancestris
 
-export CLASSPATH=$MODS$/modules/genjreports-rdf.jar;$APP$/ancestris/modules/*;$APP$/platform/*;$APP$/platform/lib/*
+### required for semweb 
+export CLASSPATH=
+export CLASSPATH=$CLASSPATH$,$MODS$/modules/genjreports-rdf.jar
+export CLASSPATH=$CLASSPATH$,$MODS$/modules/docs/genjreports-rdf.jar
+export CLASSPATH=$CLASSPATH$,$MODS$/modules/ext/*
 export JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:log4j.properties
+
+### additional requirements for gedsem 
+export CLASSPATH=$CLASSPATH$,$APP$/ancestris/modules/ancestris-core.jar
+export CLASSPATH=$CLASSPATH$,$APP$/ancestris/modules/ancestris-libs-genj.jar
+export CLASSPATH=$CLASSPATH$,$APP$/platform/lib/*
+export JAVA_OPTS=$JAVA_OPTS$ -Dnetbeans.user.dir=$APP$/bin
+export JAVA_OPTS=$JAVA_OPTS$ -Dnetbeans.home=$APP$/platform
+export JAVA_OPTS=$JAVA_OPTS$ -Dnetbeans.user=$MODS$
+export JAVA_OPTS=$JAVA_OPTS$ -Dnetbeans.dirs=$APP$/ancestris
