fe3d3fe1520f9817cf508cb3e44abbfeda0e29a8
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-11-09 12:19:12 +0000

    Improve matchers

--- trunk/AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/FamMatcher.java
+++ trunk/AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/FamMatcher.java
@@ -15,15 +15,15 @@ public class FamMatcher extends EntityMatcher<Fam> {
         Indi leftHusband = left.getHusband();
         Indi rightHusband = right.getHusband();
         if (leftHusband != null && rightHusband != null) {
-            if (leftHusband.compareTo(rightHusband) == 0) {
+            if (new IndiMatcher().compare(leftHusband, rightHusband) >= 80) {
                 Indi leftWife = left.getWife();
                 Indi rightWife = right.getWife();
                 if (leftWife != null && rightWife != null) {
-                    if (leftWife.compareTo(rightWife) == 0) {
+                    if (new IndiMatcher().compare(leftWife,rightWife) >= 80) {
                         PropertyDate leftwhen = left.getWhen();
                         PropertyDate rightwhen = right.getWhen();
                         if (leftwhen.isComparable() && rightwhen.isComparable()) {
-                            if (leftwhen.compareTo(rightwhen) == 0) {
+                            if (leftwhen.compareTo(rightwhen) <= 4000) {
                                 // should we compare chlidrens ?
                                 return 100;
                             }
