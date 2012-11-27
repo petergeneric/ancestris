c53c5af53e3c32641f5ad9b520fa65e3eff26427
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2012-11-27 22:21:58 +0000

    context menu logic for upcomming PropertyNodes

---
+++ trunk/AncestrisCore/core/src/ancestris/core/actions/CommonActions.java
@@ -0,0 +1,39 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2012 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package ancestris.core.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import org.openide.awt.DynamicMenuContent;
+
+/**
+ *
+ * @author daniel
+ */
+public class CommonActions {
+
+    public static final Action NOOP = new NoOpAction();
+
+    private static class NoOpAction extends AbstractAction {
+
+        public NoOpAction() {
+            setEnabled(false);
+            putValue(DynamicMenuContent.HIDE_WHEN_DISABLED, true);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //noop
+        }
+    }
+}
