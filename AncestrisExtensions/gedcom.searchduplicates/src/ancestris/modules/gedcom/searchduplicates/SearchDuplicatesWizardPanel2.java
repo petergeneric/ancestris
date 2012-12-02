e82969d81730b01b2561fcb03ee08b56e640b679
lemovice <lemovice@ancestris.org>
2012-12-02 15:21:04 +0000

    rename module to search duplicates, and rework duplicates display

---
+++ AncestrisExtensions/gedcom.searchduplicates/src/ancestris/modules/gedcom/searchduplicates/SearchDuplicatesWizardPanel2.java
@@ -0,0 +1,56 @@
+package ancestris.modules.gedcom.searchduplicates;
+
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+
+public class SearchDuplicatesWizardPanel2 implements WizardDescriptor.Panel<WizardDescriptor> {
+
+    /**
+     * The visual component that displays this panel. If you need to access the
+     * component from this class, just use getComponent().
+     */
+    private SearchDuplicatesVisualPanel2 component;
+
+    // Get the visual component for the panel. In this template, the component
+    // is kept separate. This can be more efficient: if the wizard is created
+    // but never displayed, or not all panels are displayed, it is better to
+    // create only those which really need to be visible.
+    @Override
+    public SearchDuplicatesVisualPanel2 getComponent() {
+        if (component == null) {
+            component = new SearchDuplicatesVisualPanel2();
+        }
+        return component;
+    }
+
+    @Override
+    public HelpCtx getHelp() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void removeChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void readSettings(WizardDescriptor wiz) {
+        // use wiz.getProperty to retrieve previous panel state
+    }
+
+    @Override
+    public void storeSettings(WizardDescriptor wiz) {
+        // use wiz.putProperty to remember current panel state
+        wiz.putProperty("selectedEntities", ((SearchDuplicatesVisualPanel2) getComponent()).getSelectedEntities());
+        wiz.putProperty("selectedOptions", ((SearchDuplicatesVisualPanel2) getComponent()).getSelectedOptions());
+    }
+}
