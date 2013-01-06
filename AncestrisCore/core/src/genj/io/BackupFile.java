984f2442a5cee9c29fe3172b17b32c1784c75af7
dandre <dandre@ancestris.org>
2013-01-06 22:33:21 +0000

    fix error on windows platform when #backups is 0

--- AncestrisCore/core/src/genj/io/BackupFile.java
+++ AncestrisCore/core/src/genj/io/BackupFile.java
@@ -1,6 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2012 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 package genj.io;
 
@@ -17,66 +24,73 @@ import java.util.regex.Pattern;
  * @author daniel
  */
 public class BackupFile {
+
     static private final SimpleDateFormat ISO_FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss");
 
+    public static boolean createBackup(File file) throws GedcomIOException {
+        try {
+            if ((Options.getNbBackups() != 0) && file.exists()) {
+                final Pattern p = Pattern.compile(getBasename(file.getName()) + "_([0-9]{8}-[0-9]{6})" + getExtension(file.getName()));
+                final File theFile = file;
+                File bak;
 
-    public static void createBackup(File file) throws GedcomIOException {
-        if ((Options.getNbBackups()!= 0) && file.exists()) {
-            final Pattern p = Pattern.compile(getBasename(file.getName()) + "_([0-9]{8}-[0-9]{6})" + getExtension(file.getName()));
-            final File theFile = file;
-            File bak;
+                FilenameFilter filter = new FilenameFilter() {
 
-            FilenameFilter filter = new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                        return (dir.equals(theFile.getParentFile()) && p.matcher(name).matches());
+                    }
+                };
+                File parent = file.getParentFile();
+                File[] backups = parent.listFiles(filter);
+                Arrays.sort(backups);
 
-                public boolean accept(File dir, String name) {
-                    return (dir.equals(theFile.getParentFile()) && p.matcher(name).matches());
-                }
-            };
-            File parent = file.getParentFile();
-            File[] backups = parent.listFiles(filter);
-            Arrays.sort(backups);
-
-            // TODO: mettre une options
-            if (backups.length >= Options.getNbBackups()) {
-                for (int i = 0; i <= backups.length - Options.getNbBackups(); i++) {
-                    if (!backups[i].delete()) {
-                        throw new GedcomIOException("Couldn't delete backup file " + backups[0].getName(), -1);
+                // TODO: mettre une options
+                if (backups.length >= Options.getNbBackups()) {
+                    for (int i = 0; i <= backups.length - Options.getNbBackups(); i++) {
+                        if (!backups[i].delete()) {
+                            throw new GedcomIOException("Couldn't delete backup file " + backups[0].getName(), -1);
+                        }
                     }
                 }
-            }
-            if (backups.length > 0) {
-                Matcher m = p.matcher(backups[backups.length - 1].getName());
-                if (!m.matches()) {
-                    throw new GedcomIOException("Couldn't create backup for file " + file.getName(), -1);
+                if (backups.length > 0) {
+                    Matcher m = p.matcher(backups[backups.length - 1].getName());
+                    if (!m.matches()) {
+                        throw new GedcomIOException("Couldn't create backup for file " + file.getName(), -1);
+                    }
+                    bak = getBackupFile(file);
+                } else {
+                    bak = getBackupFile(file);
                 }
-                bak = getBackupFile(file);
-            } else {
-                bak = getBackupFile(file);
+                if (bak.exists()) {
+                    throw new GedcomIOException("Couldn't create backup file " + bak.getName(), -1);
+                }
+                if (!file.renameTo(bak)) {
+                    throw new GedcomIOException("Couldn't create backup for " + file.getName(), -1);
+                }
             }
-            if (bak.exists()) {
-                throw new GedcomIOException("Couldn't create backup file " + bak.getName(), -1);
-            }
-            if (!file.renameTo(bak)) {
-                throw new GedcomIOException("Couldn't create backup for " + file.getName(), -1);
-            }
+            return true;
+        } finally {
+            return false;
         }
     }
 
-    private static File getBackupFile(File file){
+    private static File getBackupFile(File file) {
         String fileName = file.getAbsolutePath();
-        return new File(getBasename(fileName)+"_"+
-                ISO_FORMAT.format(new Date())+
-                getExtension(fileName));
+        return new File(getBasename(fileName) + "_"
+                + ISO_FORMAT.format(new Date())
+                + getExtension(fileName));
     }
 
     // From Util.getExtension
     /** Gets the extension of a specified file name. The extension is
-    * everything after the last dot. The extension includes the dot char so that
-    * fileName = Basename+Extension
-    *
-    * @param fileName name of the file
-    * @return extension of the file (or <code>""</code> if it had none)
-    */
+     * everything after the last dot. The extension includes the dot char so that
+     * fileName = Basename+Extension
+     *
+     * @param fileName name of the file
+     *
+     * @return extension of the file (or
+     * <code>""</code> if it had none)
+     */
     private static String getExtension(String fileName) {
         int index = fileName.lastIndexOf("."); // NOI18N
 
@@ -88,20 +102,20 @@ public class BackupFile {
     }
 
     /** Gets the basename of a specified file name. The basename is
-    * everything before the last dot.
-    *
-    * @param fileName name of the file
-    * @return basename of the file (or <code>fileName</code> if it had none)
-    */
+     * everything before the last dot.
+     *
+     * @param fileName name of the file
+     *
+     * @return basename of the file (or
+     * <code>fileName</code> if it had none)
+     */
     private static String getBasename(String fileName) {
         int index = fileName.lastIndexOf("."); // NOI18N
 
         if (index == -1) {
             return fileName; // NOI18N
         } else {
-            return fileName.substring(0,index);
+            return fileName.substring(0, index);
         }
     }
-
-
 }
