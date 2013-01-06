a5d184c8b5f813c5f1ac38bb72a0e9f1983c4567
JoPol <JoPol@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-01-06 11:42:27 +0000

    attempt to adjust the classpath

--- trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/reports.bat
+++ trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/reports.bat
@@ -1,74 +1,54 @@
 @echo off
-rem #####################################################
-rem ### replace USERNAME; put the proper duo last
-rem ### depending on the configuration of you system
-rem ### you might need other paths to ancestris
-rem ###--------------------------------------------------
-set APP=C:\Program%20Files\ancestris
-set MODS=C:\Users\USERNAME\Appdata\Roaming\.ancestris\0.8
-rem ###
-set APP=C:\Users\USERNAME\Documents\ancestris-20121002\ancestris
-set MODS=C:\Users\USERNAME\Appdata\Roaming\.ancestris\trunk
-rem #####################################################
-rem ### put the desired file last
-rem ###--------------------------------------------------
-set GED=%APP%\ancestris\exemples\gen-bourbon\bourbon.ged
-set GED=%APP%\ancestris\exemples\gen-kennedy\kennedy.ged
-rem ###
-rem #####################################################
-rem ### typically where you publish your gedcom
-rem ### do not omit the terminator
-rem ###--------------------------------------------------
-set URI=http://my.domain.com/gedcom/
-rem #####################################################
 
-rem ### required for semweb 
+rem #### put your choice last, adjust username and version numbers
+set  APP=C:\Program%20Files\ancestris
+set MODS=C:\Users\USERNAME\Appdata\Roaming\.ancestris\0.8
+rem
+set  APP=C:\Users\USERNAME\Documents\ancestris-20121002\ancestris
+set MODS=C:\Users\USERNAME\Appdata\Roaming\.ancestris\trunk
+
+rem #### required for semweb 
 set CLASSPATH=
 set CLASSPATH=%CLASSPATH%;%MODS%\modules\genjreports-rdf.jar
 set CLASSPATH=%CLASSPATH%;%MODS%\modules\docs\genjreports-rdf.jar
 set CLASSPATH=%CLASSPATH%;%MODS%\modules\ext\*
 set JAVA_OPTS= -Xmx1024M -Dlog4j.configuration=file:log4j.properties
 
-rem ### required for gedsem 
+rem #### required for gedsem 
 set CLASSPATH=%CLASSPATH%;%APP%\ancestris\modules\ancestris-core.jar
 set CLASSPATH=%CLASSPATH%;%APP%\ancestris\modules\ancestris-libs-genj.jar
+set CLASSPATH=%CLASSPATH%;%APP%\platform\*
 set CLASSPATH=%CLASSPATH%;%APP%\platform\lib\*
 set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.user.dir=%APP%\bin
 set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.home=%APP%\platform
 set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.user=%MODS%
 set JAVA_OPTS=%JAVA_OPTS% -Dnetbeans.dirs=%APP%\ancestris
 
+set GEDCOM_BOURBON=%APP%\ancestris\exemples\gen-bourbon\bourbon.ged
+set GEDCOM_JFK=%APP%\ancestris\exemples\gen-kennedy\kennedy.ged
+
+rem #### end of environment configuration
+rem #####################################################################
+
+rem #### download http://anonymous:password@arvernes.dyndns.org/fgenj/trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/reports/*Rules.txt
+rem #### download http://anonymous:password@arvernes.dyndns.org/fgenj/trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/mashup/*.arq
+set   DOCS=C:\YOUR_DOWNLOAD_FOLDER
+set    URI=http://my.domain.com/gedcom/
+set GEDCOM=-gedcom %GEDCOM_JFK%
+set  RULES=-rules %DOCS%\reports\SlowRules.txt
+set GEDTTL=gedcom.ttl
+
 echo on
 
-@echo =============
-@echo convert? CTRL-C to abort
-@pause
-java %JAVA_OPTS% genjreports.rdf.gedsem.Convert -gedcom %GED% -uri %URI% -format ttl > gedcom.ttl 
+@rem #### see http://anonymous:password@arvernes.dyndns.org/fgenj/trunk/AncestrisExtensions/report.rdf/javahelp/genjreports/rdf/docs/images/chart.jpg
 
-@echo =============
-@echo collect place names? CTRL-C to abort
-@pause
-java %JAVA_OPTS% genjreports.rdf.semweb.Select mashup.ttl gedcom.ttl mashup.tsv mashup.arq
-
-@echo =============
-@echo PLEASE: add missing GeoNameIDs to mashup.tsv
-@echo ready to collect data from GeoNames and DbPedia? CTRL-C to abort
-@pause
-java %JAVA_OPTS% genjreports.rdf.semweb.Mashup mashup.tsv %URI% mashup.ttl "de|fr"
-
-@echo =============
-@echo create reports? CTRL-C to abort
-@pause
-java %JAVA_OPTS% genjreports.rdf.semweb.Select gedcom.ttl mashup.ttl *.rdf report.txt classmates.arq
-
-@rem ##########################################################
-@rem ### Extend/duplicate the lat semwb.Select to add
-@rem ### the reports you want. You might need to add
-@rem ### "-rules slowRules.txt" to the gedsem.Convert command
-@rem ### 
-@rem ### omit all arguments for more help on the commands
-@rem ### gedsem.Convert, semweb.Mashup, semweb.Select
-@rem ##########################################################
+java %JAVA_OPTS% genjreports.rdf.gedsem.Convert %GEDCOM% -uri %URI% -format ttl 1>gedcom.ttl 
+java %JAVA_OPTS% genjreports.rdf.semweb.Select gedcom.ttl mashup.ttl mashup.tsv %DOCS%\mashup\mashup.arq
+@echo hit any key when ready adding missing GeoNameIDs to mashup.tsv
+@pause >null
+java %JAVA_OPTS% genjreports.rdf.semweb.Mashup gedcom.ttl mashup.ttl %URI% mashup.tsv "de|fr"
+java %JAVA_OPTS% genjreports.rdf.semweb.Select gedcom.ttl mashup.ttl *.rdf report.txt %DOCS%\mashup\classmates.arq
+@rem #### add/delete the reports you want
 
 @pause
 @exit /B
