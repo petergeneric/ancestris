c7b81c923fb681849556fd6e9ba22940900df82f
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-01-11 22:49:16 +0000

    improve display

--- trunk/AncestrisExtensions/familygroups/src/ancestris/modules/familygroups/OpenFamilyGroupsAction.java
+++ trunk/AncestrisExtensions/familygroups/src/ancestris/modules/familygroups/OpenFamilyGroupsAction.java
@@ -1,6 +1,5 @@
 package ancestris.modules.familygroups;
 
-import ancestris.gedcom.GedcomDirectory;
 import ancestris.modules.document.view.DocumentViewTopComponent;
 import genj.fo.Document;
 import genj.gedcom.Context;
@@ -10,15 +9,21 @@ import java.util.prefs.Preferences;
 import org.netbeans.api.options.OptionsDisplayer;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
 import org.openide.util.Utilities;
 
+@ActionID(id = "ancestris.modules.familygroups.OpenFamilyGroupsAction", category = "Tools")
+@ActionRegistration(iconInMenu = true, displayName = "#CTL_OpenFamilyGroups", iconBase = "ancestris/modules/familygroups/FamilyGroups.png")
+@ActionReference(path = "Menu/Tools", name = "ancestris-modules-familygroups-OpenFamilyGroups", position = 82)
 public final class OpenFamilyGroupsAction implements ActionListener {
 
     private DocumentViewTopComponent window = null;
     private Context context;
-    Preferences modulePreferences = NbPreferences.forModule(FamilyGroups.class);
+    Preferences modulePreferences = NbPreferences.forModule(FamilyGroupsPlugin.class);
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -27,12 +32,12 @@ public final class OpenFamilyGroupsAction implements ActionListener {
         context = Utilities.actionsGlobalContext().lookup(Context.class);
         if (context != null) {
             if (modulePreferences.getInt("minGroupSize", -1) == -1) {
-                NotifyDescriptor nd = new NotifyDescriptor.Message(NbBundle.getMessage(FamilyGroups.class, "OpenFamilyGroupsAction.setParameters"), NotifyDescriptor.INFORMATION_MESSAGE);
+                NotifyDescriptor nd = new NotifyDescriptor.Message(NbBundle.getMessage(FamilyGroupsPlugin.class, "OpenFamilyGroupsAction.setParameters"), NotifyDescriptor.INFORMATION_MESSAGE);
                 DialogDisplayer.getDefault().notify(nd);
 
                 OptionsDisplayer.getDefault().open("Extensions/FamilyGroups");
             } else {
-                Document doc = new FamilyGroups().start(context.getGedcom());
+                Document doc = new FamilyGroupsPlugin().start(context.getGedcom());
                 if (doc != null) {
                     window.displayDocument(doc, modulePreferences);
                     window.open();
