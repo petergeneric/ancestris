ca0ddb812cf78dfbefb3b92cea714463bd53f136
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-03-21 19:03:20 +0000

    Create MergeEntities movePropertyRecursively methods

--- trunk/AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/GedcomUtilities.java
+++ trunk/AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/GedcomUtilities.java
@@ -20,16 +20,20 @@ package ancestris.modules.gedcom.utilities;
 
 import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
+import genj.gedcom.GedcomException;
 import genj.gedcom.Property;
+import genj.gedcom.PropertyXRef;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.openide.util.Exceptions;
 
 public class GedcomUtilities {
 
+    private static final Logger log = Logger.getLogger(GedcomUtilities.class.getName());
     public final static int ENT_ALL = 0;
     public final static int ENT_INDI = 1;
     public final static int ENT_FAM = 2;
@@ -89,6 +93,67 @@ public class GedcomUtilities {
                 props.add(child);
             }
             getPropertiesRecursively(child, props, tag);
+        }
+    }
+
+    /*
+     * Merge 2 entities
+     * dest the final entity
+     * src the source entity
+     * properties the property list to be included in the dest entity
+     */
+    public static void MergeEntities(Gedcom gedcom, Entity dest, Entity src, List<Property> properties) {
+        for (Property rightProperty : properties) {
+            try {
+                movePropertyRecursively(rightProperty, dest);
+            } catch (GedcomException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+
+        // Update linked entities
+        for (Entity reference : PropertyXRef.getReferences(src)) {
+            System.out.println("targetEntity:" + reference.getId());
+            for (Iterator<PropertyXRef> it = reference.getProperties(PropertyXRef.class).iterator(); it.hasNext();) {
+                PropertyXRef propertyXRef = it.next();
+                if (propertyXRef.getTargetEntity().equals(src)) {
+                    propertyXRef.unlink();
+                    propertyXRef.setValue(dest.getId());
+                    try {
+                        propertyXRef.link();
+                    } catch (GedcomException e) {
+                        log.log(Level.SEVERE, "unexpected", e);
+                    }
+                }
+            }
+        }
+
+        // delete merged entity
+        gedcom.deleteEntity(src);
+    }
+
+    /*
+     * this function move property propertysrc  in parent property parentPropertyDest
+     * and update Xref properties
+     */
+    public static void movePropertyRecursively(Property propertySrc, Property parentPropertyDest) throws GedcomException {
+        Property propertyDest;
+        if (propertySrc.getMetaProperty().isSingleton()) {
+            propertyDest = parentPropertyDest.getProperty(propertySrc.getTag());
+            if (propertyDest != null) {
+                parentPropertyDest.delProperty(propertyDest);
+            }
+        }
+        propertyDest = parentPropertyDest.addProperty(propertySrc.getTag(), propertySrc.getValue());
+
+        // Move children properties
+        for (Property children : propertySrc.getProperties()) {
+            movePropertyRecursively(children, propertyDest);
+        }
+
+        propertySrc.getParent().delProperty(propertySrc);
+        if (propertyDest instanceof PropertyXRef) {
+            ((PropertyXRef) propertyDest).link();
         }
     }
 }
