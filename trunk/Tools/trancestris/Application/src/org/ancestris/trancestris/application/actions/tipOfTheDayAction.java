92c3e6a5087387769f4b2cf007cce9b8ddf6ea38
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-06-07 21:30:09 +0000

    Correct NPE found by François

--- trunk/Tools/trancestris/Application/src/org/ancestris/trancestris/application/actions/tipOfTheDayAction.java
+++ trunk/Tools/trancestris/Application/src/org/ancestris/trancestris/application/actions/tipOfTheDayAction.java
@@ -7,6 +7,9 @@ package org.ancestris.trancestris.application.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.InputStream;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.ancestris.trancestris.application.Installer;
 import org.ancestris.trancestris.tipoftheday.TipOfTheDay;
 import org.openide.awt.ActionRegistration;
@@ -16,7 +19,7 @@ import org.openide.awt.ActionID;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(category = "Help",
-id = "org.ancestris.trancestris.application.actions.tipOfTheDayAction")
+        id = "org.ancestris.trancestris.application.actions.tipOfTheDayAction")
 @ActionRegistration(displayName = "#CTL_tipOfTheDayAction")
 @ActionReferences({
     @ActionReference(path = "Menu/Help", position = 350)
@@ -24,10 +27,23 @@ id = "org.ancestris.trancestris.application.actions.tipOfTheDayAction")
 @Messages("CTL_tipOfTheDayAction=Tip of the day")
 public final class tipOfTheDayAction implements ActionListener {
 
+    private static final Logger logger = Logger.getLogger(Installer.class.getName());
+
     @Override
     public void actionPerformed(ActionEvent e) {
         //Load the tips into the tip loader:
-        InputStream propertiesIn = Installer.class.getResourceAsStream("tips.properties");
+        String tipsFileName = "resources/tips.properties";
+        String selectedLocale = Locale.getDefault().getLanguage();
+
+        if (selectedLocale.equals("en") == false) {
+            tipsFileName = "resources/tips_" + selectedLocale + ".properties";
+        }
+        logger.log(Level.INFO, "selected tips {0}", tipsFileName);
+        InputStream propertiesIn = Installer.class.getResourceAsStream(tipsFileName);
+        if (propertiesIn == null) {
+            logger.log(Level.INFO, "selected tips {0}", "resources/tips.properties");
+            propertiesIn = Installer.class.getResourceAsStream("resources/tips.properties");
+        }
         new TipOfTheDay(true, propertiesIn);
     }
 }
