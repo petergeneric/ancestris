0a92d6b234170bb533cb8266f74f7d31415d9a0f
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-11-05 20:27:29 +0000

    suppress duplicate links in MergeEntities method

--- trunk/AncestrisExtensions/place.geonames/src/ancestris/modules/place/geonames/GeonamesPlacesList.java
+++ trunk/AncestrisExtensions/place.geonames/src/ancestris/modules/place/geonames/GeonamesPlacesList.java
@@ -2,7 +2,6 @@ package ancestris.modules.place.geonames;
 
 import ancestris.api.place.Place;
 import ancestris.api.place.PlacesList;
-import genj.gedcom.PropertyPlace;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -27,9 +26,9 @@ public class GeonamesPlacesList implements PlacesList {
             WebService.setUserName("lemovice");
 
             ToponymSearchCriteria toponymSearchCriteria = new ToponymSearchCriteria();
+            toponymSearchCriteria.setStyle(Style.FULL);
             toponymSearchCriteria.setQ(place);
             toponymSearchResult = WebService.search(toponymSearchCriteria);
-
 
             for (Toponym toponym : toponymSearchResult.getToponyms()) {
                 PostalCodeSearchCriteria postalCodeSearchCriteria = new PostalCodeSearchCriteria();
@@ -40,7 +39,13 @@ public class GeonamesPlacesList implements PlacesList {
                 postalCodeSearchCriteria.setCountryCode(toponym.getCountryCode());
                 postalCodeSearchResult = WebService.postalCodeSearch(postalCodeSearchCriteria);
 
+                logger.log(Level.INFO, "toponym AdminName1 {0} AdminName2 {1} AdminName3 {2} AdminName4 {3} AdminName5 {4}", new Object[]{toponym.getAdminName1(), toponym.getAdminName2(), toponym.getAdminName3(), toponym.getAdminName4(), toponym.getAdminName5()});
+                logger.log(Level.INFO, "toponym AdminCode1 {0} AdminCode2 {1} AdminCode3 {2} AdminCode4 {3} AdminCode5 {4}", new Object[]{toponym.getAdminCode1(), toponym.getAdminCode2(), toponym.getAdminCode3(), toponym.getAdminCode4(), toponym.getAdminCode5()});
+
                 for (PostalCode postalCode : postalCodeSearchResult) {
+                    logger.log(Level.INFO, "postalCode AdminName1 {0} AdminName2 {1} AdminName3 {2}", new Object[]{postalCode.getAdminName1(), postalCode.getAdminName2(), postalCode.getAdminName3()});
+                    logger.log(Level.INFO, "postalCode AdminCode1 {0} AdminCode2 {1} AdminCode3 {2}", new Object[]{postalCode.getAdminCode1(), postalCode.getAdminCode2(), postalCode.getAdminCode3()});
+
                     placesList.add(new GeonamesPlace(toponym, postalCode));
                 }
             }
