6a19956e09777e51522bd1f2a6846b4ee0e3c7e3
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-11-26 23:36:33 +0000

    Fix disabled Tools/Multimedia/Export Web menu

--- trunk/AncestrisExtensions/exports.website/src/ancestris/modules/exports/website/WebSiteAction.java
+++ trunk/AncestrisExtensions/exports.website/src/ancestris/modules/exports/website/WebSiteAction.java
@@ -12,24 +12,40 @@
 package ancestris.modules.exports.website;
 
 import genj.gedcom.Context;
+import genj.gedcom.Property;
 import genj.report.Report;
 import genj.report.ReportPlugin;
 import genj.report.ReportView;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+
+
+@ActionID(category = "Tools",
+id = "ancestris.modules.exports.website.WebSiteAction")
+@ActionRegistration(
+        displayName = "#CTL_WebSiteAction"
+        ,iconBase="ancestris/modules/exports/website/icone_multimedia_16.png"
+        )
+@ActionReferences({
+    @ActionReference(path = "Menu/Tools/Multimedia", position = -90)
+})
 
 public final class WebSiteAction implements ActionListener {
 
-    private final Context context;
+    private final Property context;
 
-    public WebSiteAction(Context context) {
+    public WebSiteAction(Property context) {
         this.context = context;
     }
 
     public void actionPerformed(ActionEvent ev) {
         Report report = WebSiteExportPlugin.getReport();
 //        ReportView view = AncestrisPlugin.lookup(GenjViewInterface.class).getReportView(context);
-        ReportView view = ReportPlugin.getReportView(context);
+        ReportView view = ReportPlugin.getReportView(new Context(context));
         if (view != null) {
             view.startReport(report, context.getGedcom());
         }
