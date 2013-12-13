e9c5e6f06c7886dc6e3ea2cbcda13d6e469fac17
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-12-13 22:08:05 +0000

    implement search as a worker thread

--- trunk/AncestrisExtensions/place.geonames/src/ancestris/modules/place/geonames/GeonamesPlacesList.java
+++ trunk/AncestrisExtensions/place.geonames/src/ancestris/modules/place/geonames/GeonamesPlacesList.java
@@ -1,59 +1,77 @@
 package ancestris.modules.place.geonames;
 
 import ancestris.api.place.Place;
-import ancestris.api.place.PlacesList;
-import java.util.ArrayList;
+import ancestris.api.place.SearchPlace;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.geonames.*;
+import org.openide.util.RequestProcessor;
+import org.openide.util.Task;
+import org.openide.util.TaskListener;
 
 /**
  *
  * @author dominique
  */
-public class GeonamesPlacesList implements PlacesList {
+public class GeonamesPlacesList implements SearchPlace {
 
-    private ArrayList<Place> placesList = new ArrayList<Place>();
     private final static Logger logger = Logger.getLogger(GeonamesPlacesList.class.getName(), null);
 
     @Override
-    public List<Place> findPlace(String place) {
+    public void searchPlace(final String searchedPlace, final List<Place> placesList) {
+        RequestProcessor.Task theTask;
+        RequestProcessor RP = new RequestProcessor("interruptible tasks", 1, true);
+        Runnable runnable = new Runnable() {
 
-        ToponymSearchResult toponymSearchResult;
+            @Override
+            public synchronized void run() {
+                if (searchedPlace.isEmpty() == false) {
+                    ToponymSearchResult toponymSearchResult;
 
-        try {
-            WebService.setUserName("lemovice");
+                    try {
+                        WebService.setUserName("lemovice");
 
-            ToponymSearchCriteria toponymSearchCriteria = new ToponymSearchCriteria();
-            toponymSearchCriteria.setStyle(Style.FULL);
-            toponymSearchCriteria.setQ(place);
-            toponymSearchResult = WebService.search(toponymSearchCriteria);
+                        ToponymSearchCriteria toponymSearchCriteria = new ToponymSearchCriteria();
+                        toponymSearchCriteria.setStyle(Style.FULL);
+                        toponymSearchCriteria.setQ(searchedPlace);
+                        toponymSearchResult = WebService.search(toponymSearchCriteria);
 
-            for (Toponym toponym : toponymSearchResult.getToponyms()) {
-                PostalCodeSearchCriteria postalCodeSearchCriteria = new PostalCodeSearchCriteria();
-                postalCodeSearchCriteria.setStyle(Style.FULL);
-                List<PostalCode> postalCodeSearchResult;
+                        for (Toponym toponym : toponymSearchResult.getToponyms()) {
+                            PostalCodeSearchCriteria postalCodeSearchCriteria = new PostalCodeSearchCriteria();
+                            postalCodeSearchCriteria.setStyle(Style.FULL);
+                            postalCodeSearchCriteria.setPlaceName(toponym.getName());
+                            postalCodeSearchCriteria.setCountryCode(toponym.getCountryCode());
 
-                postalCodeSearchCriteria.setPlaceName(toponym.getName());
-                postalCodeSearchCriteria.setCountryCode(toponym.getCountryCode());
-                postalCodeSearchResult = WebService.postalCodeSearch(postalCodeSearchCriteria);
+                            List<PostalCode> postalCodeSearchResult;
+                            postalCodeSearchResult = WebService.postalCodeSearch(postalCodeSearchCriteria);
 
-                logger.log(Level.INFO, "toponym AdminName1 {0} AdminName2 {1} AdminName3 {2} AdminName4 {3} AdminName5 {4}", new Object[]{toponym.getAdminName1(), toponym.getAdminName2(), toponym.getAdminName3(), toponym.getAdminName4(), toponym.getAdminName5()});
-                logger.log(Level.INFO, "toponym AdminCode1 {0} AdminCode2 {1} AdminCode3 {2} AdminCode4 {3} AdminCode5 {4}", new Object[]{toponym.getAdminCode1(), toponym.getAdminCode2(), toponym.getAdminCode3(), toponym.getAdminCode4(), toponym.getAdminCode5()});
+                            logger.log(Level.INFO, "toponym AdminName1 {0} AdminName2 {1} AdminName3 {2} AdminName4 {3} AdminName5 {4}", new Object[]{toponym.getAdminName1(), toponym.getAdminName2(), toponym.getAdminName3(), toponym.getAdminName4(), toponym.getAdminName5()});
+                            logger.log(Level.INFO, "toponym AdminCode1 {0} AdminCode2 {1} AdminCode3 {2} AdminCode4 {3} AdminCode5 {4}", new Object[]{toponym.getAdminCode1(), toponym.getAdminCode2(), toponym.getAdminCode3(), toponym.getAdminCode4(), toponym.getAdminCode5()});
 
-                for (PostalCode postalCode : postalCodeSearchResult) {
-                    logger.log(Level.INFO, "postalCode AdminName1 {0} AdminName2 {1} AdminName3 {2}", new Object[]{postalCode.getAdminName1(), postalCode.getAdminName2(), postalCode.getAdminName3()});
-                    logger.log(Level.INFO, "postalCode AdminCode1 {0} AdminCode2 {1} AdminCode3 {2}", new Object[]{postalCode.getAdminCode1(), postalCode.getAdminCode2(), postalCode.getAdminCode3()});
+                            for (PostalCode postalCode : postalCodeSearchResult) {
+                                logger.log(Level.INFO, "postalCode AdminName1 {0} AdminName2 {1} AdminName3 {2}", new Object[]{postalCode.getAdminName1(), postalCode.getAdminName2(), postalCode.getAdminName3()});
+                                logger.log(Level.INFO, "postalCode AdminCode1 {0} AdminCode2 {1} AdminCode3 {2}", new Object[]{postalCode.getAdminCode1(), postalCode.getAdminCode2(), postalCode.getAdminCode3()});
 
-                    placesList.add(new GeonamesPlace(toponym, postalCode));
+                                placesList.add(new GeonamesPlace(toponym, postalCode));
+                            }
+                        }
+                    } catch (Exception e) {
+                        logger.log(Level.SEVERE, e.getLocalizedMessage(), e);
+                    }
                 }
             }
+        };
 
-            return placesList;
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, e.getLocalizedMessage(), e);
-            return null;
-        }
+        theTask = RP.create(runnable); //the task is not started yet
+
+        theTask.addTaskListener(new TaskListener() {
+
+            @Override
+            public void taskFinished(Task task) {
+            }
+        });
+
+        theTask.schedule(0); //start the task
     }
 }
