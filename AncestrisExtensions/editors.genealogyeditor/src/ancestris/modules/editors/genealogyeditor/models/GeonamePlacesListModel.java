5feadef0e0f9be23dd98bd91a6c9d74528af39b7
lemovice <lemovice@ancestris.org>
2013-12-13 22:09:02 +0000

    Add list implementation

--- AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GeonamePlacesListModel.java
+++ AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GeonamePlacesListModel.java
@@ -1,15 +1,14 @@
 package ancestris.modules.editors.genealogyeditor.models;
 
 import ancestris.api.place.Place;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import javax.swing.AbstractListModel;
 
 /**
  *
  * @author dominique
  */
-public class GeonamePlacesListModel extends AbstractListModel <String>{
+public class GeonamePlacesListModel extends AbstractListModel<String> implements List<Place> {
 
     ArrayList<Place> placesList = new ArrayList<Place>();
     String[] placesListData = null;
@@ -17,16 +16,7 @@ public class GeonamePlacesListModel extends AbstractListModel <String>{
     public GeonamePlacesListModel() {
     }
 
-    public void add(Place place) {
-        placesList.add(place);
-        update();
-    }
-
-    public void addAll(List<Place> placesList) {
-        placesList.addAll(placesList);
-        update();
-    }
-
+    @Override
     public void clear() {
         placesList.clear();
         update();
@@ -72,7 +62,7 @@ public class GeonamePlacesListModel extends AbstractListModel <String>{
     }
 
     @Override
-    public int getSize() {
+    public int size() {
         return placesListData != null ? placesListData.length : 0;
     }
 
@@ -89,5 +79,122 @@ public class GeonamePlacesListModel extends AbstractListModel <String>{
 
     public Place getPlaceAt(int index) {
         return placesList.get(index);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return placesList.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return placesList.contains(o);
+    }
+
+    @Override
+    public Iterator<Place> iterator() {
+        return placesList.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return placesList.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        return placesList.toArray(ts);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return placesList.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> clctn) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Place> clctn) {
+        boolean addAll = placesList.addAll(clctn);
+        update();
+        return addAll;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends Place> clctn) {
+        boolean addAll = placesList.addAll(i, clctn);
+        update();
+        return addAll;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> clctn) {
+        return placesList.removeAll(clctn);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> clctn) {
+        return placesList.retainAll(clctn);
+    }
+
+    @Override
+    public Place get(int i) {
+        return placesList.get(i);
+    }
+
+    @Override
+    public Place set(int i, Place e) {
+        return placesList.set(i, e);
+    }
+
+    @Override
+    public boolean add(Place place) {
+        boolean add = placesList.add(place);
+        update();
+        return add;
+    }
+
+    @Override
+    public void add(int i, Place e) {
+        placesList.add(i, e);
+        update();
+    }
+
+    @Override
+    public Place remove(int i) {
+        return placesList.remove(i);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return placesList.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return placesList.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<Place> listIterator() {
+        return placesList.listIterator();
+    }
+
+    @Override
+    public ListIterator<Place> listIterator(int i) {
+        return placesList.listIterator(i);
+    }
+
+    @Override
+    public List<Place> subList(int i, int i1) {
+        return placesList.subList(i1, i1);
+    }
+
+    @Override
+    public int getSize() {
+        return placesListData != null ? placesListData.length : 0;
     }
 }
