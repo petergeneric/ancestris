f8244f2d9ca47384112b0f2760c56521b05f4bcf
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-12-15 09:53:54 +0000

    creation of SourceCitationsTableModel.
    Add Gedcom SOURCE_RECORD definition in SourcesTableModel

---
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/SourceCitationsTableModel.java
@@ -0,0 +1,109 @@
+package ancestris.modules.editors.genealogyeditor.models;
+
+import genj.gedcom.Property;
+import genj.gedcom.PropertySource;
+import genj.gedcom.Source;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+/*
+ * SOURCE_CITATION:= 
+ [ 
+ * pointer to source record (preferred) 
+ * n SOUR @<XREF:SOUR>@ 
+ * +1 PAGE <WHERE_WITHIN_SOURCE> 
+ * +1 EVEN <EVENT_TYPE_CITED_FROM> 
+ * +2 ROLE <ROLE_IN_EVENT> 
+ * +1 DATA 
+ * +2 DATE <ENTRY_RECORDING_DATE> 
+ * +2 TEXT <TEXT_FROM_SOURCE> 
+ * +3 [CONC|CONT] <TEXT_FROM_SOURCE> 
+ * +1 <<MULTIMEDIA_LINK>> 
+ * +1 <<NOTE_STRUCTURE>> 
+ * +1 QUAY <CERTAINTY_ASSESSMENT> 
+ * |
+ * Systems not using source records
+ * n SOUR <SOURCE_DESCRIPTION>
+ * +1 [CONC|CONT] <SOURCE_DESCRIPTION>
+ * +1 TEXT <TEXT_FROM_SOURCE>
+ * +2 [CONC|CONT] <TEXT_FROM_SOURCE>
+ * +1 <<MULTIMEDIA_LINK>>
+ * +1 <<NOTE_STRUCTURE>>
+ * +1 QUAY <CERTAINTY_ASSESSMENT>
+ * ]
+ */
+
+/**
+ *
+ * @author dominique
+ */
+public class SourceCitationsTableModel extends AbstractTableModel {
+
+    ArrayList<Property> mSourcesList = new ArrayList<Property>();
+    String[] columnsName = {"ID", "Description", "Events"};
+
+    public SourceCitationsTableModel() {
+    }
+
+    @Override
+    public int getRowCount() {
+        return mSourcesList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnsName.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        Property source = mSourcesList.get(row);
+        if (source != null) {
+            if (column == 0) {
+                if (source instanceof PropertySource)
+                return ((Source) ((PropertySource) source).getTargetEntity()).getId();
+                else 
+                    return "";
+            } else if (column == 1) {
+                if (source instanceof PropertySource)
+                return ((Source) ((PropertySource) source).getTargetEntity()).getTitle();
+                else 
+                    return source.getValue();
+            } else {
+                Property propertyByPath = source.getPropertyByPath("DATA:EVEN");
+                if (propertyByPath != null) {
+                    return propertyByPath.getDisplayValue();
+                } else {
+                    return "";
+                }
+            }
+        } else {
+            return "";
+        }
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columnsName[col];
+    }
+
+    public void add(Property source) {
+        mSourcesList.add(source);
+        fireTableDataChanged();
+    }
+
+    public void addAll(List<Property> sourcesList) {
+        mSourcesList.addAll(sourcesList);
+        fireTableDataChanged();
+    }
+
+    public Property getValueAt(int row) {
+        return mSourcesList.get(row);
+    }
+
+    public Property remove(int row) {
+        Property source = mSourcesList.remove(row);
+        fireTableDataChanged();
+        return source;
+    }
+}
