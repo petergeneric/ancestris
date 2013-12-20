f587bcf8816321871fa18d3a287fb725e5faa2c0
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2013-12-20 23:52:05 +0000

    Add javahelp  skeleton for export.cousingenweb module

---
+++ trunk/AncestrisExtensions/exports.cousinsgenweb/src/ancestris/modules/exports/cousinsgenweb/docs/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2013 Ancestris
+ * 
+ * Author: Daniel Andre (daniel@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+@HelpSetRegistration(helpSet = "cousinsgenweb-hs.xml", position = 3487)
+package ancestris.modules.exports.cousinsgenweb.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
