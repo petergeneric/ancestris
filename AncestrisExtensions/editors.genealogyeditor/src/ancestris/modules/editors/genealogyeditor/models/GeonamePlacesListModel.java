2641c6c4f8ba7fba5248db64b688885919536b28
lemovice <lemovice@ancestris.org>
2013-12-21 20:30:19 +0000

    Correct NPE reported by Jean Raymond

--- AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GeonamePlacesListModel.java
+++ AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GeonamePlacesListModel.java
@@ -11,44 +11,8 @@ import javax.swing.AbstractListModel;
 public class GeonamePlacesListModel extends AbstractListModel<String> implements List<Place> {
 
     List<Place> placesList = Collections.synchronizedList(new ArrayList<Place>());
-    ArrayList<String> placesListData = new ArrayList<String>();
 
     public GeonamePlacesListModel() {
-    }
-
-    public void update(int index0, int index1) {
-        for (int index = index0; index < index1; index++) {
-            Place place = placesList.get(index);
-
-            String[] splitJurisdictions = place.getJurisdictions();
-            String jurisdictions = "";
-
-            // City
-            jurisdictions += splitJurisdictions[0] != null ? splitJurisdictions[0] : "";
-
-            //AdminName1
-            jurisdictions += splitJurisdictions[1] != null ? ", " + splitJurisdictions[1] : "";
-            //AdminCode1
-            jurisdictions += splitJurisdictions[2] != null ? " (" + splitJurisdictions[2] + ")" : "";
-
-            //AdminName2
-            jurisdictions += splitJurisdictions[3] != null ? ", " + splitJurisdictions[3] : "";
-            //AdminCode2
-            jurisdictions += splitJurisdictions[4] != null ? " (" + splitJurisdictions[4] + ")" : "";
-
-            //AdminName3
-            jurisdictions += splitJurisdictions[5] != null ? ", " + splitJurisdictions[5] : "";
-            //AdminCode3
-            jurisdictions += splitJurisdictions[6] != null ? " (" + splitJurisdictions[6] + ")" : "";
-
-            //Postal code
-            jurisdictions += splitJurisdictions[7] != null ? ", " + splitJurisdictions[7] : "";
-
-            //Country code
-            jurisdictions += splitJurisdictions[8] != null ? ", " + splitJurisdictions[8] : "";
-
-            placesListData.add(index, jurisdictions);
-        }
     }
 
     /*
@@ -67,7 +31,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
 
     @Override
     public int getSize() {
-        return placesListData.size();
+        return placesList.size();
     }
 
     /*
@@ -77,8 +41,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
     public void clear() {
         int oldSize = placesList.size();
         placesList.clear();
-        update(0, oldSize);
-        fireIntervalRemoved(placesListData, 0, oldSize);
+        fireIntervalRemoved(placesList, 0, oldSize);
     }
 
     @Override
@@ -117,12 +80,15 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
 
     @Override
     public boolean remove(Object o) {
-        return placesList.remove(o);
+        int indexOf = placesList.indexOf(o);
+        boolean remove = placesList.remove(o);
+        fireIntervalRemoved(placesList, indexOf, indexOf);
+        return remove;
     }
 
     @Override
     public boolean containsAll(Collection<?> clctn) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return placesList.containsAll(clctn);
     }
 
     @Override
@@ -130,9 +96,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
         int oldSize = placesList.size();
         boolean addAll = placesList.addAll(clctn);
         int newSize = placesList.size();
-
-        update(oldSize, newSize);
-        fireIntervalAdded(placesListData, oldSize, newSize);
+        fireIntervalAdded(placesList, oldSize, newSize);
         return addAll;
     }
 
@@ -141,8 +105,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
         int oldSize = placesList.size();
         boolean addAll = placesList.addAll(i, clctn);
         int newSize = placesList.size();
-        update(oldSize, newSize);
-        fireIntervalAdded(placesListData, oldSize, newSize);
+        fireIntervalAdded(placesList, oldSize, newSize);
         return addAll;
     }
 
@@ -150,7 +113,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
     public boolean removeAll(Collection<?> clctn) {
         int oldSize = placesList.size();
         boolean removeAll = placesList.removeAll(clctn);
-        update(oldSize, 0);
+        fireIntervalRemoved(placesList, 0, oldSize);
         return removeAll;
     }
 
@@ -166,15 +129,16 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
 
     @Override
     public Place set(int i, Place e) {
-        return placesList.set(i, e);
+        Place set = placesList.set(i, e);
+        fireIntervalAdded(placesList, i, i);
+        return set;
     }
 
     @Override
     public boolean add(Place place) {
         int oldSize = placesList.size();
         boolean add = placesList.add(place);
-        update(oldSize, oldSize + 1);
-        fireIntervalAdded(placesListData, oldSize, oldSize + 1);
+        fireIntervalAdded(placesList, oldSize, oldSize + 1);
         return add;
     }
 
@@ -182,8 +146,7 @@ public class GeonamePlacesListModel extends AbstractListModel<String> implements
     public void add(int i, Place e) {
         int oldSize = placesList.size();
         placesList.add(i, e);
-        update(oldSize, oldSize + 1);
-        fireIntervalAdded(placesListData, oldSize, oldSize + 1);
+        fireIntervalAdded(placesList, oldSize, oldSize + 1);
     }
 
     @Override
