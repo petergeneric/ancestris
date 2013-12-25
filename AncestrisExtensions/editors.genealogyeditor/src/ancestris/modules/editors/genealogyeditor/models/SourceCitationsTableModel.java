b15bd8d338c65a6abe3970885db8a01a31bf60ea
lemovice <lemovice@ancestris.org>
2013-12-25 16:01:06 +0000

    Correct NPE in case of null referenced source

--- AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/SourceCitationsTableModel.java
+++ AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/SourceCitationsTableModel.java
@@ -8,30 +8,28 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import org.openide.util.NbBundle;
 /*
- * SOURCE_CITATION:= 
- [ 
- * pointer to source record (preferred) 
- * n SOUR @<XREF:SOUR>@ 
- * +1 PAGE <WHERE_WITHIN_SOURCE> 
- * +1 EVEN <EVENT_TYPE_CITED_FROM> 
- * +2 ROLE <ROLE_IN_EVENT> 
- * +1 DATA 
- * +2 DATE <ENTRY_RECORDING_DATE> 
- * +2 TEXT <TEXT_FROM_SOURCE> 
- * +3 [CONC|CONT] <TEXT_FROM_SOURCE> 
- * +1 <<MULTIMEDIA_LINK>> 
- * +1 <<NOTE_STRUCTURE>> 
- * +1 QUAY <CERTAINTY_ASSESSMENT> 
- * |
- * Systems not using source records
- * n SOUR <SOURCE_DESCRIPTION>
- * +1 [CONC|CONT] <SOURCE_DESCRIPTION>
- * +1 TEXT <TEXT_FROM_SOURCE>
- * +2 [CONC|CONT] <TEXT_FROM_SOURCE>
+ * SOURCE_CITATION:=
+ * [ pointer to source record (preferred)
+ * n SOUR @<XREF:SOUR>@
+ * +1 PAGE <WHERE_WITHIN_SOURCE>
+ * +1 EVEN <EVENT_TYPE_CITED_FROM>
+ * +2 ROLE <ROLE_IN_EVENT>
+ * +1 DATA
+ * +2 DATE <ENTRY_RECORDING_DATE>
+ * +2 TEXT <TEXT_FROM_SOURCE>
+ * +3 [CONC|CONT] <TEXT_FROM_SOURCE>
  * +1 <<MULTIMEDIA_LINK>>
  * +1 <<NOTE_STRUCTURE>>
  * +1 QUAY <CERTAINTY_ASSESSMENT>
- * ]
+ * | Systems not using source records
+ * n SOUR <SOURCE_DESCRIPTION>
+ * +1 [CONC|CONT]
+ * <SOURCE_DESCRIPTION>
+ * +1 TEXT <TEXT_FROM_SOURCE>
+ * +2 [CONC|CONT] <TEXT_FROM_SOURCE>
+ * +1 <<MULTIMEDIA_LINK>>
+ * +1 <<NOTE_STRUCTURE>> +1 QUAY
+ * <CERTAINTY_ASSESSMENT> ]
  */
 
 /**
@@ -46,7 +44,6 @@ public class SourceCitationsTableModel extends AbstractTableModel {
         NbBundle.getMessage(MultiMediaObjectsTableModel.class, "SourceCitationsTableModel.column.description.title"),
         NbBundle.getMessage(MultiMediaObjectsTableModel.class, "SourceCitationsTableModel.column.events.title")
     };
-
 
     public SourceCitationsTableModel() {
     }
@@ -66,15 +63,27 @@ public class SourceCitationsTableModel extends AbstractTableModel {
         Property source = mSourcesList.get(row);
         if (source != null) {
             if (column == 0) {
-                if (source instanceof PropertySource)
-                return ((Source) ((PropertySource) source).getTargetEntity()).getId();
-                else 
+                if (source instanceof PropertySource) {
+                    Source targetEntity = (Source) ((PropertySource) source).getTargetEntity();
+                    if (targetEntity != null) {
+                        return targetEntity.getId();
+                    } else {
+                        return "";
+                    }
+                } else {
                     return "";
+                }
             } else if (column == 1) {
-                if (source instanceof PropertySource)
-                return ((Source) ((PropertySource) source).getTargetEntity()).getTitle();
-                else 
+                if (source instanceof PropertySource) {
+                    Source targetEntity = (Source) ((PropertySource) source).getTargetEntity();
+                    if (targetEntity != null) {
+                        return targetEntity.getTitle();
+                    } else {
+                        return "";
+                    }
+                } else {
                     return source.getValue();
+                }
             } else {
                 Property propertyByPath = source.getPropertyByPath("DATA:EVEN");
                 if (propertyByPath != null) {
