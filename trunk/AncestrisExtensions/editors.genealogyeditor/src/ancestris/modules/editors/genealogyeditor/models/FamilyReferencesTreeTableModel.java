8418ea45123e3bb582d2dbc76d0ca05b1ba8c798
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-01-12 15:31:59 +0000

    Add FamilyReferencesTreeTableModel class

---
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/FamilyReferencesTreeTableModel.java
@@ -0,0 +1,198 @@
+package ancestris.modules.editors.genealogyeditor.models;
+
+import genj.gedcom.*;
+import java.util.List;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import org.jdesktop.swingx.treetable.AbstractTreeTableModel;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author dominique
+ */
+public class FamilyReferencesTreeTableModel extends AbstractTreeTableModel {
+
+    public static int FAMILY_CHILD = 1;
+    public static int FAMILY_SPOUSE = 2;
+    private int mFamilyTableType = FAMILY_CHILD;
+    private static String[] familyChildColumnsName = {
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.column.ID.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.column.husband.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.column.wife.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.column.date.title"),};
+    private static String[] familySpouseColumnsName = {
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.column.ID.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.column.husband.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.column.wife.title"),
+        NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.column.date.title"),};
+    private String[] familyColumnsName = familyChildColumnsName;
+    private String mFemale = "";
+    private String mMale = "";
+
+    public FamilyReferencesTreeTableModel() {
+        this(FAMILY_CHILD);
+    }
+
+    public FamilyReferencesTreeTableModel(int familyType) {
+        super(new DefaultMutableTreeNode());
+        mFamilyTableType = familyType;
+        if (mFamilyTableType == FAMILY_CHILD) {
+            familyColumnsName = familyChildColumnsName;
+            mFemale = NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.female.title");
+            mMale = NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familyChild.male.title");
+        } else if (mFamilyTableType == FAMILY_SPOUSE) {
+            familyColumnsName = familySpouseColumnsName;
+            mFemale = NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.female.title");
+            mMale = NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.familySpouse.male.title");
+        }
+    }
+
+    @Override
+    public int getColumnCount() {
+        return familyColumnsName.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        if (column < familyColumnsName.length) {
+            return (String) familyColumnsName[column];
+        } else {
+            return "";
+        }
+    }
+
+    @Override
+    public Object getValueAt(Object object, int index) {
+        if (object instanceof DefaultMutableTreeNode) {
+            DefaultMutableTreeNode dataNode = (DefaultMutableTreeNode) object;
+            Property property = (Property) dataNode.getUserObject();
+            if (property instanceof PropertyXRef) {
+                Entity entity = ((PropertyXRef) property).getTargetEntity();
+                if (entity instanceof Fam) {
+                    Fam family = (Fam) entity;
+                    switch (index) {
+                        case 0:
+                            return family.getId();
+
+                        case 1:
+                            return family.getHusband() != null ? family.getHusband().getName() : "";
+
+                        case 2:
+                            return family.getWife() != null ? family.getWife().getName() : "";
+
+                        case 3:
+                            return family.getMarriageDate() != null
+                                    ? NbBundle.getMessage(FamilyReferencesTreeTableModel.class, "FamiliesTreeTableModel.family.wedding") + " " + family.getMarriageDate().getDisplayValue()
+                                    : "";
+
+                        default:
+                            return "";
+                    }
+                } else if (entity instanceof Indi) {
+                    Indi child = (Indi) entity;
+                    switch (index) {
+                        case 0:
+                            if (child.getSex() == PropertySex.MALE) {
+                                return mMale + " (" + child.getId() + ")";
+                            } else if (child.getSex() == PropertySex.FEMALE) {
+                                return mFemale + " (" + child.getId() + ")";
+                            } else {
+                                return child.getId();
+                            }
+
+                        case 1:
+                            return child.getFirstName();
+
+                        case 2:
+                            return child.getLastName();
+
+                        case 3:
+                            return child.getBirthDate() != null
+                                    ? NbBundle.getMessage(
+                                    FamilyReferencesTreeTableModel.class,
+                                    "FamiliesTreeTableModel.child.birth") + " " + child.getBirthDate().getDisplayValue()
+                                    : "";
+
+                        default:
+                            return "";
+                    }
+                } else {
+                    return "";
+                }
+            } else {
+                return "";
+            }
+        } else {
+            return object.getClass().getCanonicalName();
+        }
+    }
+
+    @Override
+    public Object getChild(Object object, int index) {
+
+        if (object instanceof DefaultMutableTreeNode) {
+            DefaultMutableTreeNode nodes = (DefaultMutableTreeNode) object;
+            return nodes.getChildAt(index);
+        }
+        return null;
+    }
+
+    @Override
+    public int getChildCount(Object object) {
+        if (object instanceof DefaultMutableTreeNode) {
+            DefaultMutableTreeNode nodes = (DefaultMutableTreeNode) object;
+            return nodes.getChildCount();
+        }
+        return 0;
+    }
+
+    @Override
+    public int getIndexOfChild(Object o, Object o1) {
+        return 0;
+    }
+
+    public void add(PropertyXRef familyRef) {
+        DefaultMutableTreeNode familyNode = new DefaultMutableTreeNode(familyRef);
+        Entity entity = familyRef.getTargetEntity();
+        if (entity instanceof Fam) {
+            Fam family = (Fam) entity;
+            for (PropertyChild childRef : family.getProperties(PropertyChild.class)) {
+                familyNode.add(new DefaultMutableTreeNode(childRef));
+            }
+        }
+        ((DefaultMutableTreeNode) getRoot()).add(familyNode);
+        modelSupport.fireNewRoot();
+    }
+
+    public void addAll(List<? extends PropertyXRef> familiesList) {
+        for (PropertyXRef familyRef : familiesList) {
+            DefaultMutableTreeNode familyNode = new DefaultMutableTreeNode(familyRef);
+            Entity entity = familyRef.getTargetEntity();
+            if (entity instanceof Fam) {
+                Fam family = (Fam) entity;
+                for (PropertyChild childRef : family.getProperties(PropertyChild.class)) {
+                    familyNode.add(new DefaultMutableTreeNode(childRef));
+                }
+                ((DefaultMutableTreeNode) getRoot()).add(familyNode);
+            }
+            modelSupport.fireNewRoot();
+        }
+    }
+
+    public void remove(DefaultMutableTreeNode dataNode) {
+        TreeNode parent = dataNode.getParent();
+        if (parent instanceof DefaultMutableTreeNode) {
+            ((DefaultMutableTreeNode) parent).remove(dataNode);
+        }
+        modelSupport.fireNewRoot();
+    }
+
+    public void update(List<Fam> familiesList) {
+    }
+
+    @Override
+    public boolean isLeaf(Object node) {
+        return getChildCount(node) == 0;
+    }
+}
