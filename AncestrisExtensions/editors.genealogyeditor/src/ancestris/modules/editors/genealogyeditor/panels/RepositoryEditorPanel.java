044228f05f8755c02718c1593df8b39e43d14ac8
lemovice <lemovice@ancestris.org>
2014-03-02 15:20:26 +0000

    First complete version of the repository editor

--- AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/panels/RepositoryEditorPanel.java
+++ AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/panels/RepositoryEditorPanel.java
@@ -10,6 +10,17 @@ import org.openide.util.Exceptions;
  *
  * @author dominique
  */
+/*
+ * REPOSITORY_RECORD
+ * n @<XREF:REPO>@ REPO
+ * +1 NAME <NAME_OF_REPOSITORY>
+ * +1 <<ADDRESS_STRUCTURE>>
+ * +1 <<NOTE_STRUCTURE>>
+ * +1 REFN <USER_REFERENCE_NUMBER>
+ * +2 TYPE <USER_REFERENCE_TYPE>
+ * +1 RIN <AUTOMATED_RECORD_ID>
+ * +1 <<CHANGE_DATE>>
+ */
 public class RepositoryEditorPanel extends javax.swing.JPanel {
 
     private Repository mRepository;
@@ -34,12 +45,9 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
         repositoryIDTextField = new javax.swing.JTextField();
         repositoryNameLabel = new javax.swing.JLabel();
         repositoryNameTextField = new javax.swing.JTextField();
-        repositoryAddressLabel = new javax.swing.JLabel();
-        repositoryAddressTextField = new javax.swing.JTextField();
-        addRepositoryAddressButton = new javax.swing.JButton();
-        editRepositoryAddressButton = new javax.swing.JButton();
-        removeRepositoryAddressButton = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
+        addressPanel = new javax.swing.JPanel();
+        addressEditorPanel = new ancestris.modules.editors.genealogyeditor.panels.AddressEditorPanel();
         notesPanel = new javax.swing.JPanel();
         noteCitationsListPanel = new ancestris.modules.editors.genealogyeditor.panels.NoteCitationsListPanel();
         referencesPanel = new javax.swing.JPanel();
@@ -56,32 +64,28 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
 
         repositoryNameTextField.setText(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle("ancestris/modules/editors/genealogyeditor/panels/Bundle").getString("RepositoryEditorPanel.repositoryNameTextField.text"), new Object[] {})); // NOI18N
 
-        repositoryAddressLabel.setText(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle("ancestris/modules/editors/genealogyeditor/panels/Bundle").getString("RepositoryEditorPanel.repositoryAddressLabel.text"), new Object[] {})); // NOI18N
+        javax.swing.GroupLayout addressPanelLayout = new javax.swing.GroupLayout(addressPanel);
+        addressPanel.setLayout(addressPanelLayout);
+        addressPanelLayout.setHorizontalGroup(
+            addressPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(addressEditorPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 690, Short.MAX_VALUE)
+        );
+        addressPanelLayout.setVerticalGroup(
+            addressPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(addressEditorPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 
-        repositoryAddressTextField.setText(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle("ancestris/modules/editors/genealogyeditor/panels/Bundle").getString("RepositoryEditorPanel.repositoryAddressTextField.text"), new Object[] {})); // NOI18N
-
-        addRepositoryAddressButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/editors/genealogyeditor/resources/edit_add.png"))); // NOI18N
-        addRepositoryAddressButton.setText(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle("ancestris/modules/editors/genealogyeditor/panels/Bundle").getString("RepositoryEditorPanel.addRepositoryAddressButton.text"), new Object[] {})); // NOI18N
-
-        editRepositoryAddressButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/editors/genealogyeditor/resources/edit.png"))); // NOI18N
-        editRepositoryAddressButton.setText(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle("ancestris/modules/editors/genealogyeditor/panels/Bundle").getString("RepositoryEditorPanel.editRepositoryAddressButton.text"), new Object[] {})); // NOI18N
-        editRepositoryAddressButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                editRepositoryAddressButtonActionPerformed(evt);
-            }
-        });
-
-        removeRepositoryAddressButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/editors/genealogyeditor/resources/edit_delete.png"))); // NOI18N
+        jTabbedPane1.addTab(org.openide.util.NbBundle.getMessage(RepositoryEditorPanel.class, "RepositoryEditorPanel.addressPanel.TabConstraints.tabTitle"), addressPanel); // NOI18N
 
         javax.swing.GroupLayout notesPanelLayout = new javax.swing.GroupLayout(notesPanel);
         notesPanel.setLayout(notesPanelLayout);
         notesPanelLayout.setHorizontalGroup(
             notesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(noteCitationsListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 456, Short.MAX_VALUE)
+            .addComponent(noteCitationsListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 690, Short.MAX_VALUE)
         );
         notesPanelLayout.setVerticalGroup(
             notesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(noteCitationsListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE)
+            .addComponent(noteCitationsListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)
         );
 
         jTabbedPane1.addTab("Notes", new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/editors/genealogyeditor/resources/Note.png")), notesPanel); // NOI18N
@@ -92,13 +96,13 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
             referencesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, referencesPanelLayout.createSequentialGroup()
                 .addGap(0, 0, 0)
-                .addComponent(referencesListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 456, Short.MAX_VALUE))
+                .addComponent(referencesListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 690, Short.MAX_VALUE))
         );
         referencesPanelLayout.setVerticalGroup(
             referencesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, referencesPanelLayout.createSequentialGroup()
                 .addGap(0, 0, 0)
-                .addComponent(referencesListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE))
+                .addComponent(referencesListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("References", new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/editors/genealogyeditor/resources/association.png")), referencesPanel); // NOI18N
@@ -110,26 +114,17 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTabbedPane1)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(repositoryNameLabel)
-                            .addComponent(repositoryIDLabel)
-                            .addComponent(repositoryAddressLabel))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(repositoryIDLabel))
+                        .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(repositoryAddressTextField)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(addRepositoryAddressButton)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(editRepositoryAddressButton)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(removeRepositoryAddressButton))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(repositoryIDTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
-                            .addComponent(repositoryNameTextField)))
-                    .addComponent(jTabbedPane1))
+                            .addComponent(repositoryNameTextField))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -144,32 +139,18 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
                     .addComponent(repositoryNameLabel)
                     .addComponent(repositoryNameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(addRepositoryAddressButton)
-                    .addComponent(editRepositoryAddressButton)
-                    .addComponent(removeRepositoryAddressButton)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(repositoryAddressLabel)
-                        .addComponent(repositoryAddressTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTabbedPane1)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
-
-    private void editRepositoryAddressButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editRepositoryAddressButtonActionPerformed
-    }//GEN-LAST:event_editRepositoryAddressButtonActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton addRepositoryAddressButton;
-    private javax.swing.JButton editRepositoryAddressButton;
+    private ancestris.modules.editors.genealogyeditor.panels.AddressEditorPanel addressEditorPanel;
+    private javax.swing.JPanel addressPanel;
     private javax.swing.JTabbedPane jTabbedPane1;
     private ancestris.modules.editors.genealogyeditor.panels.NoteCitationsListPanel noteCitationsListPanel;
     private javax.swing.JPanel notesPanel;
     private ancestris.modules.editors.genealogyeditor.panels.ReferencesListPanel referencesListPanel;
     private javax.swing.JPanel referencesPanel;
-    private javax.swing.JButton removeRepositoryAddressButton;
-    private javax.swing.JLabel repositoryAddressLabel;
-    private javax.swing.JTextField repositoryAddressTextField;
     private javax.swing.JLabel repositoryIDLabel;
     private javax.swing.JTextField repositoryIDTextField;
     private javax.swing.JLabel repositoryNameLabel;
@@ -188,22 +169,20 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
      */
     public void setRepository(Repository repository) {
         this.mRepository = repository;
-        update();
-    }
 
-    private void update() {
         repositoryIDTextField.setText(mRepository.getId());
 
         Property repositoryName = mRepository.getProperty("NAME");
         repositoryNameTextField.setText(repositoryName != null ? repositoryName.getValue() : "");
 
         Property address = mRepository.getProperty("ADDR");
-        repositoryAddressTextField.setText(address != null ? address.getValue() : "");
+        addressEditorPanel.set(mRepository, address);
 
         List<Entity> entitiesList = new ArrayList<Entity>();
         for (PropertyXRef entityRef : mRepository.getProperties(PropertyXRef.class)) {
             entitiesList.add(entityRef.getTargetEntity());
         }
+
         referencesListPanel.set(mRepository, entitiesList);
 
         noteCitationsListPanel.setNotesList(mRepository, Arrays.asList(mRepository.getProperties("NOTE")));
@@ -218,6 +197,9 @@ public class RepositoryEditorPanel extends javax.swing.JPanel {
                 public void perform(Gedcom gedcom) throws GedcomException {
                 }
             }); // end of doUnitOfWork
+
+            addressEditorPanel.commit();
+
             return mRepository;
         } catch (GedcomException ex) {
             Exceptions.printStackTrace(ex);
