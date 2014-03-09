213c908b88bf1304a09e3934fd32eff348e775d4
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-03-09 09:23:06 +0000

    Update definition and add localization string

--- trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/ConfidenceLevelComboBoxModel.java
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/ConfidenceLevelComboBoxModel.java
@@ -1,7 +1,7 @@
 package ancestris.modules.editors.genealogyeditor.models;
 
-import ancestris.modules.gedcom.utilities.PropertyTag2Name;
 import javax.swing.DefaultComboBoxModel;
+import org.openide.util.NbBundle;
 
 /**
  *
@@ -10,10 +10,9 @@ import javax.swing.DefaultComboBoxModel;
 public class ConfidenceLevelComboBoxModel extends DefaultComboBoxModel<String> {
 
     public ConfidenceLevelComboBoxModel() {
-        addElement("");
-        
-        addElement("High");
-        addElement("Medium");
-        addElement("Low");
+        addElement(NbBundle.getMessage(ConfidenceLevelComboBoxModel.class, "ConfidenceLevelComboBoxModel.confidenceLevel.unreliable"));
+        addElement(NbBundle.getMessage(ConfidenceLevelComboBoxModel.class, "ConfidenceLevelComboBoxModel.confidenceLevel.questionable"));
+        addElement(NbBundle.getMessage(ConfidenceLevelComboBoxModel.class, "ConfidenceLevelComboBoxModel.confidenceLevel.secondary"));
+        addElement(NbBundle.getMessage(ConfidenceLevelComboBoxModel.class, "ConfidenceLevelComboBoxModel.confidenceLevel.direct"));
     }
 }
