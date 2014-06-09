6d2d3437a1c8cc58f91c40ea0d469e5d2079c002
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-06-09 16:43:07 +0000

    Add toolbar separator

---
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/actions/Separator.java
@@ -0,0 +1,41 @@
+package ancestris.modules.editors.genealogyeditor.actions;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import javax.swing.JSeparator;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.Presenter;
+
+@ActionID(
+        category = "Edit",
+        id = "ancestris.modules.editors.genealogyeditor.actions.Separator"
+)
+@ActionRegistration(
+        lazy = false,
+        displayName = ""
+)
+@ActionReferences({
+    @ActionReference(path = "Menu/File", position = 3333),
+    @ActionReference(path = "Toolbars/GenealogyEditor", position = 150)
+})
+@Messages("CTL_Separator=Separator")
+public final class Separator extends AbstractAction implements Presenter.Toolbar {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+
+    @Override
+    public Component getToolbarPresenter() {
+        JSeparator separator = new JSeparator();
+        separator.setOrientation(JSeparator.VERTICAL);
+        return separator;
+    }
+}
