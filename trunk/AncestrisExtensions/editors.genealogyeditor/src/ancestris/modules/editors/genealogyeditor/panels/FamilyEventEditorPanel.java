721469579b3831ad92605d345243af844e1e01da
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-06-10 19:50:25 +0000

    fix date commit problem reported by Jean-Raymond

--- trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/panels/FamilyEventEditorPanel.java
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/panels/FamilyEventEditorPanel.java
@@ -740,7 +740,7 @@ public class FamilyEventEditorPanel extends javax.swing.JPanel {
         if (mRoot == null) {
             return null;
         }
-        if (mEventModified == true) {
+        if (mEventModified == true || aDateBean.hasChanged()) {
             try {
                 mRoot.getGedcom().doUnitOfWork(new UnitOfWork() {
 
