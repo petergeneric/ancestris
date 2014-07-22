a7e63353ecae94322590e1eb46ccbd8c5b5304d4
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-07-22 20:26:55 +0000

    François request point 7

--- trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/EventsRoleComboBoxModel.java
+++ trunk/AncestrisExtensions/editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/EventsRoleComboBoxModel.java
@@ -12,12 +12,12 @@ public class EventsRoleComboBoxModel extends DefaultComboBoxModel<String> {
     // ROLE_IN_EVENT:=
     // [ CHIL | HUSB | WIFE | MOTH | FATH | SPOU | (<ROLE_DESCRIPTOR>) ]    
     public EventsRoleComboBoxModel() {
+        addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.USER"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.CHIL"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.HUSB"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.WIFE"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.MOTH"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.FATH"));
         addElement(NbBundle.getMessage(EventsRoleComboBoxModel.class, "EventsRoleComboBoxModel.roleInEvent.SPOU"));
-        addElement("");
     }
 }
