99f5d175d01dd75e5dbfc0938b3d4cc80ec1fe45
lemovice <lemovice@ancestris.org>
2014-07-27 22:23:20 +0000

    revert commit 5541

--- AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/SosaNumbersPlugin.java
+++ AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/SosaNumbersPlugin.java
@@ -9,49 +9,17 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
+
 package ancestris.modules.gedcom.sosanumbers;
 
 import ancestris.core.pluginservice.AncestrisPlugin;
-import genj.app.GedcomFileListener;
-import genj.gedcom.Context;
-import genj.gedcom.Gedcom;
-import genj.gedcom.Indi;
-import java.util.prefs.Preferences;
-import org.openide.util.NbPreferences;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author daniel
  */
-@ServiceProvider(service = ancestris.core.pluginservice.PluginInterface.class)
-public class SosaNumbersPlugin extends AncestrisPlugin implements GedcomFileListener {
+@ServiceProvider(service=ancestris.core.pluginservice.PluginInterface.class)
+public class SosaNumbersPlugin extends AncestrisPlugin{
 
-    SosaNumbers sosaNumbers = null;
-
-    @Override
-    public void commitRequested(Context context) {
-    }
-
-    @Override
-    public void gedcomClosed(Gedcom gedcom) {
-        if (sosaNumbers != null) {
-//        gedcom.removeGedcomListener(sosaNumbers);
-        }
-    }
-
-    @Override
-    public void gedcomOpened(Gedcom gedcom) {
-        Preferences modulePreferences = NbPreferences.forModule(SosaNumbers.class);
-        String selectedEntityID = modulePreferences.get("SelectEntityDialog." + gedcom.getName(), "");
-        Indi indiDeCujus = null;
-        if (!selectedEntityID.isEmpty()) {
-            indiDeCujus = (Indi) gedcom.getEntity(Gedcom.INDI, selectedEntityID);
-        }
-        if (indiDeCujus != null) {
-            sosaNumbers = new SosaNumbers();
-            sosaNumbers.generateSosaNbs(gedcom, indiDeCujus);
-//          gedcom.addGedcomListener(sosaNumbers);
-        }
-    }
 }
