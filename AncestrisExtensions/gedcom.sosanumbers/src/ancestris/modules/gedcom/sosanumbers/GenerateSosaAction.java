56d8cc0a1f9cf811a31f23f986be37c883ffcf02
dandre <dandre@ancestris.org>
2014-08-25 21:36:05 +0000

    add 'Remove SOSA numbering' functionality

--- AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/GenerateSosaAction.java
+++ AncestrisExtensions/gedcom.sosanumbers/src/ancestris/modules/gedcom/sosanumbers/GenerateSosaAction.java
@@ -11,8 +11,10 @@
  */
 package ancestris.modules.gedcom.sosanumbers;
 
-import ancestris.util.swing.SelectEntityDialog;
+import ancestris.util.swing.DialogManager;
+import ancestris.util.swing.SelectEntityPanel;
 import genj.gedcom.Context;
+import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
 import genj.gedcom.Indi;
 import java.awt.event.ActionEvent;
@@ -28,10 +30,10 @@ import org.openide.util.NbPreferences;
 import org.openide.util.Utilities;
 
 @ActionID(id = "ancestris.modules.gedcom.sosanumbers.GenerateSosaAction",
-category = "Tools")
+        category = "Tools")
 @ActionRegistration(iconBase = "ancestris/modules/gedcom/sosanumbers/SosaNumbersIcon.png",
-iconInMenu = true,
-displayName = "#CTL_GenerateSosaAction")
+        iconInMenu = true,
+        displayName = "#CTL_GenerateSosaAction")
 @ActionReference(path = "Menu/Tools/Gedcom")
 public final class GenerateSosaAction implements ActionListener {
 
@@ -44,13 +46,38 @@ public final class GenerateSosaAction implements ActionListener {
 
         if ((context = Utilities.actionsGlobalContext().lookup(Context.class)) != null) {
             myGedcom = context.getGedcom();
+            SelectEntityPanel selectEntityPanel = new SelectEntityPanel(myGedcom, Gedcom.INDI);
 
-            SelectEntityDialog selectEntityDialog = new SelectEntityDialog(NbBundle.getMessage(this.getClass(), "GenerateSosaAction.AskDeCujus"), myGedcom, Gedcom.INDI);
-            Indi indiDeCujus = (Indi) selectEntityDialog.getEntity();
-            if (indiDeCujus != null) {
+            String selectedEntityID = modulePreferences.get("SelectEntityDialog." + myGedcom.getName() + "." + Gedcom.INDI, "");
+            if (!selectedEntityID.isEmpty()) {
+                Entity selectedEntity = myGedcom.getEntity(selectedEntityID);
+                if (selectedEntity != null) {
+                    selectEntityPanel.setSelection(selectedEntity);
+                }
+            }
+
+            String clearChoice = new String(NbBundle.getMessage(GenerateSosaAction.class, "GenerateSosaAction.clear"));
+            Object choice = DialogManager.create(
+                    NbBundle.getMessage(this.getClass(), "GenerateSosaAction.AskDeCujus"),
+                    selectEntityPanel)
+                    .setMessageType(DialogManager.QUESTION_MESSAGE)
+                    .setOptions(new Object[]{DialogManager.OK_OPTION, DialogManager.CANCEL_OPTION})
+                    .setAdditionalOptions(new Object[]{clearChoice})
+                    .show();
+
+            if (choice != DialogManager.CANCEL_OPTION) {
+                Indi indiDeCujus = null;
+                if (choice == DialogManager.OK_OPTION) {
+                    indiDeCujus = (Indi) selectEntityPanel.getSelection();
+                }
+                // else null means clear sosa
                 new SosaNumbers().generateSosaNbs(myGedcom, indiDeCujus);
-                modulePreferences.put("SelectEntityDialog." + myGedcom.getName(), indiDeCujus.getId());
-                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(GenerateSosaAction.class, "GenerateSosaAction.done", indiDeCujus.getName()), NotifyDescriptor.INFORMATION_MESSAGE));
+                if (indiDeCujus == null) {
+                    modulePreferences.remove("SelectEntityDialog." + myGedcom.getName());
+                } else {
+                    modulePreferences.put("SelectEntityDialog." + myGedcom.getName(), indiDeCujus.getId());
+                    DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(GenerateSosaAction.class, "GenerateSosaAction.done", indiDeCujus.getName()), NotifyDescriptor.INFORMATION_MESSAGE));
+                }
             }
         }
     }
