24fb1f338c2ad573ffb5e07ad476c4521c972e15
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-09-02 21:42:55 +0000

    add latitude and longitude columns

--- trunk/AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GedcomPlaceTableModel.java
+++ trunk/AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/models/GedcomPlaceTableModel.java
@@ -5,6 +5,7 @@ import genj.gedcom.PropertyPlace;
 import genj.util.ReferenceSet;
 import java.util.Set;
 import javax.swing.table.AbstractTableModel;
+import org.openide.util.NbBundle;
 
 /**
  *
@@ -26,23 +27,67 @@ public class GedcomPlaceTableModel extends AbstractTableModel {
 
     @Override
     public int getColumnCount() {
-        return placeFormat.length;
+        return placeFormat.length + 2;
     }
 
     @Override
     public Object getValueAt(int row, int column) {
         Object[] toArray = gedcomPlacesMap.getKeys().toArray();
         String key = (String) toArray[row];
-        if (key.split(PropertyPlace.JURISDICTION_SEPARATOR).length > column) {
-            return key.split(PropertyPlace.JURISDICTION_SEPARATOR)[column];
+        if (column < placeFormat.length) {
+            if (key.split(PropertyPlace.JURISDICTION_SEPARATOR).length > column) {
+                return key.split(PropertyPlace.JURISDICTION_SEPARATOR)[column];
+            } else {
+                return "";
+            }
         } else {
+            Set<Property> references = gedcomPlacesMap.getReferences(key);
+            String MAPTag = "";
+            String LATITag = "";
+            String LONGTag = "";
+            if (((Property) references.toArray()[0]).getGedcom().getGrammar().getVersion().equals("5.5.1") == true) {
+                MAPTag = "MAP";
+                LATITag = "LATI";
+                LONGTag = "LONG";
+            } else {
+                MAPTag = "_MAP";
+                LATITag = "_LATI";
+                LONGTag = "_LONG";
+            }
+
+            Property placeMap = ((Property) references.toArray()[0]).getProperty(MAPTag);
+            if (placeMap != null) {
+                if (column < placeFormat.length + 1) {
+                    Property latitude = placeMap.getProperty(LATITag);
+                    if (latitude != null) {
+                        return latitude.getValue();
+                    } else {
+                        return "";
+                    }
+                } else {
+                    Property longitude = placeMap.getProperty(LONGTag);
+                    if (longitude != null) {
+                        return longitude.getValue();
+                    } else {
+                        return "";
+                    }
+                }
+            }
             return "";
         }
     }
 
     @Override
     public String getColumnName(int col) {
-        return placeFormat[col].toString();
+        if (col < placeFormat.length) {
+            return placeFormat[col];
+        } else {
+            if (col < placeFormat.length + 1) {
+                return NbBundle.getMessage(FamiliesTableModel.class, "GedcomPlaceTableModel.column.latitude.title");
+            } else {
+                return NbBundle.getMessage(FamiliesTableModel.class, "GedcomPlaceTableModel.column.longitude.title");
+            }
+        }
     }
 
     public void update(ReferenceSet<String, Property> gedcomPlacesMap) {
@@ -50,7 +95,7 @@ public class GedcomPlaceTableModel extends AbstractTableModel {
 
         fireTableDataChanged();
     }
-    
+
     public Set<Property> getValueAt(int row) {
         Object[] toArray = gedcomPlacesMap.getKeys().toArray();
         String key = (String) toArray[row];
