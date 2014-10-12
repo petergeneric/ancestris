2e72017914500e0cc4daed40ee398b4123e2266c
lemovice <lemovice@ancestris.org>
2014-10-12 20:51:21 +0000

    modify submitter editor prefered languages display

--- AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/editors/SubmitterEditor.java
+++ AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/editors/SubmitterEditor.java
@@ -31,7 +31,9 @@ public class SubmitterEditor extends EntityEditor {
         super(isNew);
         initComponents();
         submitterNameTextField.getDocument().addDocumentListener(changes);
-        submitterLanguageTextField.getDocument().addDocumentListener(changes);
+        firstPreferLanguageTextField.getDocument().addDocumentListener(changes);
+        secondPreferLanguageTextField.getDocument().addDocumentListener(changes);
+        thirdPreferLanguageTextField.getDocument().addDocumentListener(changes);
         addressEditorPanel.addChangeListener(changes);
         multimediaObjectCitationsListPanel.addChangeListener(changes);
         noteCitationsListPanel.addChangeListener(changes);
@@ -46,23 +48,49 @@ public class SubmitterEditor extends EntityEditor {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        languagesPanel = new javax.swing.JPanel();
+        firstPreferLanguageTextField = new javax.swing.JTextField();
+        secondPreferLanguageTextField = new javax.swing.JTextField();
+        thirdPreferLanguageTextField = new javax.swing.JTextField();
         changeDateLabel = new javax.swing.JLabel();
         changeDateLabeldate = new javax.swing.JLabel();
         submitterNameLabel = new javax.swing.JLabel();
         submitterNameTextField = new javax.swing.JTextField();
-        languageLabel = new javax.swing.JLabel();
-        submitterLanguageTextField = new javax.swing.JTextField();
         submitterTabbedPane = new javax.swing.JTabbedPane();
         addressEditorPanel = new ancestris.modules.editors.genealogyeditor.panels.AddressEditorPanel();
         multimediaObjectCitationsListPanel = new ancestris.modules.editors.genealogyeditor.panels.MultimediaObjectCitationsListPanel();
         noteCitationsListPanel = new ancestris.modules.editors.genealogyeditor.panels.NoteCitationsListPanel();
 
+        languagesPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.languagesPanel.border.title"))); // NOI18N
+
+        javax.swing.GroupLayout languagesPanelLayout = new javax.swing.GroupLayout(languagesPanel);
+        languagesPanel.setLayout(languagesPanelLayout);
+        languagesPanelLayout.setHorizontalGroup(
+            languagesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(languagesPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(languagesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(firstPreferLanguageTextField)
+                    .addComponent(secondPreferLanguageTextField)
+                    .addComponent(thirdPreferLanguageTextField))
+                .addContainerGap())
+        );
+        languagesPanelLayout.setVerticalGroup(
+            languagesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(languagesPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(firstPreferLanguageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(secondPreferLanguageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(thirdPreferLanguageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
         changeDateLabel.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         org.openide.awt.Mnemonics.setLocalizedText(changeDateLabel, org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.changeDateLabel.text")); // NOI18N
 
         org.openide.awt.Mnemonics.setLocalizedText(submitterNameLabel, org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.submitterNameLabel.text")); // NOI18N
-
-        org.openide.awt.Mnemonics.setLocalizedText(languageLabel, org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.languageLabel.text")); // NOI18N
 
         submitterTabbedPane.addTab(org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.addressEditorPanel.TabConstraints.tabTitle"), addressEditorPanel); // NOI18N
         submitterTabbedPane.addTab(org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.multimediaObjectCitationsListPanel.TabConstraints.tabTitle"), multimediaObjectCitationsListPanel); // NOI18N
@@ -75,19 +103,17 @@ public class SubmitterEditor extends EntityEditor {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(languagesPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(submitterTabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(changeDateLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 309, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(changeDateLabeldate, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(languageLabel)
-                            .addComponent(submitterNameLabel))
+                        .addGap(2, 2, 2)
+                        .addComponent(submitterNameLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(submitterNameTextField)
-                            .addComponent(submitterLanguageTextField))))
+                        .addComponent(submitterNameTextField)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -98,15 +124,14 @@ public class SubmitterEditor extends EntityEditor {
                     .addComponent(submitterNameLabel)
                     .addComponent(submitterNameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(languageLabel)
-                    .addComponent(submitterLanguageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(languagesPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(submitterTabbedPane)
+                .addComponent(submitterTabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(changeDateLabeldate, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(changeDateLabel)))
+                    .addComponent(changeDateLabel))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -115,13 +140,15 @@ public class SubmitterEditor extends EntityEditor {
     private ancestris.modules.editors.genealogyeditor.panels.AddressEditorPanel addressEditorPanel;
     private javax.swing.JLabel changeDateLabel;
     private javax.swing.JLabel changeDateLabeldate;
-    private javax.swing.JLabel languageLabel;
+    private javax.swing.JTextField firstPreferLanguageTextField;
+    private javax.swing.JPanel languagesPanel;
     private ancestris.modules.editors.genealogyeditor.panels.MultimediaObjectCitationsListPanel multimediaObjectCitationsListPanel;
     private ancestris.modules.editors.genealogyeditor.panels.NoteCitationsListPanel noteCitationsListPanel;
-    private javax.swing.JTextField submitterLanguageTextField;
+    private javax.swing.JTextField secondPreferLanguageTextField;
     private javax.swing.JLabel submitterNameLabel;
     private javax.swing.JTextField submitterNameTextField;
     private javax.swing.JTabbedPane submitterTabbedPane;
+    private javax.swing.JTextField thirdPreferLanguageTextField;
     // End of variables declaration//GEN-END:variables
     @Override
     public ViewContext getContext() {
@@ -194,54 +221,72 @@ public class SubmitterEditor extends EntityEditor {
             /*
              * +1 LANG <LANGUAGE_PREFERENCE>    
              */
-            Property language = mSubmitter.getProperty("LANG", false);
-            submitterLanguageTextField.setText(language != null ? language.getValue() : "");
-
-            /*
-             * +1 <<NOTE_STRUCTURE>>
-             */
-            if (!mSubmitter.getGedcom().getGrammar().getVersion().equals("5.5.1")) {
-                int indexOfTab = submitterTabbedPane.indexOfTab(NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle"));
-                if (indexOfTab != -1) {
-                    submitterTabbedPane.removeTabAt(indexOfTab);
+            Property[] preferedLanguages = mSubmitter.getProperties("LANG", false);
+            int index = 0;
+            for (; index < preferedLanguages.length; index++) {
+                if (index == 0) {
+                    firstPreferLanguageTextField.setText(preferedLanguages[0].getValue());
+                } else if (index == 1) {
+                    secondPreferLanguageTextField.setText(preferedLanguages[1].getValue());
+                } else if (index == 2) {
+                    thirdPreferLanguageTextField.setText(preferedLanguages[2].getValue());
                 }
-            } else {
-                if (submitterTabbedPane.indexOfTab(NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle")) == -1) {
-                    submitterTabbedPane.addTab(org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle"), noteCitationsListPanel);
-                }
-                noteCitationsListPanel.set(mSubmitter, Arrays.asList(mSubmitter.getProperties("NOTE")));
             }
 
-            /*
-             * +1 <<CHANGE_DATE>>
-             * Handle by gedcom doUnitOfWork
-             */
-            Property changeDate = mSubmitter.getProperty("CHAN");
-            if (changeDate != null) {
-                changeDateLabeldate.setText(((PropertyChange) changeDate).getDisplayValue());
+            for (; index < 3; index++) {
+                if (index == 0) {
+                    firstPreferLanguageTextField.setText("");
+                } else if (index == 1) {
+                    secondPreferLanguageTextField.setText("");
+                } else if (index == 2) {
+                    thirdPreferLanguageTextField.setText("");
+                }
+            }
+                /*
+                 * +1 <<NOTE_STRUCTURE>>
+                 */
+                if (!mSubmitter.getGedcom().getGrammar().getVersion().equals("5.5.1")) {
+                    int indexOfTab = submitterTabbedPane.indexOfTab(NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle"));
+                    if (indexOfTab != -1) {
+                        submitterTabbedPane.removeTabAt(indexOfTab);
+                    }
+                } else {
+                    if (submitterTabbedPane.indexOfTab(NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle")) == -1) {
+                        submitterTabbedPane.addTab(org.openide.util.NbBundle.getMessage(SubmitterEditor.class, "SubmitterEditor.noteCitationsListPanel.TabConstraints.tabTitle"), noteCitationsListPanel);
+                    }
+                    noteCitationsListPanel.set(mSubmitter, Arrays.asList(mSubmitter.getProperties("NOTE")));
+                }
+
+                /*
+                 * +1 <<CHANGE_DATE>>
+                 * Handle by gedcom doUnitOfWork
+                 */
+                Property changeDate = mSubmitter.getProperty("CHAN");
+                if (changeDate != null) {
+                    changeDateLabeldate.setText(((PropertyChange) changeDate).getDisplayValue());
+                }
+            }
+        }
+
+        @Override
+        public void commit() throws GedcomException {
+
+            if (changes.hasChanged()) {
+                Property name = mSubmitter.getProperty("NAME");
+                if (name != null) {
+                    name.setValue(submitterNameTextField.getText());
+                } else {
+                    mSubmitter.addProperty("NAME", submitterNameTextField.getText());
+                }
+
+                Property language = mSubmitter.getProperty("LANG", false);
+                if (language != null) {
+                    language.setValue(firstPreferLanguageTextField.getText());
+                } else {
+                    mSubmitter.addProperty("LANG", firstPreferLanguageTextField.getText());
+                }
+
+                addressEditorPanel.commit();
             }
         }
     }
-
-    @Override
-    public void commit() throws GedcomException {
-
-        if (changes.hasChanged()) {
-            Property name = mSubmitter.getProperty("NAME");
-            if (name != null) {
-                name.setValue(submitterNameTextField.getText());
-            } else {
-                mSubmitter.addProperty("NAME", submitterNameTextField.getText());
-            }
-
-            Property language = mSubmitter.getProperty("LANG", false);
-            if (language != null) {
-                language.setValue(submitterLanguageTextField.getText());
-            } else {
-                mSubmitter.addProperty("LANG", submitterLanguageTextField.getText());
-            }
-
-            addressEditorPanel.commit();
-        }
-    }
-}
