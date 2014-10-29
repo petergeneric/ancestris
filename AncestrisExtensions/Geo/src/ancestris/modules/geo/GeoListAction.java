e87a843cdf41040e2337cd74e73b7ccac6a6e1a0
dandre <dandre@ancestris.org>
2014-10-29 23:44:06 +0000

    Disable geo and editor view menu entries when no gedcom is selected (ie when startup page has focus)

--- AncestrisExtensions/Geo/src/ancestris/modules/geo/GeoListAction.java
+++ AncestrisExtensions/Geo/src/ancestris/modules/geo/GeoListAction.java
@@ -2,26 +2,37 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ancestris.modules.geo;
 
 import genj.gedcom.Context;
+import genj.gedcom.Property;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.openide.util.Utilities;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 
 /**
  *
  * @author frederic
  */
+@ActionID(id = "ancestris.modules.geo.GeoListAction", category = "Window")
+@ActionRegistration(iconBase = "ancestris/modules/geo/list.png", displayName = "#CTL_GeoListAction", iconInMenu = true)
+@ActionReference(path = "Menu/View", name = "GeoListAction", position = -450)
 public final class GeoListAction implements ActionListener {
+
+    Context context = null;
+
+    public GeoListAction(Property property) {
+        context = new Context(property);
+    }
+
     public void actionPerformed(ActionEvent e) {
         GeoListTopComponent tc = new GeoListTopComponent();
-        Context c = Utilities.actionsGlobalContext().lookup(Context.class);
-        if (c==null){
+        if (context == null) {
             return;
         }
-        tc.init(c);
+        tc.init(context);
         tc.open();
         tc.requestActive();
     }
