14a2635b854ddc5ed9757d81d1193f0a6b39897e
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-10-29 23:44:06 +0000

    Disable geo and editor view menu entries when no gedcom is selected (ie when startup page has focus)

--- trunk/AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/OpenAction.java
+++ trunk/AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/OpenAction.java
@@ -9,23 +9,32 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-
 package ancestris.modules.editors.standard;
 
-import ancestris.gedcom.GedcomDirectory;
 import ancestris.view.AncestrisTopComponent;
 import genj.gedcom.Context;
+import genj.gedcom.Property;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.openide.util.Utilities;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 
+@ActionID(id = "ancestris.editor.EditorStdAction", category = "Window")
+@ActionRegistration(iconBase = "ancestris/modules/editors/standard/editeur_standard.png", displayName = "#CTL_EditorStdAction", iconInMenu = true)
+@ActionReference(path = "Menu/View", name = "AncestrisEditorStdAction", position = -501)
 public final class OpenAction implements ActionListener {
+
+    private Context context = null;
+
+    public OpenAction(Property context) {
+        this.context = new Context(context);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Context contextToOpen = Utilities.actionsGlobalContext().lookup(Context.class);
-        if (contextToOpen != null) {
-            AncestrisTopComponent win = new EditorTopComponent().create(contextToOpen);
+        if (context != null) {
+            AncestrisTopComponent win = new EditorTopComponent().create(context);
 //            win.init(contextToOpen);
             win.open();
             win.requestActive();
--- trunk/AncestrisExtensions/Geo/src/ancestris/modules/geo/GeoMapAction.java
+++ trunk/AncestrisExtensions/Geo/src/ancestris/modules/geo/GeoMapAction.java
@@ -6,18 +6,32 @@
 package ancestris.modules.geo;
 
 import genj.gedcom.Context;
+import genj.gedcom.Property;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.Utilities;
 
+@ActionID(id = "ancestris.modules.geo.GeoMapAction", category = "Window")
+@ActionRegistration(iconBase = "ancestris/modules/geo/geo.png", displayName = "#CTL_GeoMapAction", iconInMenu = true)
+@ActionReference(path = "Menu/View", name = "GeoMapAction", position = -650)
 public final class GeoMapAction implements ActionListener {
+
+    private Context context = null;
+
+    public GeoMapAction(Property property) {
+        context = new Context(property);
+    }
+    
     public void actionPerformed(ActionEvent e) {
         GeoMapTopComponent tc = new GeoMapTopComponent();
-        Context c = Utilities.actionsGlobalContext().lookup(Context.class);
-        if (c == null){
+//        Context c = Utilities.actionsGlobalContext().lookup(Context.class);
+        if (context == null){
             return;
         }
-        tc.init(c);
+        tc.init(context);
         tc.open();
         tc.requestActive();
     }
