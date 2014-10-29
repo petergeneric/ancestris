5bbc40dfed323244fbb0a067b769228934a1d186
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-10-29 19:21:10 +0000

    Disablel most view menu entries when no gedcom is selected (ie when startup page has focus)

--- trunk/AncestrisCore/ancestris.app/src/ancestris/app/OpenGenjViewAction.java
+++ trunk/AncestrisCore/ancestris.app/src/ancestris/app/OpenGenjViewAction.java
@@ -2,13 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ancestris.app;
 
-import ancestris.gedcom.GedcomDirectory;
 import ancestris.view.AncestrisTopComponent;
 import genj.gedcom.Context;
-import ancestris.core.actions.AbstractAncestrisAction;
+import ancestris.core.actions.AbstractAncestrisContextAction;
 import genj.util.swing.ImageIcon;
 import java.awt.event.ActionEvent;
 import java.util.Map;
@@ -18,25 +16,32 @@ import org.openide.util.Utilities;
  *
  * @author daniel
  */
-final public class OpenGenjViewAction extends AbstractAncestrisAction  {
-    private final Map<?,?> map;
+final public class OpenGenjViewAction extends AbstractAncestrisContextAction {
+
+    private final Map<?, ?> map;
     private final AncestrisTopComponent component;
 
-    public OpenGenjViewAction(AncestrisTopComponent component, Map<?,?> map) {
+    public OpenGenjViewAction(AncestrisTopComponent component, Map<?, ?> map) {
         this.component = component;
         this.map = map;
         setImage(new ImageIcon(component.getImageIcon()));
-        setText((String)map.get("displayName"));
+        setText((String) map.get("displayName"));
 //        setText(component.getViewFactory().getTitle(true));
 //        putValue(Action.NAME,(String)map.get("displayName"));
 //        putValue(Action.SMALL_ICON, component.getViewFactory().getImage());
     }
 
+    @Override
+    protected void contextChanged() {
+        setEnabled(!contextProperties.isEmpty());
+        super.contextChanged();
+    }
+
     /** execute callback */
     @Override
-  public void actionPerformed(ActionEvent e) {
+    public void actionPerformedImpl(ActionEvent e) {
         Context contextToOpen = Utilities.actionsGlobalContext().lookup(Context.class);
-        if (contextToOpen != null){
+        if (contextToOpen != null) {
             AncestrisTopComponent win = component.create(contextToOpen);
 //            win.init(contextToOpen);
             win.open();
