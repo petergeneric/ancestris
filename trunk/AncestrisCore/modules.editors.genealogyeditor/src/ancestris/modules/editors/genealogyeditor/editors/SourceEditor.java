95d9a5fd67a4c01d3672cb895087fdfbb5997553
lemovice <lemovice@246880a2-2d26-488f-8150-ff0ebaa74848>
2014-11-23 17:14:20 +0000

    Fix  empty created tags

--- trunk/AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/editors/SourceEditor.java
+++ trunk/AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/editors/SourceEditor.java
@@ -6,7 +6,6 @@ import java.awt.Component;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 
 /*
@@ -484,35 +483,45 @@ public class SourceEditor extends EntityEditor {
     public void commit() {
 
         if (changes.hasChanged()) {
-            Property sourceTitle = mSource.getProperty("TITL");
-            if (sourceTitle == null) {
-                mSource.addProperty("TITL", sourceTitleTextField.getText());
-            } else {
-                sourceTitle.setValue(sourceTitleTextField.getText());
+            if (!sourceTitleTextField.getText().isEmpty()) {
+                Property sourceTitle = mSource.getProperty("TITL");
+                if (sourceTitle == null) {
+                    mSource.addProperty("TITL", sourceTitleTextField.getText());
+                } else {
+                    sourceTitle.setValue(sourceTitleTextField.getText());
+                }
             }
-            Property publicationFacts = mSource.getProperty("PUBL");
-            if (publicationFacts == null) {
-                mSource.addProperty("PUBL", sourceTextTextArea.getText());
-            } else {
-                publicationFacts.setValue(sourceTextTextArea.getText());
+            if (!sourceTextTextArea.getText().isEmpty()) {
+                Property publicationFacts = mSource.getProperty("PUBL");
+                if (publicationFacts == null) {
+                    mSource.addProperty("PUBL", sourceTextTextArea.getText());
+                } else {
+                    publicationFacts.setValue(sourceTextTextArea.getText());
+                }
             }
-            Property sourceText = mSource.getProperty("TEXT");
-            if (sourceText == null) {
-                mSource.addProperty("TEXT", sourceTextTextArea.getText());
-            } else {
-                sourceText.setValue(sourceTextTextArea.getText());
+            if (!sourceTextTextArea.getText().isEmpty()) {
+                Property sourceText = mSource.getProperty("TEXT");
+                if (sourceText == null) {
+                    mSource.addProperty("TEXT", sourceTextTextArea.getText());
+                } else {
+                    sourceText.setValue(sourceTextTextArea.getText());
+                }
             }
-            Property abbreviation = mSource.getProperty("ABBR");
-            if (abbreviation == null) {
-                mSource.addProperty("ABBR", abbreviationTextField.getText());
-            } else {
-                abbreviation.setValue(abbreviationTextField.getText());
+            if (!abbreviationTextField.getText().isEmpty()) {
+                Property abbreviation = mSource.getProperty("ABBR");
+                if (abbreviation == null) {
+                    mSource.addProperty("ABBR", abbreviationTextField.getText());
+                } else {
+                    abbreviation.setValue(abbreviationTextField.getText());
+                }
             }
-            Property sourceAuthor = mSource.getProperty("AUTH");
-            if (sourceAuthor == null) {
-                mSource.addProperty("AUTH", authorTextField.getText());
-            } else {
-                sourceAuthor.setValue(authorTextField.getText());
+            if (!authorTextField.getText().isEmpty()) {
+                Property sourceAuthor = mSource.getProperty("AUTH");
+                if (sourceAuthor == null) {
+                    mSource.addProperty("AUTH", authorTextField.getText());
+                } else {
+                    sourceAuthor.setValue(authorTextField.getText());
+                }
             }
         }
     }
