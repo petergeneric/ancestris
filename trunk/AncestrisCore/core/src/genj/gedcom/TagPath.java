9a9a8eb1d2b9d8584015eec68159f641d5b31fdf
dandre <dandre@246880a2-2d26-488f-8150-ff0ebaa74848>
2015-05-12 14:46:46 +0000

    small api update: TagPath->get(-n) return tag part from the end. get(-1) returns last element

--- trunk/AncestrisCore/core/src/genj/gedcom/TagPath.java
+++ trunk/AncestrisCore/core/src/genj/gedcom/TagPath.java
@@ -245,11 +245,17 @@ public class TagPath implements Comparable{
   }
   
   /**
-   * Returns the n-th tag of this path 
-   * @param which 0-based number
+   * Returns the n-th tag of this path.
+   * @param which 0-based number. If negative starts from the end. 
+   * get(-1) returns the las element (equivalent to getLast)
    * @return tag as <code>String</code>
    */
   public String get(int which) {
+      if (which<0)
+          which += len;
+      
+      if (which<0)
+          return null;
     return tags[which];
   }
 
