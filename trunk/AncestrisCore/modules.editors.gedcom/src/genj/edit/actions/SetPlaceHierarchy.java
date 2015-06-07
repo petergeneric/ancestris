420f05681609b1eebef7e51325220409aa403d97
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2015-06-07 19:57:40 +0000

    Uses placeformat panel of Gedcom Properties

--- trunk/AncestrisCore/modules.editors.gedcom/src/genj/edit/actions/SetPlaceHierarchy.java
+++ trunk/AncestrisCore/modules.editors.gedcom/src/genj/edit/actions/SetPlaceHierarchy.java
@@ -29,23 +29,27 @@ import genj.gedcom.Grammar;
 import genj.gedcom.Property;
 import genj.gedcom.PropertyPlace;
 import genj.gedcom.TagPath;
+import genj.gedcom.UnitOfWork;
 import genj.util.swing.ImageIcon;
 import genj.util.swing.NestedBlockLayout;
 import genj.util.swing.TextAreaWidget;
-import genj.util.swing.TextFieldWidget;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.Collection;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import modules.editors.gedcomproperties.GedcomPropertiesPlaceFormatPanel;
 import modules.editors.gedcomproperties.utils.GedcomPlacesConverter;
 import modules.editors.gedcomproperties.utils.PlaceFormatConverterPanel;
+import modules.editors.gedcomproperties.utils.PlaceFormatInterface;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.LookupEvent;
 import org.openide.util.NbBundle;
 
@@ -57,13 +61,23 @@ import org.openide.util.NbBundle;
         lazy = false)
 @ActionReferences(value = {
     @ActionReference(path = "Ancestris/Actions/GedcomProperty")})
-public class SetPlaceHierarchy extends AbstractChange {
+public class SetPlaceHierarchy extends AbstractChange implements PlaceFormatInterface {
 
     private static final ImageIcon IMG = Grammar.V55.getMeta(TagPath.valueOf("INDI:BIRT:PLAC")).getImage();
+
     /** the place to use as the global example */
     private PropertyPlace place;
-    /** textfield for hierarchy */
-    private TextFieldWidget hierarchy;
+
+    /** main place panel */
+    GedcomPropertiesPlaceFormatPanel placePanel;
+            
+    /** textfield for warning msg */
+    private JLabel wrngMsg;
+
+    /** place format converter */
+    PlaceFormatConverterPanel pfc;
+
+
     
     public SetPlaceHierarchy() {
         super();
@@ -122,62 +136,85 @@ public class SetPlaceHierarchy extends AbstractChange {
     }
 
     /**
-     * Override content components to show to user
+     * Override content components to show to user to change place format
      */
     @Override
     protected JPanel getDialogContent() {
-
-        JPanel result = new JPanel(new NestedBlockLayout("<col><row><text wx=\"1\" wy=\"1\" pad=\"5\"/></row><table><row><label pad=\"5\"/><label/><text/></row><row><label pad=\"5\"/><label/><text/></row></table><row><text wx=\"1\" wy=\"1\" pad=\"5\"/></row></col>"));
-
-        // prepare textfield for format
-        TextFieldWidget file = new TextFieldWidget(place.getGedcom().getName());
-        file.setEditable(false);
-        hierarchy = new TextFieldWidget(place.getFormatAsString());
-        hierarchy.setBackground(Color.WHITE);
-        Color defaultColor = new Color(240,240,240);
-
-        // Add panel components
-        result.add(getComponent(resources.getString("place.hierarchy.msg1", ""), 4, defaultColor));
-        result.add(new JLabel(resources.getString("place.hierarchy.label1", "")));
-        result.add(new JLabel("   "));
-        result.add(file);
-        result.add(new JLabel(resources.getString("place.hierarchy.label2", "")));
-        result.add(new JLabel("   "));
-        result.add(hierarchy);
-        result.add(getComponent(resources.getString("place.hierarchy.msg2", ""), 9, defaultColor));
-
-        // done
+        JPanel result = new JPanel(new NestedBlockLayout("<col>"
+                + "<row><text wx=\"1\" wy=\"1\" pad=\"5\"/></row>"
+                + "<row><text wx=\"1\" wy=\"1\" pad=\"17\"/></row>"
+                + "</col>"));        
+        placePanel = new GedcomPropertiesPlaceFormatPanel(this);
+        placePanel.setPLAC(getOriginalPlaceFormat());
+        result.add(placePanel);
+        wrngMsg = new JLabel("");
+        // Add both lines below is warning is to be displayed (not so useful)
+        //result.add(wrngMsg);
+        //result.setPreferredSize(new Dimension(result.getPreferredSize().height, result.getPreferredSize().width));
+        
         return result;
     }
 
+    @Override
+    public void warnVersionChange(boolean canBeConverted) {
+        if (wrngMsg == null) {
+            return;
+        }
+        if (canBeConverted) {
+           wrngMsg.setText(NbBundle.getMessage(GedcomPropertiesPlaceFormatPanel.class, "WNG_PlaceFormatChanged"));    
+        } else {
+           wrngMsg.setText("");    
+        }
+    }
+
+    @Override
+    public String getOriginalPlaceFormat() {
+        return place.getGedcom().getPlaceFormat();
+    }
+
+    @Override
+    public void setPlaceFormatConverter(PlaceFormatConverterPanel pfc) {
+        this.pfc = pfc;
+    }
+
+    @Override
+    public PlaceFormatConverterPanel getPlaceFormatConverter() {
+        return this.pfc;
+    }
+
+    
     /**
-     * set the submitter
+     * Converts places to new place format
+     * @param gedcom
+     * @param event
+     * @return 
+     * @throws genj.gedcom.GedcomException
      */
     @Override
     protected Context execute(Gedcom gedcom, ActionEvent event) throws GedcomException {
-        String currentFormat = place.getGedcom().getPlaceFormat();
-        String newFormat = hierarchy.getText().trim();
-        String title = NbBundle.getMessage(SetPlaceHierarchy.class, "TITL_PlacesFormatModification");
-        String msg = "";
-        if (!currentFormat.equals(newFormat)) {
-            PlaceFormatConverterPanel pfc = new PlaceFormatConverterPanel(currentFormat, newFormat);
-            pfc.initToFields(newFormat);
-            Object o = DialogManager.create(NbBundle.getMessage(PlaceFormatConverterPanel.class, "TITL_PlaceFormatConversionSettings"), pfc).setMessageType(DialogManager.PLAIN_MESSAGE).show();
-            if (o == DialogManager.OK_OPTION) {
-                pfc.setValidatedMap(true, null);
-                final GedcomPlacesConverter placesConverter = new GedcomPlacesConverter(gedcom, currentFormat, newFormat, pfc.getConversionMapAsString());
+        if (placePanel.getConversionToBeDone()) {
+            Object o = DialogManager.createYesNo(
+                    NbBundle.getMessage(GedcomPropertiesPlaceFormatPanel.class, "GedcomPropertiesPlaceFormatPanel.jCheckBox1.text"),
+                    NbBundle.getMessage(GedcomPropertiesPlaceFormatPanel.class, "WNG_ConfirmPlaceConversion")).setMessageType(DialogManager.YES_NO_OPTION).show();
+            if (o == DialogManager.YES_OPTION) {
+                String title = NbBundle.getMessage(SetPlaceHierarchy.class, "TITL_PlacesFormatModification");
+                String msg = "";
+                GedcomPlacesConverter placesConverter = new GedcomPlacesConverter(gedcom, getOriginalPlaceFormat(), placePanel.getPLAC(), pfc.getConversionMapAsString());
                 if (placesConverter.convert()) {
-                    // ok
                     msg = NbBundle.getMessage(SetPlaceHierarchy.class, "MSG_GedcomModified", placesConverter.getNbOfDifferentChangedPlaces(), placesConverter.getNbOfDifferentFoundPlaces());
-                    place.getGedcom().setPlaceFormat(newFormat);
+                    place.getGedcom().setPlaceFormat(placePanel.getPLAC());
                 } else {
-                    //ko
                     msg = NbBundle.getMessage(SetPlaceHierarchy.class, "MSG_GedcomNotModified", placesConverter.getNbOfDifferentChangedPlaces(), placesConverter.getNbOfDifferentFoundPlaces());
                 }
                 DialogManager.create(title, msg).setMessageType(DialogManager.INFORMATION_MESSAGE).show();
             }
         }
         return new Context(place);
+    }
+
+    @Override
+    public int getMode() {
+        return GedcomPropertiesPlaceFormatPanel.DEFAULT_MODE;
     }
 } //SetPlaceFormat
 
