425e208b96c28d7df6cf642f57e5b3c312b16ba3
frederic <frederic@ancestris.org>
2015-06-25 20:10:14 +0000

    Continuation...

--- AncestrisExtensions/TreeSharing/src/ancestris/modules/treesharing/panels/SharedGedcom.java
+++ AncestrisExtensions/TreeSharing/src/ancestris/modules/treesharing/panels/SharedGedcom.java
@@ -90,7 +90,7 @@ public class SharedGedcom extends JInternalFrame implements GedcomListener {
         setIconifiable(true);
         setToolTipText(org.openide.util.NbBundle.getMessage(SharedGedcom.class, "SharedGedcom.toolTipText")); // NOI18N
         setFrameIcon(new javax.swing.ImageIcon(getClass().getResource("/ancestris/modules/treesharing/resources/tree.png"))); // NOI18N
-        setPreferredSize(new java.awt.Dimension(280, 130));
+        setPreferredSize(new java.awt.Dimension(300, 130));
         setRequestFocusEnabled(false);
         setVisible(true);
 
@@ -202,7 +202,7 @@ public class SharedGedcom extends JInternalFrame implements GedcomListener {
                     .addComponent(jLabel8, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jCheckBox1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jCheckBox2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
