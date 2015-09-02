e9293f0a279562d30c335caff0c2c641a9b322c3
frederic <frederic@ancestris.org>
2015-09-02 21:25:39 +0000

    - Remove trackers. Communication now works fine with large files.

--- AncestrisExtensions/TreeSharing/src/ancestris/modules/treesharing/SearchSharedTrees.java
+++ AncestrisExtensions/TreeSharing/src/ancestris/modules/treesharing/SearchSharedTrees.java
@@ -76,7 +76,7 @@ public class SearchSharedTrees extends Thread {
         Set<String> myIndiLastnames = owner.getCommHandler().getMySharedIndiLastnames(sharedGedcoms);
         Set<String> myFamLastnames = owner.getCommHandler().getMySharedFamLastnames(sharedGedcoms);  
         
-        owner.getCommHandler().debug();
+        //owner.getCommHandler().debug();
         
         // Loop on all members
         for (AncestrisMember member : copyOfAncestrisMembers) {
@@ -89,70 +89,70 @@ public class SearchSharedTrees extends Thread {
             
             // A. Individuals
             // Phase 1 - Get all lastnames from member for all its shared gedcoms at the same time and identify commons ones with mine
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ current member is : " + member.getMemberName() + ". Asking lastnames...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ current member is : " + member.getMemberName() + ". Asking lastnames...");
             Set<String> memberIndiLastnames = owner.getCommHandler().getSharedIndiLastnamesFromMember(member);
             if (memberIndiLastnames == null || memberIndiLastnames.isEmpty()) {
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no lastnames received. Exit");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no lastnames received. Exit");
                 continue;
             }
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of lastnames received is : " + memberIndiLastnames.size() + ".......Extracting common ones : ");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of lastnames received is : " + memberIndiLastnames.size() + ".......Extracting common ones : ");
             Set<String> commonIndiLastnames = getCommonItems(myIndiLastnames, new HashSet<String>(memberIndiLastnames));
             if (commonIndiLastnames == null || commonIndiLastnames.isEmpty()) {
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no common lastnames found. Exit");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no common lastnames found. Exit");
                 continue;
             }
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of common lastnames found is : " + commonIndiLastnames.size() + ". Listing them...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of common lastnames found is : " + commonIndiLastnames.size() + ". Listing them...");
             for (String str : commonIndiLastnames) {
-                LOG_DEBUG.log(Level.INFO, ".......DEBUG common lastnames found : " + str);
+                //LOG_DEBUG.log(Level.INFO, ".......DEBUG common lastnames found : " + str);
             }
             
             // Phase 2 - Get individual details for common lastnames from member and identify matching ones according to matching type
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Asking details of those individuals...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Asking details of those individuals...");
             Set<GedcomIndi> memberGedcomIndis = owner.getCommHandler().getGedcomIndisFromMember(member, commonIndiLastnames);
             if (memberGedcomIndis != null && !memberGedcomIndis.isEmpty()) {
                 // Phase 3 - Identify and create/update matches
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Details have been received. Extracting the ones in common...");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Details have been received. Extracting the ones in common...");
                 Set<GedcomIndi> myGedcomIndis = owner.getCommHandler().getMySharedGedcomIndis(sharedGedcoms, commonIndiLastnames);
                 if (myGedcomIndis != null && !myGedcomIndis.isEmpty()) {
-                    LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Nb of my individuals with common lastnames : " + myGedcomIndis.size() + ". Checking matches...");
+                    //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Nb of my individuals with common lastnames : " + myGedcomIndis.size() + ". Checking matches...");
                     addCommonIndis(sharedGedcoms, myGedcomIndis, memberGedcomIndis, matchType, member);    // create/update matches and friends
                 } else {
-                    LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ No individuals in common have been found.");
+                    //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ No individuals in common have been found.");
                 }
             }
 
             
             // B. Families
             // Phase 1 - Get all fams lastnames from member for all its shared gedcoms at the same time and identify commons ones with mine
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ current member is : " + member.getMemberName() + ". Asking family names...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ current member is : " + member.getMemberName() + ". Asking family names...");
             Set<String> memberFamLastnames = owner.getCommHandler().getSharedFamLastnamesFromMember(member);
             if (memberFamLastnames == null || memberFamLastnames.isEmpty()) {
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no family names received. Exit");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no family names received. Exit");
                 continue;
             }
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of family names received is : " + memberFamLastnames.size() + ". Extracting common ones...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of family names received is : " + memberFamLastnames.size() + ". Extracting common ones...");
             Set<String> commonFamLastnames = getCommonItems(myFamLastnames, new HashSet<String>(memberFamLastnames));
             if (commonFamLastnames == null || commonFamLastnames.isEmpty()) {
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no common family names found. Exit");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ no common family names found. Exit");
                 continue;
             }
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of common family names found is : " + commonFamLastnames.size() + ". Listing them...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ nb of common family names found is : " + commonFamLastnames.size() + ". Listing them...");
             for (String str : commonFamLastnames) {
-                LOG_DEBUG.log(Level.INFO, ".......DEBUG common family names found : " + str);
+                //LOG_DEBUG.log(Level.INFO, ".......DEBUG common family names found : " + str);
             }
             
             // Phase 2 - Get individual details for common fam lastnames from member and identify matching ones according to matching type
-            LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Asking details of those families...");
+            //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Asking details of those families...");
             Set<GedcomFam> memberGedcomFams = owner.getCommHandler().getGedcomFamsFromMember(member, commonFamLastnames);
             if (memberGedcomFams != null && !memberGedcomFams.isEmpty()) {
                 // Phase 3 - Identify and create/update matches
-                LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Details have been received. Extracting the ones in common...");
+                //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Details have been received. Extracting the ones in common...");
                 Set<GedcomFam> myGedcomFams = owner.getCommHandler().getMySharedGedcomFams(sharedGedcoms, commonFamLastnames);
                 if (myGedcomFams != null && !myGedcomFams.isEmpty()) {
-                    LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Nb of my individuals with common family names : " + myGedcomFams.size() + ". Checking matches...");
+                    //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ Nb of my individuals with common family names : " + myGedcomFams.size() + ". Checking matches...");
                     addCommonFams(sharedGedcoms, myGedcomFams, memberGedcomFams, matchType, member);    // create/update matches and friends
                 } else {
-                    LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ No family in common have been found.");
+                    //LOG_DEBUG.log(Level.INFO, "¤¤¤¤¤¤ DEBUG ¤¤¤¤¤¤ No family in common have been found.");
                 }
                 
             }
