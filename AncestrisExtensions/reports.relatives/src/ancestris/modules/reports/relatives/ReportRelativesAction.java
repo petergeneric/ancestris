914dacb1c634f18dc065181ce6fb1f6203b16519
frederic <frederic@ancestris.org>
2016-01-24 15:16:57 +0000

    General improvement of all menus (icons, labels, enable/disable, etc.)

--- AncestrisExtensions/reports.relatives/src/ancestris/modules/reports/relatives/ReportRelativesAction.java
+++ AncestrisExtensions/reports.relatives/src/ancestris/modules/reports/relatives/ReportRelativesAction.java
@@ -11,6 +11,7 @@
  */
 package ancestris.modules.reports.relatives;
 
+import ancestris.core.actions.AbstractAncestrisContextAction;
 import ancestris.modules.document.view.FopDocumentView;
 import static ancestris.modules.reports.relatives.Bundle.*;
 import ancestris.util.swing.SelectEntityDialog;
@@ -19,32 +20,51 @@ import genj.gedcom.Context;
 import genj.gedcom.Gedcom;
 import genj.gedcom.Indi;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.prefs.Preferences;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
-import org.openide.util.Utilities;
+@ActionID(id = "ancestris.modules.reports.relatives.ReportRelativesAction", category = "Tools")
+@ActionRegistration(
+        displayName = "#CTL_ReportRelativesAction",
+        iconInMenu = true,
+        lazy = false)
+@ActionReference(path = "Menu/View/Reports", name = "ReportRelativesAction", position = 30)
 @NbBundle.Messages({
     "tabtitle.short=Relatives",
     "tabtitle={0}: Close Relatives"
 })
-public final class ReportRelativesAction implements ActionListener {
+public final class ReportRelativesAction extends AbstractAncestrisContextAction {
 
     private final Preferences modulePreferences = NbPreferences.forModule(ReportRelatives.class);
 
+    public ReportRelativesAction() {
+        super();
+        setImage("ancestris/modules/reports/relatives/relatives.png");
+        setText(NbBundle.getMessage(ReportRelativesAction.class, "CTL_ReportRelativesAction"));
+    }
+    
     @Override
-    public void actionPerformed(ActionEvent e) {
-        Context context;
+    protected void contextChanged() {
+        setEnabled(!contextProperties.isEmpty());
+        super.contextChanged();
+    }
 
-        if ((context = Utilities.actionsGlobalContext().lookup(Context.class)) != null) {
-            Gedcom myGedcom = context.getGedcom();
+    @Override
+    protected void actionPerformedImpl(ActionEvent event) {
+        Context contextToOpen = getContext();
+
+        if (contextToOpen != null) {
+            Gedcom myGedcom = contextToOpen.getGedcom();
 
             SelectEntityDialog selectEntityDialog = new SelectEntityDialog(NbBundle.getMessage(this.getClass(), "ReportRelatives.AskDeCujus"), myGedcom, Gedcom.INDI);
             Indi indiDeCujus = (Indi) selectEntityDialog.getEntity();
             if (indiDeCujus != null) {
                 Document document = new ReportRelatives().start(indiDeCujus);
                 if (document != null) {
-                    FopDocumentView window = new FopDocumentView(context,  tabtitle_short(),tabtitle(context.toString()));
+                    FopDocumentView window = new FopDocumentView(contextToOpen,  tabtitle_short(),tabtitle(contextToOpen.toString()));
                     window.displayDocument(document, modulePreferences);
                 }
             }
