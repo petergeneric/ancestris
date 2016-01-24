2d84211103fae2dd8fecf0dd2d39e5bb11091460
frederic <frederic@ancestris.org>
2016-01-24 15:15:40 +0000

    General improvement of all menus (icons, labels, enable/disable, etc.)

--- AncestrisExtensions/flashlist/src/ancestris/modules/flashlist/FlashListAction.java
+++ AncestrisExtensions/flashlist/src/ancestris/modules/flashlist/FlashListAction.java
@@ -1,42 +1,60 @@
 package ancestris.modules.flashlist;
 
+import ancestris.core.actions.AbstractAncestrisContextAction;
 import ancestris.modules.document.view.FopDocumentView;
 import static ancestris.modules.flashlist.Bundle.*;
 import genj.fo.Document;
 import genj.gedcom.Context;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.prefs.Preferences;
 import org.netbeans.api.options.OptionsDisplayer;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
-import org.openide.util.Utilities;
 
+@ActionID(id = "ancestris.modules.flashlist.FlashListAction", category = "Tools")
+@ActionRegistration(
+        displayName = "#CTL_FlashListAction",
+        iconInMenu = true,
+        lazy = false)
+@ActionReference(path = "Menu/View/Reports", name = "CommonAncestorOpenAction", position = 10)
 @NbBundle.Messages({
     "title={0}: Flash Lists",
     "title.short=Flash Lists",
 })
-public final class FlashListAction implements ActionListener {
+public final class FlashListAction extends AbstractAncestrisContextAction {
 
-    private Context context;
+    public FlashListAction() {
+        super();
+        setImage("ancestris/modules/flashlist/ReportFO.png");
+        setText(NbBundle.getMessage(FlashListAction.class, "CTL_FlashListAction"));
+    }
+    
+    @Override
+    protected void contextChanged() {
+        setEnabled(!contextProperties.isEmpty());
+        super.contextChanged();
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    protected void actionPerformedImpl(ActionEvent event) {
         Preferences modulePreferences = NbPreferences.forModule(ReportFlashList.class);
 
-        context = Utilities.actionsGlobalContext().lookup(Context.class);
-        if (context != null) {
+        Context contextToOpen = getContext();
+        if (contextToOpen != null) {
             if (modulePreferences.getInt("displayLegendComboBox", 3) == 3) {
                 NotifyDescriptor nd = new NotifyDescriptor.Message(NbBundle.getMessage(FlashListPanel.class, "FlashListPanel.setParameters"), NotifyDescriptor.INFORMATION_MESSAGE);
                 DialogDisplayer.getDefault().notify(nd);
 
                 OptionsDisplayer.getDefault().open("Extensions/FlashList");
             } else {
-                Document doc = new ReportFlashList().start(context.getGedcom(), modulePreferences.get("reportFilename", "flash-list"));
+                Document doc = new ReportFlashList().start(contextToOpen.getGedcom(), modulePreferences.get("reportFilename", "flash-list"));
                 if (doc != null) {
-                    FopDocumentView window = new FopDocumentView(context, title_short(),title(context.getGedcom().getName()));
+                    FopDocumentView window = new FopDocumentView(contextToOpen, title_short(),title(contextToOpen.getGedcom().getName()));
                     window.displayDocument(doc, modulePreferences);
                 }
             }
