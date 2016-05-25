9f6f3f507f1b394bdd7266389fc9242323c9db39
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2016-05-25 21:48:12 +0000

    Edition of an individual from outside the editor will re-use an existing editor rather than adding a tab editor.

--- trunk/AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/actions/EditorAction.java
+++ trunk/AncestrisCore/modules.editors.standard/src/ancestris/modules/editors/standard/actions/EditorAction.java
@@ -17,6 +17,7 @@ import ancestris.core.pluginservice.AncestrisPlugin;
 import ancestris.modules.editors.standard.EditorTopComponent;
 import ancestris.modules.editors.standard.tools.IndiCreator;
 import ancestris.view.AncestrisTopComponent;
+import ancestris.view.SelectionDispatcher;
 import genj.gedcom.Context;
 import genj.gedcom.Fam;
 import genj.gedcom.Indi;
@@ -59,9 +60,16 @@ public class EditorAction extends AncestrisEditor {
             contextToOpen = new Context(property);
         }
         if (contextToOpen != null) {
-            AncestrisTopComponent win = new EditorTopComponent().create(contextToOpen);
-            win.open();
-            win.requestActive();
+            EditorTopComponent editorTopComponent = getCurrentEditorTopComponent(contextToOpen);
+            if (editorTopComponent != null) {
+                //editorTopComponent.setContext(contextToOpen);
+                //editorTopComponent.requestActive();
+                SelectionDispatcher.fireSelection(contextToOpen);
+            } else {
+                AncestrisTopComponent win = new EditorTopComponent().create(contextToOpen);
+                win.open();
+                win.requestActive();
+            }
         }
         return null;
     }
@@ -86,30 +94,44 @@ public class EditorAction extends AncestrisEditor {
 
     @Override
     public Action getCreateParentAction(Indi indi, int sex) {
-        for (EditorTopComponent editorTopComponent : (List<EditorTopComponent>) AncestrisPlugin.lookupAll(EditorTopComponent.class)) {
-            if (editorTopComponent.getEditor() != null && editorTopComponent.getEditor().getEditedIndi() == indi) {
-                return new ActionCreation(
+        EditorTopComponent editorTopComponent = getCurrentEditorTopComponent(new Context(indi));
+        if (editorTopComponent != null) {
+            return new ActionCreation(
                         editorTopComponent, 
                         IndiCreator.CREATION, 
                         sex == PropertySex.MALE ? IndiCreator.REL_FATHER : IndiCreator.REL_MOTHER);
-                
-            }
         }
         return getDefaultAction(indi);
     }
 
     @Override
     public Action getCreateSpouseAction(Indi indi) {
-        for (EditorTopComponent editorTopComponent : (List<EditorTopComponent>) AncestrisPlugin.lookupAll(EditorTopComponent.class)) {
-            if (editorTopComponent.getEditor() != null && editorTopComponent.getEditor().getEditedIndi() == indi) {
-                return new ActionCreation(
+        EditorTopComponent editorTopComponent = getCurrentEditorTopComponent(new Context(indi));
+        if (editorTopComponent != null) {
+            return new ActionCreation(
                         editorTopComponent, 
                         IndiCreator.CREATION, 
                         IndiCreator.REL_PARTNER);
                 
-            }
         }
         return getDefaultAction(indi);
+    }
+    
+    
+    
+    
+
+    private EditorTopComponent getCurrentEditorTopComponent(Context context) {
+        EditorTopComponent ret = null;
+        for (EditorTopComponent editorTopComponent : (List<EditorTopComponent>) AncestrisPlugin.lookupAll(EditorTopComponent.class)) {
+            if (editorTopComponent.getEditor() != null) {
+                if (editorTopComponent.getEditor().getEditedIndi() == context.getEntity()) {
+                    return editorTopComponent;
+                }
+                ret = editorTopComponent;
+            }
+        }
+        return ret;
     }
     
     
