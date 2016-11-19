d4062e6008531535475645e1f0fe61b45c4f3ff8
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2016-11-19 13:03:32 +0000

    Improve Gedart: ensure default templates are always available.

--- trunk/AncestrisCore/reports/src/ancestris/reports/gedart/GedartTemplates.java
+++ trunk/AncestrisCore/reports/src/ancestris/reports/gedart/GedartTemplates.java
@@ -1,105 +1,108 @@
 package ancestris.reports.gedart;
 
-import genj.report.ReportLoader;
 import genj.util.EnvironmentChecker;
+import genj.util.PackageUtils;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
+import org.apache.commons.io.FileUtils;
+import org.openide.util.Exceptions;
 
 class GedartTemplates extends TreeMap<String, GedartTemplate> {
 
-	public GedartTemplates() {
-		// first templates in report/gedart/templates
-            // XXX: Does not work in ancestris, see SamplePrivider implementation
-//		File gedartDir = new File(ReportLoader.getReportDirectory(),
-//				"gedart/templates");
-//		putAll(new GedartTemplates(gedartDir));
+    public GedartTemplates() {
 
-            
-// XXX: copied from almanach:            
-//        @Override
-//        protected void loadFromResources(File[] loaded) {
-//            Set<String> seen = new HashSet<String>();
-//            for (File file:loaded){
-//                seen.add(file.getName());
-//            }
-//            final String PCKNAME = "genj.almanac.resources";
-//            try {
-//                for (String res : PackageUtils.findInPackage(PCKNAME, Pattern.compile(".*/[^/]*\\.almanac"))) {
-//                    String name = res.substring(PCKNAME.length() + 1);
-//                    if (seen.contains(name))
-//                        continue;
-//                    try {
-//                        load(new BufferedReader(
-//                                new InputStreamReader(
-//                                Almanac.class.getResourceAsStream("/" + PCKNAME.replace('.', '/') + "/" +name))));
-//                    } catch (Exception ex) {
-//                        LOG.log(Level.WARNING, "IO Problem reading " + res, ex);
-//                        Exceptions.printStackTrace(ex);
-//                    }
-//                }
-//            } catch (ClassNotFoundException ex) {
-//                Exceptions.printStackTrace(ex);
-//            }
-//        }
-            
-            
-            
-            
-            
-		// second templates in {user.home.ancestris}/gedart/contrib-templates
-		File dir = new File(EnvironmentChecker.getProperty(
-				"user.home.ancestris/gedart/contrib-templates", "?",
-				"Looking for gedart/contrib-templates"));
+        // First templates in {user.home.ancestris}/gedart/contrib-templates
+        File dir = new File(EnvironmentChecker.getProperty(
+                "user.home.ancestris/gedart/contrib-templates", "?",
+                "Looking for gedart/contrib-templates"));
 
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		putAll(new GedartTemplates(dir));
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        // ... ensure templates resources files as a dir into it 
+        File dirDefault = new File(dir + File.separator + "default");
+        if (!dirDefault.exists()) {
+            dirDefault.mkdirs();
+        }
+        File dest = null;
+        final String PCKNAME = "ancestris.reports.gedart.templates.resources";
+        try {
+            for (String res : PackageUtils.findInPackage(PCKNAME, Pattern.compile(".*/[^/]*\\" + ".vm"))) {
+                String name = res.substring(PCKNAME.length() + 1);
+                URL inputUrl = GedartTemplates.class.getResource("/" + PCKNAME.replace('.', '/') + "/" + name);
+                dest = new File(dirDefault + File.separator + name);
+                FileUtils.copyURLToFile(inputUrl, dest);
+            }
+            for (String res : PackageUtils.findInPackage(PCKNAME, Pattern.compile(".*/[^/]*\\" + ".properties"))) {
+                String name = res.substring(PCKNAME.length() + 1);
+                URL inputUrl = GedartTemplates.class.getResource("/" + PCKNAME.replace('.', '/') + "/" + name);
+                dest = new File(dirDefault + File.separator + name);
+                FileUtils.copyURLToFile(inputUrl, dest);
+            }
+        } catch (Exception ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        
+        // ... then load content of directory
+        putAll(new GedartTemplates(dir));
 
-		// then templates in {user.home.ancestris}/gedart/templates
-		dir = new File(EnvironmentChecker.getProperty(
-				"user.home.ancestris/gedart/templates", "?",
-				"Looking for gedart/templates"));
+        
+        
+        // then look for templates in {user.home.ancestris}/gedart/templates
+        dir = new File(EnvironmentChecker.getProperty(
+                "user.home.ancestris/gedart/templates", "?",
+                "Looking for gedart/templates"));
 
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
-		putAll(new GedartTemplates(dir));
-	}
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        putAll(new GedartTemplates(dir));
 
-	public GedartTemplates(File dir) {
-		if (dir.isDirectory()) {
-			// loop over Templates
-			File[] files = dir.listFiles();
-			for (int b = 0; b < files.length; b++) {
-				GedartTemplate t = GedartTemplate.create(files[b]);
-				if (t == null)
-					continue;
-				put(t.getName(),t);
-			}
-		}
-	}
-	  /**
-	   * Convert collection of templates into array
-	   */
-	  public GedartTemplate[] toArray() {
-	    return (values().toArray(new GedartTemplate[0]));
-	  }
-	  public GedartTemplate[] toArray(Object context) {
-		  ArrayList<GedartTemplate> result = new ArrayList<GedartTemplate>(5);
-		  String ctx = context.getClass().getSimpleName();
-		  for (GedartTemplate ga: this.values()) {
-			  if (ga.getDescription(ctx) == null) continue;
-			  GedartTemplate _ga;
-			  try {
-				  _ga = ga.clone();
-				  _ga.setDescription(ga.getDescription(ctx));
-				  result.add(_ga);
-			  } catch (CloneNotSupportedException e) {
-			  }
-		  }
-	    return (result.toArray(new GedartTemplate[0]));
-	  }
+
+    }
+
+    public GedartTemplates(File dir) {
+        if (dir.isDirectory()) {
+            // loop over Templates
+            File[] files = dir.listFiles();
+            for (int b = 0; b < files.length; b++) {
+                GedartTemplate t = GedartTemplate.create(files[b]);
+                if (t == null) {
+                    continue;
+                }
+                put(t.getName(), t);
+            }
+        }
+    }
+
+    /**
+     * Convert collection of templates into array
+     */
+    public GedartTemplate[] toArray() {
+        return (values().toArray(new GedartTemplate[0]));
+    }
+
+    public GedartTemplate[] toArray(Object context) {
+        ArrayList<GedartTemplate> result = new ArrayList<GedartTemplate>(5);
+        String ctx = context.getClass().getSimpleName();
+        for (GedartTemplate ga : this.values()) {
+            if (ga.getDescription(ctx) == null) {
+                continue;
+            }
+            GedartTemplate _ga;
+            try {
+                _ga = ga.clone();
+                _ga.setDescription(ga.getDescription(ctx));
+                result.add(_ga);
+            } catch (CloneNotSupportedException e) {
+            }
+        }
+        return (result.toArray(new GedartTemplate[0]));
+    }
+
+
 }
