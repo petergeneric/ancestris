a43176dc3a43c7075f4e2100e98a454dee3d39fc
frederic <frederic@ancestris.org>
2016-12-10 21:26:07 +0000

    New screenshot functionality : possibility to screenshot portion of view, and save to a file. Also limits capture to memory size.

--- AncestrisCore/core/src/genj/view/ScreenshotAction.java
+++ AncestrisCore/core/src/genj/view/ScreenshotAction.java
@@ -3,135 +3,245 @@
  *
  * Copyright (C) 1997 - 2010 Nils Meier <nils@meiers.net>
  *
- * This piece of code is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This piece of code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
  *
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package genj.view;
 
 import ancestris.core.actions.AbstractAncestrisAction;
 import ancestris.util.swing.DialogManager;
 import genj.renderer.RenderSelectionHintKey;
+import genj.util.Registry;
 import genj.util.Resources;
 import genj.util.swing.ImageIcon;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
-import javax.swing.JRadioButton;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.openide.util.Cancellable;
+import org.openide.util.Exceptions;
+import org.openide.windows.WindowManager;
 
 /**
  * An action for copying to an image
  */
 public class ScreenshotAction extends AbstractAncestrisAction {
-  
-  private final static ImageIcon IMG = new ImageIcon(ScreenshotAction.class, "images/Camera.png");
-  private final static Resources RES = Resources.get(ScreenshotAction.class);
-  
-  private JComponent component;
 
-  public ScreenshotAction(JComponent component) {
-    setImage(IMG);
-    setTip(RES.getString("screenshot"));
-    this.component = component;
-  }
+    private final static ImageIcon IMG = new ImageIcon(ScreenshotAction.class, "images/Camera.png");
+    private final static Resources RES = Resources.get(ScreenshotAction.class);
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    
-    Rectangle rVisible = component.getVisibleRect();
-    Rectangle r = new Rectangle(new Point(),component.getSize());
-    
-    if (r.width>rVisible.width || r.height>rVisible.height) {
-    
-      JRadioButton viewport = new JRadioButton(RES.getString("screenshot.asviewed"), true);
-      JRadioButton all = new JRadioButton(RES.getString("screenshot.all", false));
-      ButtonGroup group = new ButtonGroup();
-      group.add(viewport);
-      group.add(all);
-      Box choices = new Box(BoxLayout.Y_AXIS);
-      choices.add(viewport);
-      choices.add(all);
-      
-      if (DialogManager.OK_OPTION != DialogManager.create(getTip(), choices)
-              .setOptionType(DialogManager.OK_CANCEL_OPTION)
-              .setDialogId("screenshot")
-              .show())
-        return;
+    private JComponent component;
 
-      if (viewport.isSelected())
-        r = rVisible;
-    }
-    
-    // Create image & copy
-    try {
-      BufferedImage image = new BufferedImage(r.width, r.height, BufferedImage.TYPE_INT_RGB);
-      Graphics2D g = image.createGraphics();
-      g.setRenderingHint(RenderSelectionHintKey.KEY, false);
-      g.setClip(0, 0, r.width, r.height);
-      g.translate(-r.x, -r.y);
-      component.paint(g);
-      g.dispose();
-      ImageTransferable imageSelection = new ImageTransferable(image);
-      Toolkit toolkit = Toolkit.getDefaultToolkit();
-      toolkit.getSystemClipboard().setContents(imageSelection, null);
-    } catch (OutOfMemoryError oom) {
-      long max = Runtime.getRuntime().maxMemory()/1024/1000;
-      String msg = RES.getString("screenshot.oom", r.width*r.height*4/1024/1000, max, String.valueOf(max));
-      Logger.getLogger("ancestris.view").log(Level.WARNING, msg, oom);
-      DialogManager.createError(getTip(), msg).show();
-    }
-    
-    
-    // done
-  }
-  
-  /**
-   * A Transferable able to transfer an AWT Image. Similar to the JDK
-   * StringSelection class.
-   */
-  private static class ImageTransferable implements Transferable {
-
-    private Image image;
-
-    private ImageTransferable(Image image) {
-      this.image = image;
+    public ScreenshotAction(JComponent component) {
+        setImage(IMG);
+        setTip(RES.getString("screenshot"));
+        this.component = component;
     }
 
-    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
-      if (flavor.equals(DataFlavor.imageFlavor) == false)
-        throw new UnsupportedFlavorException(flavor);
-      return image;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        ScreenshotPanel panel = new ScreenshotPanel(component);
+
+        if (DialogManager.OK_OPTION != DialogManager.create(getTip(), panel)
+                .setOptionType(DialogManager.OK_CANCEL_OPTION)
+                .setDialogId("screenshot")
+                .show()) {
+            return;
+        }
+
+        // Create image & copy
+        final ImageCreator imageCreator = new ImageCreator(panel);
+        final ProgressHandle ph = ProgressHandleFactory.createHandle(RES.getString("progressTask"), new Cancellable() {
+            @Override
+            public boolean cancel() {
+                return imageCreator.finish();
+            }
+            
+        });
+        imageCreator.setProgress(ph);
+        ph.start();
+        (new Thread(imageCreator)).start();
+
+        // done
     }
 
-    public boolean isDataFlavorSupported(DataFlavor flavor) {
-      return flavor.equals(DataFlavor.imageFlavor);
+    public void clearClipboard() {
+        try {
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new Transferable() {
+                @Override
+                public DataFlavor[] getTransferDataFlavors() {
+                    return new DataFlavor[0];
+                }
+
+                @Override
+                public boolean isDataFlavorSupported(DataFlavor flavor) {
+                    return false;
+                }
+
+                @Override
+                public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
+                    throw new UnsupportedFlavorException(flavor);
+                }
+            }, null);
+        } catch (IllegalStateException e) {
+        }
     }
 
-    public DataFlavor[] getTransferDataFlavors() {
-      return new DataFlavor[] { DataFlavor.imageFlavor };
+    /**
+     * A Transferable able to transfer an AWT Image. Similar to the JDK
+     * StringSelection class.
+     */
+    private static class ImageTransferable implements Transferable {
+
+        private Image image;
+
+        private ImageTransferable(Image image) {
+            this.image = image;
+        }
+
+        public void clear() {
+            image = null;
+        }
+
+        @Override
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
+            if (flavor.equals(DataFlavor.imageFlavor) == false) {
+                throw new UnsupportedFlavorException(flavor);
+            }
+            return image;
+        }
+
+        @Override
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return flavor.equals(DataFlavor.imageFlavor);
+        }
+
+        @Override
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[]{DataFlavor.imageFlavor};
+        }
     }
-  }
+
+    
+    public class ImageCreator implements Runnable {
+        
+        private int capX = 0;
+        private int capY = 0;
+        private int capW = 0;
+        private int capH = 0;
+        private ScreenshotPanel panel = null;
+        private ProgressHandle ph = null;
+        
+        public ImageCreator(ScreenshotPanel scp) {
+            this.panel = scp;
+            this.capX = panel.getCaptureX();
+            this.capY = panel.getCaptureY();
+            this.capW = panel.getCaptureW();
+            this.capH = panel.getCaptureH();
+        }
+        
+        public void setProgress(ProgressHandle ph) {
+            this.ph = ph;
+        }
+
+        @Override
+        public void run() {
+        
+            System.gc();
+            ImageTransferable imageSelection = null;
+            BufferedImage image = null;
+            String msg = "";
+            try {
+                String tempDir = Registry.get(genj.gedcom.GedcomOptions.class).get("reportDir", System.getProperty("java.io.tmpdir"));
+                image = BigBufferedImage.create(new File(tempDir), capW, capH, BufferedImage.TYPE_INT_RGB);
+                Graphics2D g = image.createGraphics();
+                g.setRenderingHint(RenderSelectionHintKey.KEY, false);
+                g.setClip(0, 0, capW, capH);
+                g.translate(-capX, -capY);
+                component.paint(g);
+                g.dispose();
+
+                if (panel.isClipboard()) {
+                    clearClipboard();
+                    imageSelection = new ImageTransferable(image);
+                    Toolkit toolkit = Toolkit.getDefaultToolkit();
+                    toolkit.getSystemClipboard().setContents(imageSelection, null);
+                    msg = RES.getString("screenshot.copiedToClipboard");
+                } else if (panel.isFile()) {
+                    File outputfile = new File(panel.getFile());
+                    ImageIO.write(image, "png", outputfile); // png allows bigger files
+                    msg = RES.getString("screenshot.copiedToFile", outputfile.getAbsolutePath());
+                } else {
+                    msg = RES.getString("screenshot.nothingToDo");
+                    return;
+                }
+                finish();
+                DialogManager.create(getTip(), msg).show();
+
+            } catch (OutOfMemoryError oom) { // we should never get there because calcSize is lower than maxSize, but just in case, I leave this old code below.
+                finish();
+                long maxSize = Runtime.getRuntime().freeMemory() / 1024 / 1024;
+                long calcSize = capW * capH * 8 / 1024 / 1024;  // 8 is depth of image (?)
+                msg = RES.getString("screenshot.oom", calcSize, maxSize, String.valueOf(maxSize));
+                Logger.getLogger("ancestris.view").log(Level.WARNING, msg, oom);
+                DialogManager.createError(getTip(), msg).show();
+
+            } catch (IOException ex) {
+                finish();
+                Exceptions.printStackTrace(ex);
+                
+            }
+
+            // Clear all used memory
+            if (panel.isClipboard()) {
+                clearClipboard();
+            }
+            if (imageSelection != null) {
+                imageSelection.clear();
+            }
+            image = null;
+            imageSelection = null;
+            WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
+                @Override
+                public void run() {
+                    System.gc();
+                }
+
+            });
+            
+            // done
+
+        } // run
+
+        private boolean finish() {
+            ph.finish();
+            return true;
+        }
+
+    }
+    
+    
 }
