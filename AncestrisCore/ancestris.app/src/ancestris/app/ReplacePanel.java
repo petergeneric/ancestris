513771912e65720f80f192812d5538ab1311cb67
frederic <frederic@ancestris.org>
2017-03-26 23:01:19 +0000

    Reengineer Search selection features overall, to apply between Advanced Search, Search/Replace and Geo module

--- AncestrisCore/ancestris.app/src/ancestris/app/ReplacePanel.java
+++ AncestrisCore/ancestris.app/src/ancestris/app/ReplacePanel.java
@@ -11,7 +11,7 @@
  */
 package ancestris.app;
 
-import ancestris.api.search.SearchResults;
+import ancestris.api.search.SearchCommunicator;
 import ancestris.view.SelectionDispatcher;
 import genj.gedcom.Context;
 import genj.gedcom.Entity;
@@ -54,7 +54,6 @@ import javax.swing.text.JTextComponent;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
-import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 
 /**
@@ -912,22 +911,9 @@ public class ReplacePanel extends javax.swing.JPanel {
      * Get result properties of all Search TopComponent found
      * @return
      */
-    private Set<Property> getSelection() {
-        Set<Property> ret = new HashSet<Property>();
-        
-        for (SearchResults selectionResults : Lookup.getDefault().lookupAll(SearchResults.class)) {
-            for (SearchResults instance : selectionResults.getInstances()) {
-                Gedcom searchGedcom = instance.getGedcom();
-                if (searchGedcom != null && searchGedcom.getName().equals(gedcom.getName())) {
-                    ret.addAll(instance.getResultProperties());
-                }
-            }
-            break;
-        }
-        
-        return ret;
+    private List<Property> getSelection() {
+        return SearchCommunicator.getResults(gedcom);
     }
-    
     
     
 
@@ -1057,6 +1043,7 @@ public class ReplacePanel extends javax.swing.JPanel {
                             property.getPath().getName() + ":" + property.getPath().toString()));
         }
     }
+
     
     
     
@@ -1080,7 +1067,6 @@ public class ReplacePanel extends javax.swing.JPanel {
 
     
     
-
     private class FilterPopupListener implements PopupMenuListener {
 
         public FilterPopupListener() {
