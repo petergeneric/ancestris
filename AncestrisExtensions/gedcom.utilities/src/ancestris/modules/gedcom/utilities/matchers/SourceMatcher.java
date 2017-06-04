fc5c06c6347e498deb23984027c8a8bb99dea113
frederic <frederic@ancestris.org>
2017-06-04 20:46:30 +0000

    Improve Check for Duplicates module. Fix.

--- AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/matchers/SourceMatcher.java
+++ AncestrisExtensions/gedcom.utilities/src/ancestris/modules/gedcom/utilities/matchers/SourceMatcher.java
@@ -1,5 +1,6 @@
 package ancestris.modules.gedcom.utilities.matchers;
 
+import genj.gedcom.Property;
 import genj.gedcom.Source;
 
 /**
@@ -15,9 +16,27 @@ public class SourceMatcher extends EntityMatcher<Source, SourceMatcherOptions> {
 
     @Override
     public int compare(Source left, Source right) {
-        if (left.getTitle().equals(right.getTitle())) {
-            if (left.getText().equals(right.getText())) {
-                return 100;
+        if (!left.getTitle().isEmpty() && left.getTitle().equals(right.getTitle())) {
+            Property abbrLeft = left.getProperty("ABBR");
+            Property abbrRight = right.getProperty("ABBR");
+            if (abbrLeft != null && abbrRight != null 
+                &&  !abbrLeft.getDisplayValue().isEmpty() 
+                &&  !abbrRight.getDisplayValue().isEmpty() 
+                &&  abbrRight.getDisplayValue().equals(abbrLeft.getDisplayValue())) {
+                Property authLeft = left.getProperty("AUTH");
+                Property authRight = right.getProperty("AUTH");
+                if (authLeft != null && authRight != null
+                        && !authLeft.getDisplayValue().isEmpty()
+                        && !authRight.getDisplayValue().isEmpty()
+                        && authRight.getDisplayValue().equals(authLeft.getDisplayValue())) {
+                    if (!left.getText().isEmpty() && left.getText().equals(right.getText())) {
+                        return 100;
+                    } else {
+                        return 90;
+                    }
+                } else {
+                    return 80;
+                }
             } else {
                 return 50;
             }
