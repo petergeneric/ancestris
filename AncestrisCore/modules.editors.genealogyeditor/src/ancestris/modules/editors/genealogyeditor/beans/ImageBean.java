c83e5542b9d87c320d0d70f7a6f5cb083615912c
frederic <frederic@ancestris.org>
2017-10-23 21:50:51 +0000

    Fix photo.

--- AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/beans/ImageBean.java
+++ AncestrisCore/modules.editors.genealogyeditor/src/ancestris/modules/editors/genealogyeditor/beans/ImageBean.java
@@ -90,8 +90,11 @@ public class ImageBean extends javax.swing.JPanel {
             try {
                 imageInputStream = new FileInputStream(file);
                 loadImage = ImageIO.read(imageInputStream);
-                // FIXME: We should display some icon if file cannot be read as image
-                if (loadImage != null){
+                if (loadImage == null) {
+                    imageInputStream = ImageBean.class.getResourceAsStream("/ancestris/modules/editors/genealogyeditor/resources/invalid_photo.png");
+                    loadImage = ImageIO.read(imageInputStream);
+                }
+                if (loadImage != null) {
                     if (getWidth() > 0 && getWidth() < getHeight()) {
                         scaledImage = loadImage.getScaledInstance(getWidth(), -1, Image.SCALE_DEFAULT);
                     } else if (getHeight() > 0) {
