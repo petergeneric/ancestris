828d048bf33a70149bc489e63a74aebc221fabab
frederic <frederic@ancestris.org>
2017-10-26 19:55:51 +0000

    Add Geneatique import

--- AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportGeneatique.java
+++ AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportGeneatique.java
@@ -15,10 +15,16 @@ package ancestris.modules.imports.gedcom;
 import ancestris.api.imports.Import;
 import static ancestris.modules.imports.gedcom.Bundle.importgeneatique_name;
 import static ancestris.modules.imports.gedcom.Bundle.importgeneatique_note;
+import static ancestris.util.swing.FileChooserBuilder.getExtension;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.NbBundle;
 import genj.gedcom.Gedcom;
+import genj.gedcom.Grammar;
+import genj.gedcom.Property;
+import genj.gedcom.PropertyFile;
+import genj.gedcom.TagPath;
 import java.io.IOException;
+import java.util.List;
 
 /**
  *
@@ -77,6 +83,19 @@ public class ImportGeneatique extends Import {
      */
     @Override
     protected boolean process() throws IOException {
+
+        String tag = input.getTag();
+        TagPath path = input.getPath();
+        
+        // Replace ASSO:TYPE by ASSO:RELA
+        if (path.toString().endsWith("ASSO:TYPE")) { 
+            String result = output.writeLine(input.getLevel(), "RELA", input.getValue());
+            console.println(NbBundle.getMessage(ImportHeredis.class, "Import.fixAssoRela", input.getLine() + " ==> " + result));
+            return true;
+        }
+        
+        
+        
         if (super.process()) {
             return true;
         }
@@ -108,6 +127,7 @@ public class ImportGeneatique extends Import {
     public boolean fixGedcom(Gedcom gedcom) {
         boolean ret = super.fixGedcom(gedcom);
         ret |= fixOther(gedcom);
+        ret |= super.convertAssociations(gedcom);
         return ret;
     }
 
@@ -138,6 +158,8 @@ public class ImportGeneatique extends Import {
      * @return 
      */
     private boolean processOther() {
+
+        
         return false;
     }
 
@@ -147,7 +169,32 @@ public class ImportGeneatique extends Import {
      */
     public boolean fixOther(Gedcom gedcom) {
         boolean hasErrors = false;
-
+        Property[] props = null;
+        Property prop = null;
+        Property host = null;
+        
+        
+        
+        // Add OBJE:FORM next to OBJE:FILE
+        if (gedcom.getGrammar().equals(Grammar.V55)) {
+            List<Property> fileList = (List<Property>) gedcom.getPropertiesByClass(PropertyFile.class);
+            for (Property file : fileList) {
+                host = file.getParent();
+                prop = host.getProperty("FORM");
+                if (prop == null) {
+                    String ext = getExtension(file.getValue());
+                    if (ext == null) {
+                        ext = "none";
+                    }
+                    host.addProperty("FORM", ext);
+                    console.println(NbBundle.getMessage(ImportGramps.class, "Import.fixMediaForm", file.toString()));
+                    hasErrors = true;
+                }
+            }
+        }
+        
+        
+        
         return hasErrors;
     }
 
