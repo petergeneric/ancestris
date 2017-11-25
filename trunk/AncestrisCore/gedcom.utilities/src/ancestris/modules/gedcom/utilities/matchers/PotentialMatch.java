ac805574da96a529b8ee9d6c51107f8cd4481cea
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2017-11-25 21:39:28 +0000

    Improve duplicates and add swap button

--- trunk/AncestrisCore/gedcom.utilities/src/ancestris/modules/gedcom/utilities/matchers/PotentialMatch.java
+++ trunk/AncestrisCore/gedcom.utilities/src/ancestris/modules/gedcom/utilities/matchers/PotentialMatch.java
@@ -3,34 +3,40 @@
  * Distributed under the terms of the GPL version 2.
  * See LICENSE.TXT for details.
  */
-
 package ancestris.modules.gedcom.utilities.matchers;
 
-public class PotentialMatch <T> {
-	private T left;
-	private T right;
-	private int certainty;
-	
-	public PotentialMatch(T left, T right, int certainty) {
-		this.left = left;
-		this.right = right;
-		this.certainty = certainty;
-	}
+public class PotentialMatch<T> {
 
-	public int getCertainty() {
-		return certainty;
-	}
+    private T left;
+    private T right;
+    private int certainty;
 
-	public T getLeft() {
-		return left;
-	}
+    public PotentialMatch(T left, T right, int certainty) {
+        this.left = left;
+        this.right = right;
+        this.certainty = certainty;
+    }
 
-	public T getRight() {
-		return right;
-	}
-	
-	@Override
-	public String toString() {
-		return certainty + "\t" + left + "\t" + right;
-	}
+    public int getCertainty() {
+        return certainty;
+    }
+
+    public T getLeft() {
+        return left;
+    }
+
+    public T getRight() {
+        return right;
+    }
+
+    public void swap() {
+        T temp = left;
+        left = right;
+        right = temp;
+    }
+
+    @Override
+    public String toString() {
+        return certainty + "\t" + left + "\t" + right;
+    }
 }
