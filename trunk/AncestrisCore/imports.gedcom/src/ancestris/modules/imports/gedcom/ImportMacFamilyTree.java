765bd5deb71f0da59b4f733046b3c99c22cae43d
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2018-06-24 20:18:12 +0000

    Fix import for MyHeritage.

--- trunk/AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportMacFamilyTree.java
+++ trunk/AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportMacFamilyTree.java
@@ -209,6 +209,7 @@ public class ImportMacFamilyTree extends Import {
                 reduceEvents(entity, "DEAT");
                 reduceEvents(entity, "BURI");
                 reduceEvents(entity, "CREM");
+                reduceEvents(entity, "CONF");
             }
 
             
--- trunk/AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportMyHeritage.java
+++ trunk/AncestrisCore/imports.gedcom/src/ancestris/modules/imports/gedcom/ImportMyHeritage.java
@@ -16,12 +16,15 @@ import ancestris.api.imports.Import;
 import static ancestris.modules.imports.gedcom.Bundle.importmyheritage_name;
 import static ancestris.modules.imports.gedcom.Bundle.importmyheritage_note;
 import static ancestris.util.swing.FileChooserBuilder.getExtension;
+import genj.gedcom.Entity;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.NbBundle;
 import genj.gedcom.Gedcom;
 import genj.gedcom.Grammar;
 import genj.gedcom.Property;
 import genj.gedcom.PropertyFile;
+import genj.gedcom.PropertySource;
+import genj.gedcom.TagPath;
 import java.io.IOException;
 import java.util.List;
 
@@ -143,7 +146,19 @@ public class ImportMyHeritage extends Import {
      * Specific code depending from import type
      * @return 
      */
-    private boolean processOther() {
+    private boolean processOther() throws IOException {
+        String tag = input.getTag();
+        TagPath path = input.getPath();
+        if ("RIN".equals(tag) && (path.length() != 2)) {  // invalid tag here
+            String result = output.writeLine(input.getLevel(), "_" + tag, input.getValue());
+            console.println(NbBundle.getMessage(ImportHeredis.class, "Import.fixTagNotAllowed", input.getLine() + " ==> " + result));
+            return true;
+        }
+        if ("FAM:NCHI:NOTE".equalsIgnoreCase(path.toString())) {  // invalid tag here
+            String result = output.writeLine(input.getLevel(), "_" + tag, input.getValue());
+            console.println(NbBundle.getMessage(ImportHeredis.class, "Import.fixTagNotAllowed", input.getLine() + " ==> " + result));
+            return true;
+        }
         return false;
     }
 
@@ -188,6 +203,40 @@ public class ImportMyHeritage extends Import {
                 }
             }
         }
+        
+        // Only one PAGE tag in a SOUR link
+        List<PropertySource> sourceLinks = (List<PropertySource>) gedcom.getPropertiesByClass(PropertySource.class);
+        for (PropertySource ps : sourceLinks) {
+            Property[] props = ps.getProperties("PAGE");
+            if (props.length > 1) {
+                int cnt = 0;
+                for (Property page : props) {
+                    cnt++;
+                    if (cnt > 1) {
+                        ps.addProperty("_PAGE", page.getValue());
+                        ps.delProperty(page);
+                    }
+                }
+                console.println(NbBundle.getMessage(ImportGramps.class, "Import.fixSource") + " : " + ps.toString());
+                hasErrors = true;
+            }
+        }
+            
+        for (Entity entity : gedcom.getEntities()) {
+            
+            // For individual with several BIRTs, BAPM, DEAT, etc. change the other ones into EVEN
+            if (entity.getTag().equals("INDI")) {
+                reduceEvents(entity, "BIRT");
+                reduceEvents(entity, "BAPM");
+                reduceEvents(entity, "DEAT");
+                reduceEvents(entity, "BURI");
+                reduceEvents(entity, "CREM");
+                reduceEvents(entity, "CONF");
+            }
+
+            
+        }
+        
         
         
         return hasErrors;
