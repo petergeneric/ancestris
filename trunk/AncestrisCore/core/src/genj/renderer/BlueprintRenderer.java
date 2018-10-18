9aa117de66f2ece4243a1bd2b4348dcbbe408cdf
zurga <zurga@246880a2-2d26-488f-8150-ff0ebaa74848>
2018-10-18 22:07:31 +0000

    Add "ifvalue" tag for the display tree. Included in default display.

--- trunk/AncestrisCore/core/src/genj/renderer/BlueprintRenderer.java
+++ trunk/AncestrisCore/core/src/genj/renderer/BlueprintRenderer.java
@@ -354,10 +354,16 @@ public class BlueprintRenderer {
 
       // check if the element is "prop"
       if ("prop".equals(name)) {
-        PropertyView result = new PropertyView(elem);
+        final PropertyView result = new PropertyView(elem);
         volatileViews.add(result);
         return result;
-        
+      }
+      
+      // check the IFValue tag
+      if ("ifvalue".equals(name)) {
+          final IfValueView result = new IfValueView(elem);
+          volatileViews.add(result);
+          return result;
       }
       
       // maybe its "name" or "i18n"
@@ -367,7 +373,7 @@ public class BlueprintRenderer {
       
       // a media item?
       if ("media".equals(name)) {
-        MediaView result = new MediaView(elem);
+        final MediaView result = new MediaView(elem);
         volatileViews.add(result);
         return result;
       }
@@ -829,21 +835,7 @@ public class BlueprintRenderer {
       
       // done
     }
-    
-    /**
-     * Gets an int value from attributes
-     */
-    private int getAttribute(String key, int min, int max, int def) {
-      // grab a value and try to parse
-      String val = attributes.get(key);
-      if (val!=null) try {
-        return Math.max(min, Math.min(max, Integer.parseInt(val)));
-      } catch (NumberFormatException e) {
-      }
-      // not found
-      return def;
-    }
-    
+ 
     /**
      * Get Property
      */
@@ -1095,5 +1087,311 @@ public class BlueprintRenderer {
     }
     
   } //PropertyView
+  
+  private class IfValueView extends MyView {
+        
+    /** configuration */
+    private Map<String,String> attributes;
+    private TagPath path = null;
+     private TagPath elsePath = null;
+    
+    /** cached information */
+    private Property cachedProperty = null;
+    private Dimension2D cachedSize = null;
+    
+    /** 
+     * Constructor
+     */
+    IfValueView(Element elem) {
+      super(elem);
+
+      // prepare attributes
+      attributes = new HashMap<String, String>();
+      
+      for (Enumeration<?> as = elem.getAttributes().getAttributeNames(); as.hasMoreElements(); ) {
+        Object key = as.nextElement();
+        if (key instanceof String)
+          attributes.put((String)key, (String)elem.getAttributes().getAttribute(key));
+      }
+      
+      // grab path
+      Object p = elem.getAttributes().getAttribute("path");
+      if (p!=null) try {
+        path = new TagPath((String)p);
+      } catch (IllegalArgumentException e) {
+        if (LOG.isLoggable(Level.FINER))
+          LOG.log(Level.FINER, "got wrong path "+p);
+      }
+      
+       // grab path
+      Object ep = elem.getAttributes().getAttribute("default");
+      if (ep!=null) try {
+        elsePath = new TagPath((String)ep);
+      } catch (IllegalArgumentException e) {
+        if (LOG.isLoggable(Level.FINER))
+          LOG.log(Level.FINER, "got wrong path "+ep);
+      }
+      
+      // done
+    }
+    
+    /**
+     * Get Property
+     */
+    private Property getProperty(TagPath myPath) {
+      // still looking for property?
+      if (cachedProperty!=null)
+        return cachedProperty;
+      if (entity==null||myPath==null)
+        return null;
+      cachedProperty = BlueprintRenderer.this.getProperty(entity, myPath);
+      return cachedProperty;
+    }
+    
+    /**
+     * @see javax.swing.text.View#paint(Graphics, Shape)
+     */
+    public void paint(Graphics g, Shape allocation) {
+      
+      Property prop = getProperty(path);
+      if (prop==null) {
+          prop = getProperty(elsePath);
+      } 
+      if (prop == null) {
+        return;
+      }
+      
+      Graphics2D graphics = (Graphics2D)g;
+      
+      // setup painting attributes and bounds
+      Rectangle r = (allocation instanceof Rectangle) ? (Rectangle)allocation : allocation.getBounds();
+      Color fg = super.getForeground();
+      
+      // debug?
+      if (isDebug) {
+        Stroke stroke = graphics.getStroke();
+        graphics.setStroke(DEBUG_STROKE);
+        g.setColor(new Color(fg.getRed(), fg.getGreen(), fg.getBlue(), 32));
+        graphics.draw(r);
+        graphics.setStroke(stroke);
+      }
+      
+      // clip and render
+      Shape old = graphics.getClip();
+      graphics.clip(r);
+      g.setColor(fg);
+      g.setFont(super.getFont());
+      render(prop, graphics, r);
+      g.setClip(old);
+      
+      // done
+    }
+    
+    private void render(Property prop, Graphics2D g, Rectangle r) {
+
+          // entities (specifically NOTE) can be a multi-line - let fall through
+          if (!(prop instanceof Entity) && prop instanceof MultiLineProperty) {
+              render((MultiLineProperty) prop, g, r);
+              return;
+          }
+          if (prop instanceof PropertyFile || prop instanceof PropertyBlob) {
+              MediaRenderer.render(g, r, prop);
+              return;
+          }
+          // image?
+          if (HINT_VALUE_TRUE.equals(attributes.get(HINT_KEY_IMG))) {
+              //XXX: prop or prop.getParent may be null. We must handle this before prop is rendered
+              if (prop != null && prop.getParent() != null) {
+                  render(prop instanceof PropertyDate ? prop.getParent().getImage(false) : prop.getImage(false), g, r);
+              }
+          }
+          // text?
+          if (!HINT_VALUE_FALSE.equals(attributes.get(HINT_KEY_TXT))) {
+              render(getText(prop), g, r);
+          }
+      }
+
+    private void render(MultiLineProperty mle, Graphics2D g, Rectangle r) {
+      // get lines
+      MultiLineProperty.Iterator line = mle.getLineIterator();
+      
+      // paint
+      Graphics2D graphics = g;
+      Font font = g.getFont();
+      FontRenderContext context = graphics.getFontRenderContext();
+
+      float 
+        x = (float)r.getX(),
+        y = (float)r.getY();
+      
+      do {
+
+        // analyze line
+        String txt = line.getValue();
+        LineMetrics lm = font.getLineMetrics(txt, context);
+        y += lm.getHeight();
+        
+        // draw line
+        graphics.drawString(txt, x, y - lm.getDescent());
+        
+        // .. break if line doesn't fit anymore
+        if (y>r.getMaxY()) 
+          break;
+        
+      } while (line.next());
+      
+      // done
+    }
+    
+    private void render(ImageIcon img, Graphics2D g, Rectangle bounds) {
+      
+      // no space?
+      if (bounds.getHeight()==0||bounds.getWidth()==0)
+        return;
+      
+      // draw image with maximum height of a character
+      int 
+        w = img.getIconWidth(),
+        max = g.getFontMetrics().getHeight();
+      
+      AffineTransform at = AffineTransform.getTranslateInstance(bounds.getX(), bounds.getY());
+      if (max<img.getIconHeight()) {
+        float scale = max/(float)img.getIconHeight();
+        at.scale(scale, scale);
+        w = (int)Math.ceil(w*scale);
+      }
+      g.drawImage(img.getImage(), at, null);
+      
+      // patch bounds for skip
+      bounds.x += w+IMAGE_GAP;
+      bounds.width -= w+IMAGE_GAP;
+    }
+        
+    
+    private String getText(Property prop) {
+      if (prop instanceof Entity)
+        return getText((Entity)prop);
+      if (prop.isPrivate())
+        return STARS;
+      if (prop instanceof PropertyPlace)
+        return getText((PropertyPlace)prop);
+      if (prop instanceof PropertySex)
+        return getText((PropertySex)prop);
+
+      if (attributes.get("format") != null)
+          return prop.format(attributes.get("format"));
+      return prop.getDisplayValue();
+    }
+    
+    private String getText(Entity entity) {
+      if (attributes.get("format") != null)
+          return entity.format(attributes.get("format"));
+      return entity.getId();
+    }
+    
+    private String getText(PropertySex sex) {
+      
+      if (!attributes.containsKey(HINT_KEY_TXT))
+        attributes.put(HINT_KEY_TXT, HINT_VALUE_FALSE);
+      if (!attributes.containsKey(HINT_KEY_IMG))
+        attributes.put(HINT_KEY_IMG, HINT_VALUE_TRUE);
+        
+      String result = sex.getDisplayValue();
+      if (result.length()>0 && HINT_VALUE_TRUE.equals(attributes.get(HINT_KEY_SHORT)))
+        result = result.substring(0,1);
+      return result;
+    }
+    
+    private String getText(PropertyPlace place) {
+      return Utilities.html2text(place.format(attributes.get("format")));
+    }
+    
+    /**
+     * @see genj.renderer.EntityRenderer.MyView#getPreferredSpan()
+     */
+    protected Dimension2D getPreferredSpan() {
+      // cached?
+      if (cachedSize!=null)
+        return cachedSize;
+      // check property
+      cachedSize = getSize();
+      return cachedSize;
+    }
+    
+    private Dimension2D getSize() {
+      Property prop = getProperty(path);
+      if (prop==null) {
+          prop = getProperty(elsePath);
+      }
+      if (prop==null) {
+        return new Dimension();
+      }
+      if (!(prop instanceof Entity)&&prop instanceof MultiLineProperty)
+        return getSize((MultiLineProperty)prop);
+      if (prop instanceof PropertyFile || prop instanceof PropertyBlob)
+        return MediaRenderer.getSize(prop, getGraphics());
+      return getSize(prop);
+    }
+    
+    private Dimension2D getSize(Property prop) {
+      
+      String txt = getText(prop);
+      
+      double 
+        w = 0,
+        h = 0;
+      
+      // calculate text size (the default size we use)
+      graphics.setFont(super.getFont());
+      FontMetrics fm = graphics.getFontMetrics();
+      if (!HINT_VALUE_FALSE.equals(attributes.get(HINT_KEY_TXT))&&txt.length()>0) {
+        w += fm.stringWidth(txt);
+        h = Math.max(h, fm.getAscent() + fm.getDescent());
+      }
+      // add image size
+      if (HINT_VALUE_TRUE.equals(attributes.get(HINT_KEY_IMG))) {
+        ImageIcon img = prop.getImage(false);
+        float max = fm.getHeight();
+        float scale = 1F;
+        if (max<img.getIconHeight()) 
+          scale = max/img.getIconHeight();
+        w += (int)Math.ceil(img.getIconWidth()*scale) + IMAGE_GAP;
+        h = Math.max(h, fm.getHeight());
+      }
+      
+      // done
+      return new Dimension2d(w,h);
+    }
+    
+    private Dimension2D getSize(MultiLineProperty mle) {
+      
+      // count 'em
+      graphics.setFont(super.getFont());
+      FontMetrics fm = graphics.getFontMetrics();
+      int lines = 0;
+      double width = 0;
+      double height = 0;
+      MultiLineProperty.Iterator line = mle.getLineIterator();
+      do {
+        lines++;
+        width = Math.max(width, fm.stringWidth(line.getValue()));
+        height += fm.getHeight();
+      } while (line.next());
+      
+      // done
+      return new Dimension2d(width, height);
+    }
+    
+    /**
+     * Invalidates this views current state
+     */
+    protected void invalidate() {
+      // invalidate cached information that's depending
+      // on the current entity's properties
+      cachedProperty = null;
+      cachedSize = null;
+      super.invalidate();
+    }
+  } // IfValueView
 
 } //EntityRenderer
