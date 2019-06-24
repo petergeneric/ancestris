40f40a6b93473dd19c874e353658b6de168af576
zurga <zurga@246880a2-2d26-488f-8150-ff0ebaa74848>
2019-06-24 22:16:15 +0000

    FIX : Enable Nimbus look and feel change Nimbus for javax.swing.plaf.nimbus.NimbusLookAndFeel seems to work either java 8 or Java 11

--- trunk/AncestrisCore/ancestris.app/src/ancestris/lnf/standard/LnFNimbus.java
+++ trunk/AncestrisCore/ancestris.app/src/ancestris/lnf/standard/LnFNimbus.java
@@ -28,7 +28,7 @@ public class LnFNimbus extends LookAndFeelProvider{
 
     @Override
     public String getName() {
-        return "Nimbus";
+        return "javax.swing.plaf.nimbus.NimbusLookAndFeel";
     }
 
     @Override
