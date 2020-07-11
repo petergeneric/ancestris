ce013939fd66ebc8e5fa4578355dd907e81c3141
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2020-07-11 13:24:40 +0000

    Rename project.

---
+++ trunk/AncestrisCore/gedcom.genids/src/ancestris/modules/gedcom/genids/GenerateIDPlugin.java
@@ -0,0 +1,25 @@
+/*
+ * Ancestris - http://www.ancestris.org
+ * 
+ * Copyright 2020 Ancestris
+ * 
+ * Author: Frédéric Lapeyre (frederic@ancestris.org).
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package ancestris.modules.gedcom.genids;
+
+import ancestris.core.pluginservice.AncestrisPlugin;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author frederic
+ */
+@ServiceProvider(service=ancestris.core.pluginservice.PluginInterface.class)
+public class GenerateIDPlugin extends AncestrisPlugin {
+
+}
