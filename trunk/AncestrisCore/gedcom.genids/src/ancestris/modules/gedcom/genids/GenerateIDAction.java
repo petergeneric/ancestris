a0630811c8945c5a70665cd7941a5ce307b485ac
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2020-08-02 16:26:20 +0000

    Optimize process performance (thread)

--- trunk/AncestrisCore/gedcom.genids/src/ancestris/modules/gedcom/genids/GenerateIDAction.java
+++ trunk/AncestrisCore/gedcom.genids/src/ancestris/modules/gedcom/genids/GenerateIDAction.java
@@ -12,23 +12,17 @@
 package ancestris.modules.gedcom.genids;
 
 import ancestris.core.actions.AbstractAncestrisContextAction;
+import ancestris.util.ProgressListener;
 import ancestris.util.swing.DialogManager;
-import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
 import genj.gedcom.GedcomException;
-import genj.gedcom.UnitOfWork;
 import java.awt.event.ActionEvent;
-import java.text.DecimalFormat;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeMap;
-import org.openide.*;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
+import spin.Spin;
 
 /**
  *
@@ -69,122 +63,26 @@ public final class GenerateIDAction extends AbstractAncestrisContextAction {
 
         if (choice == DialogManager.OK_OPTION) {
             genidPanel.savePreferences();
-            if (generateIDs(genidPanel.getSettings())) {
-                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(GenerateIDAction.class, "GenerateIDAction.Done"), NotifyDescriptor.INFORMATION_MESSAGE));
-            }
+            GenerateIDTask task = (GenerateIDTask) Spin.off(GenerateIDTaskFactory.create(gedcom, genidPanel.getSettings()));
+            ProgressListener.Dispatcher.processStarted(task);
+            commit(task);
+            ProgressListener.Dispatcher.processStopped(task);
         }
     }
-
-    private boolean generateIDs(GenIDPanel.Settings settings) {
-
-        // Case all entites IDs are to be generated
-        if (settings.isCat) {
-            int pad = 1;
-            if (settings.len > 0 && settings.len < 11) {
-                pad = settings.len;
+    
+    private void commit(final Runnable task) {
+        try {
+            if (gedcom.isWriteLocked()) {
+                task.run();
+            } else {
+                gedcom.doUnitOfWork((Gedcom localGedcom) -> {
+                    task.run();
+                });
             }
-            if (settings.len < 0) {
-                pad = 0;
-            }
-            if (settings.len > 11) {
-                pad = 11;
-            }
-            final DecimalFormat formatNbrs = new DecimalFormat("000000000000".substring(0, pad));
-
-            // Loop over all entity types
-            for (String tag : settings.entsTags) {
-                if (tag.isEmpty()) {
-                    continue;
-                }
-                Collection<? extends Entity> entities = gedcom.getEntities(tag);
-                final String entityIDPrefix = gedcom.getNextAvailableID(tag).substring(0, 1);
-                final Map<String, String> listID = new TreeMap<>(); // sorted mapping list
-                String key, ID;
-
-                // First loop to get list of ids and sort on value of entity
-                for (Entity entity : entities) {
-                    ID = entity.getId();
-                    key = entity.toString();
-                    listID.put(key, ID);
-                } // end loop
-
-                try {
-                    gedcom.doUnitOfWork(new UnitOfWork() {
-                        @Override
-                        public void perform(Gedcom gedcom) throws GedcomException {
-
-                            // Second loop to give temp ids in order to avoid duplicates
-                            int iCounter = 0;
-                            for (Iterator it = listID.keySet().iterator(); it.hasNext();) {
-                                String key = (String) it.next();
-                                String oldID = listID.get(key);
-                                Entity entity = gedcom.getEntity(oldID);
-                                iCounter++;
-                                String newID = entityIDPrefix + settings.prefix + "XYZAWZ" + iCounter + settings.suffix;  // Just a weird string ensuring no duplicates with existing ids
-                                try {
-                                    entity.setId(newID);
-                                    listID.put(key, newID);
-                                } catch (GedcomException e) {
-                                    Exceptions.printStackTrace(e);
-                                }
-                            } // proceed with other entity
-
-                            // Third loop to give final ids
-                            iCounter = 0;
-                            for (Iterator it = listID.keySet().iterator(); it.hasNext();) {
-                                String key = (String) it.next();
-                                String oldID = listID.get(key);
-                                Entity entity = gedcom.getEntity(oldID);
-                                iCounter++;
-                                String newID = entityIDPrefix + settings.prefix + formatNbrs.format(iCounter) + settings.suffix;
-                                try {
-                                    entity.setId(newID);
-                                } catch (GedcomException e) {
-                                    Exceptions.printStackTrace(e);
-                                }
-                            } // proceed with other entity
-
-                        }
-                    }); // end of doUnitOfWork
-                } catch (GedcomException e) {
-                    Exceptions.printStackTrace(e);
-                }
-
-            } // proceed with other entity type
-
-        // Case we only change 1 ID    
-        } else {
-            final Entity entityFrom = gedcom.getEntity(settings.IDFrom);
-            Entity entityTo = gedcom.getEntity(settings.IDTo);
-            if (settings.IDFrom.isEmpty() || settings.IDTo.isEmpty() || entityFrom == null || entityTo != null) {
-                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(GenerateIDAction.class, "GenerateIDAction.IncorrectIDs"), NotifyDescriptor.ERROR_MESSAGE));
-                return false;
-            }
-            String letterFrom = Gedcom.getEntityPrefix(entityFrom.getTag());
-            String letterTo = settings.IDTo.substring(0, 1);
-            if (!letterFrom.equals(letterTo)) {
-                if (DialogManager.OK_OPTION != DialogManager.create(NbBundle.getMessage(GenerateIDAction.class, "GenerateIDAction.TTL_ConfirmChange"), 
-                        NbBundle.getMessage(GenerateIDAction.class, "GenerateIDAction.MSG_ConfirmChange", letterFrom, letterTo, settings.IDFrom, settings.IDTo))
-                        .setMessageType(DialogManager.WARNING_MESSAGE).setOptionType(DialogManager.OK_CANCEL_OPTION).show()) {
-                    return false;
-                }
-            }
-            try {
-                gedcom.doUnitOfWork(new UnitOfWork() {
-                    @Override
-                    public void perform(Gedcom gedcom) throws GedcomException {
-                            try {
-                                entityFrom.setId(settings.IDTo);
-                            } catch (GedcomException e) {
-                                Exceptions.printStackTrace(e);
-                            }
-                    }
-                }); // end of doUnitOfWork
-            } catch (GedcomException e) {
-                Exceptions.printStackTrace(e);
-            }
+        } catch (GedcomException ge) {
+            Exceptions.printStackTrace(ge);
+        } finally {
         }
-        return true;
     }
-
+    
 }
