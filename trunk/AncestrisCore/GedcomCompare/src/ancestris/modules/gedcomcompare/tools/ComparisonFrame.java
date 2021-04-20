a813ba06a46c0abd299a0428f52c92475b848d62
zurga <zurga@246880a2-2d26-488f-8150-ff0ebaa74848>
2021-04-20 18:42:10 +0000

    REC: frederic Wed Jan 13 00:59:13 2021 Fix display with various "look and feel" skins

--- trunk/AncestrisCore/GedcomCompare/src/ancestris/modules/gedcomcompare/tools/ComparisonFrame.java
+++ trunk/AncestrisCore/GedcomCompare/src/ancestris/modules/gedcomcompare/tools/ComparisonFrame.java
@@ -143,7 +143,7 @@ public class ComparisonFrame extends DataFrame {
                         .addComponent(progressBar, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE))
                     .addComponent(lastCityLabel)
                     .addComponent(eventLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(geoButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(listButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
