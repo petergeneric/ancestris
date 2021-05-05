6a45ab19de218b351b8062d9f7720cc1c95b21dc
zurga <zurga@ancestris.org>
2021-05-05 19:35:00 +0000

    REC: frederic Tue Mar  9 19:39:08 2021 Bypass for MacOS: without this modification, the search for fonts on "MacOS Big Sur" made Ancestris crash. We will have to find a better way to exploit fonts on this system. In the mean time, I commit this bypass.

--- AncestrisCore/modules.fop/src/genj/fo/PDFFormat.java
+++ AncestrisCore/modules.fop/src/genj/fo/PDFFormat.java
@@ -3,22 +3,22 @@
  *
  * Copyright (C) 1997 - 2002 Nils Meier <nils@meiers.net>
  *
- * This piece of code is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This piece of code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
  *
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package genj.fo;
 
+import genj.util.EnvironmentChecker;
 import java.io.File;
 import java.io.OutputStream;
 import javax.xml.transform.Transformer;
@@ -32,42 +32,45 @@ import org.apache.fop.fo.FOTreeBuilder;
 import org.xml.sax.ContentHandler;
 
 /**
- * Format  for PDF - using FOP
+ * Format for PDF - using FOP
  */
 public class PDFFormat extends Format {
 
-  /**
-   * Constructor
-   */
-  public PDFFormat() {
-    super("PDF", "pdf", false);
-  }
-  
-  /**
-   * our format logic
-   */
-  protected void formatImpl(Document doc, OutputStream out) throws Throwable {
-      
-      DefaultConfigurationBuilder config = new DefaultConfigurationBuilder();
-      
-      Configuration conf = config.build(getClass().getResourceAsStream("resources/fopPdfConfiguration.xml"));
-      
-      
-      FopFactoryBuilder builder = new FopFactoryBuilder(new File(".").toURI());
-      builder.setStrictFOValidation(false);
-      builder.setConfiguration(conf);
-      final FopFactory fopF = builder.build();
+    /**
+     * Constructor
+     */
+    public PDFFormat() {
+        super("PDF", "pdf", false);
+    }
 
-    // create FOP tree builder that handles the document content and generates out
-      ContentHandler handler = new FOTreeBuilder("application/pdf", fopF.newFOUserAgent(), out);
+    /**
+     * our format logic
+     */
+    @Override
+    protected void formatImpl(Document doc, OutputStream out) throws Throwable {
 
-    // grab xsl transformer
-    Transformer transformer = TransformerFactory.newInstance().newTransformer();
-    
-    // do the transformation
-    transformer.transform(doc.getDOMSource(), new SAXResult(handler));
+        DefaultConfigurationBuilder config = new DefaultConfigurationBuilder();
 
-    // done
-  }
-  
+        String resourceFile = "resources/fopPdfConfiguration.xml";
+        Configuration conf = config.build(getClass().getResourceAsStream(resourceFile));
+
+        FopFactoryBuilder builder = new FopFactoryBuilder(new File(".").toURI());
+        builder.setStrictFOValidation(false);
+        if (!EnvironmentChecker.isMac()) {
+            builder.setConfiguration(conf);
+        }
+        final FopFactory fopF = builder.build();
+
+        // create FOP tree builder that handles the document content and generates out
+        ContentHandler handler = new FOTreeBuilder("application/pdf", fopF.newFOUserAgent(), out);
+
+        // grab xsl transformer
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+
+        // do the transformation
+        transformer.transform(doc.getDOMSource(), new SAXResult(handler));
+
+        // done
+    }
+
 }
