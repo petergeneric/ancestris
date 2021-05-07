9aeabcfc8c770f144657234edd29f058d7b3ec8c
zurga <zurga@246880a2-2d26-488f-8150-ff0ebaa74848>
2021-05-07 21:45:35 +0000

    REP: zurga Thu Apr 1 23:41:56 2021 ENH : Allow to display unknown entities in order to be able to delete them.

--- trunk/AncestrisCore/ancestris.app/src/ancestris/explorer/EntitiesChildren.java
+++ trunk/AncestrisCore/ancestris.app/src/ancestris/explorer/EntitiesChildren.java
@@ -4,7 +4,10 @@
  */
 package ancestris.explorer;
 
+import genj.gedcom.Entity;
 import genj.gedcom.Gedcom;
+import java.util.HashSet;
+import java.util.Set;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
 
@@ -27,9 +30,21 @@ public class EntitiesChildren extends Children.Keys<GedcomEntities> {
     @Override
     protected void addNotify() {
         super.addNotify();
-        GedcomEntities[] objs = new GedcomEntities[Gedcom.ENTITIES.length];
+        Set<String> tagEntities = new HashSet<>();
+        // Get tags of entities
+        for (String e : Gedcom.ENTITIES) {
+            tagEntities.add(e);
+        }
+        // add tags of unkwown entities present in GEDCOM file
+        for (Entity e : gedcom.getEntities()) {
+            tagEntities.add(e.getTag());
+        }
+        // No need to display HEADER
+        tagEntities.remove("HEAD");
+        GedcomEntities[] objs = new GedcomEntities[tagEntities.size()];
+        String[] tags = tagEntities.toArray(new String[0]);
         for (int i = 0; i < objs.length; i++) {
-            GedcomEntities cat = new GedcomEntities(gedcom, Gedcom.ENTITIES[i]);
+            GedcomEntities cat = new GedcomEntities(gedcom, tags[i]);
             objs[i] = cat;
         }
         setKeys(objs);
