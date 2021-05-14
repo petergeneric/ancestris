22309bae69d3d6f763e343c2d5dcfcf217ec66a5
zurga <zurga@246880a2-2d26-488f-8150-ff0ebaa74848>
2021-05-14 15:06:43 +0000

    FIX: Remove possibility to make association with everything and everywhere. Allow only ASSO with events from Fam or Indi.

--- trunk/AncestrisCore/modules.editors.gedcom/src/genj/edit/actions/CreateAssociation.java
+++ trunk/AncestrisCore/modules.editors.gedcom/src/genj/edit/actions/CreateAssociation.java
@@ -64,8 +64,10 @@ public class CreateAssociation extends CreateRelationship {
         Collection<? extends Property> props = lkpInfo.allInstances();
         if (props.size() == 1) {
             target = props.iterator().next();
-            if (target instanceof Entity)
+            // Available only for Event
+            if (!target.isEvent()) {
                 target = null;
+            }
         }
         super.resultChanged(ev);
     }
