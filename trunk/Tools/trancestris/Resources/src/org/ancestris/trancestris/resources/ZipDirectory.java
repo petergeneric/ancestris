b27aef560eafee6b0cf6c55a0dd5c5e7f68e337e
frederic <frederic@246880a2-2d26-488f-8150-ff0ebaa74848>
2021-06-14 10:40:25 +0000

    Transcestris : display modules in alphabetical order.

--- trunk/Tools/trancestris/Resources/src/org/ancestris/trancestris/resources/ZipDirectory.java
+++ trunk/Tools/trancestris/Resources/src/org/ancestris/trancestris/resources/ZipDirectory.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -121,7 +122,13 @@ public class ZipDirectory implements PropertyChangeListener {
     }
 
     public ArrayList<ZipDirectory> getDirs() {
-        return new ArrayList<ZipDirectory>(dirs.values());
+        List<ZipDirectory> list = new ArrayList<>(dirs.values());
+        Collections.sort(list, new Comparator<ZipDirectory>() {
+            public int compare(ZipDirectory o1, ZipDirectory o2) {
+                return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+            }
+        });
+        return new ArrayList<>(list);
     }
 
     public ResourceFile getResourceFile() {
