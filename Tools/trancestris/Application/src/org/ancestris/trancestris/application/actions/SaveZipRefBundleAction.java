7d0039a14987e67f349af2e5032002abfc87270b
frederic <frederic@ancestris.org>
2021-12-05 14:14:10 +0000

    Trancestris : add notifications before and after saving bundle and reference bundle.

--- Tools/trancestris/Application/src/org/ancestris/trancestris/application/actions/SaveZipRefBundleAction.java
+++ Tools/trancestris/Application/src/org/ancestris/trancestris/application/actions/SaveZipRefBundleAction.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import org.ancestris.trancestris.explorers.zipexplorer.ZipExplorerTopComponent;
 import org.ancestris.trancestris.resources.ZipArchive;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
@@ -15,7 +18,19 @@ public final class SaveZipRefBundleAction implements ActionListener {
         TopComponent tc = WindowManager.getDefault().findTopComponent("ZipExplorerTopComponent");
         ZipArchive zipArchive = ((ZipExplorerTopComponent) tc).getBundles();
         if (zipArchive != null) {
-            zipArchive.writeRef();
+            NotifyDescriptor nd = new NotifyDescriptor.Confirmation(NbBundle.getMessage(SendTranslationAction.class, "SaveZipRefBundleAction.msg.askconfirmation"), 
+                                                                    NbBundle.getMessage(SendTranslationAction.class, "SaveZipRefBundleAction.msg.askconfirmationtitle"),
+                                                                    NotifyDescriptor.WARNING_MESSAGE,
+                                                                    NotifyDescriptor.YES_NO_OPTION);
+            if (DialogDisplayer.getDefault().notify(nd) == NotifyDescriptor.OK_OPTION) {
+                zipArchive.writeRef();
+                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(SendTranslationAction.class, "SaveZipRefBundleAction.msg.ok"), 
+                        NotifyDescriptor.INFORMATION_MESSAGE));
+            } else {
+                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(NbBundle.getMessage(SendTranslationAction.class, "SaveZipRefBundleAction.msg.cancelled"), 
+                        NotifyDescriptor.INFORMATION_MESSAGE));
+            }
+            
         }
 
     }
